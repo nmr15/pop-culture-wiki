2a86d3c6297ea99094a7b3bc26dcad1a7fe1c5b0
Niel <niel_ravelo@yahoo.com>
2025-01-31 11:03:06 -0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => 
+{
+  return (
+    <>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <a href="#" className="navbar-brand">Pop Culture Wiki</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
