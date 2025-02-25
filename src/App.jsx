06d50a764b02e9d712b5e7fc74d8f83f0d4bf2f8
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-02-25 10:01:34 -0800

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import './App.css'
+import Mcu from "./pages/Mcu"
 import Community from "./pages/Community"
 import MarvelComics from "./pages/MarvelComics"
 import Spiderman from "./pages/Spiderman"
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/mcu' element={<Mcu />} />
         <Route path='/marvelcomics' element={<MarvelComics />} />
         <Route path='/marvelcomics/spiderman' element={<Spiderman />} />
         <Route path='/starwars' element={<StarWars />} />
