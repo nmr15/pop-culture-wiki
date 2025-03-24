5892cfde78141478374946ff1230c9a25a9e5319
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-03-24 09:45:59 -0700

    Add files via upload

--- src/pages/Mcu.jsx
+++ src/pages/Mcu.jsx
@@ -7,11 +7,11 @@ const Mcu = () =>
           <li className="py-1">
             <a href="#top">Top</a>
           </li>
-          <div class="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#development"
+          <div className="accordion py-1">
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#development"
               aria-expanded="true" aria-controls="development">
             </span>
-            <li class="d-inline ms-2"><a href="#1">Development</a>
+            <li className="d-inline ms-2"><a href="#1">Development</a>
               <ul id="development" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                 <li className="py-1">
                   <a href="#1.1">Marvel Studios films and series</a>
@@ -28,40 +28,40 @@ const Mcu = () =>
             </li>
           </div>
           <div className="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#films"
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#films"
               aria-expanded="true" aria-controls="films">
             </span>
-            <li class="d-inline ms-2"><a href="#2">Feature films</a></li>
+            <li className="d-inline ms-2"><a href="#2">Feature films</a></li>
             <ul id="films" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <li className="py-1"><a href="#2.1">The Infinity Saga</a></li>
               <li className="py-1"><a href="#2.2">The Multiverse Saga</a></li>
             </ul>
           </div>
           <div className="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#series-specials"
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#series-specials"
               aria-expanded="true" aria-controls="series-specials">
             </span>
-            <li class="d-inline ms-2"><a href="#3">Television series and specials</a></li>
+            <li className="d-inline ms-2"><a href="#3">Television series and specials</a></li>
             <ul id="series-specials" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <li className="py-1"><a href="#3.1">Marvel Television series</a></li>
               <li className="py-1"><a href="#3.2">Marvel Studio series</a></li>
             </ul>
           </div>
           <div className="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#short-films"
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#short-films"
               aria-expanded="true" aria-controls="short-films">
             </span>
-            <li class="d-inline ms-2"><a href="#4">Short films</a></li>
+            <li className="d-inline ms-2"><a href="#4">Short films</a></li>
             <ul id="short-films" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <li className="py-1"><a href="#4.1">Marvel One-Shots</a></li>
               <li className="py-1"><a href="#4.2">I Am Groot</a></li>
             </ul>
           </div>
           <div className="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#other-media"
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#other-media"
               aria-expanded="true" aria-controls="other-media">
             </span>
-            <li class="d-inline ms-2"><a href="#5">Other media</a></li>
+            <li className="d-inline ms-2"><a href="#5">Other media</a></li>
             <ul id="other-media" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <li className="py-1"><a href="#5.1">Digital series</a></li>
               <li className="py-1"><a href="#5.2">Comic books</a></li>
@@ -70,22 +70,22 @@ const Mcu = () =>
             </ul>
           </div>
           <div className="accordion py-1">
-            <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#universe"
+            <span className="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#universe"
               aria-expanded="true" aria-controls="universe">
             </span>
-            <li class="d-inline ms-2"><a href="#6">Fictional universe</a></li>
+            <li className="d-inline ms-2"><a href="#6">Fictional universe</a></li>
             <ul id="universe" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <li className="py-1"><a href="#6.1">Timeline</a></li>
               <li className="py-1"><a href="#6.2">Multiverse</a></li>
             </ul>
           </div>
-          <li class="d-inline ms-2"><a href="#7">Recurring cast and characters</a></li>
-          <li class="d-inline ms-2"><a href="#8">Reception</a></li>
-          <div class="accordion py-1">
+          <li className="py-1"><a href="#7">Recurring cast and characters</a></li>
+          <li className="py-1"><a href="#8">Reception</a></li>
+          <div className="accordion py-1">
             <span class="accordion-toggle dropdown-toggle d-inline ptr" data-bs-toggle="collapse" data-bs-target="#impact"
               aria-expanded="true" aria-controls="impact">
             </span>
-            <li class="d-inline ms-2"><a href="#9">Cultural impact</a>
+            <li className="d-inline ms-2"><a href="#9">Cultural impact</a>
               <ul id="impact" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                 <li className="py-1">
                   <a href="#9.1">Other studios</a>
@@ -118,6 +118,32 @@ const Mcu = () =>
             produced by <a href="#">Marvel Studios</a>. The films are based on characters that appear in American comic books published by <a href="#">Marvel Comics</a>.
             The franchise also includes <a href="#">several television series</a>, short films, digital series, and literature. The shared universe, much like the original
             <a href="#">Marvel Universe</a> in comic books, was established by crossing over common plot elements, settings, cast, and characters.
+          </p>
+          <p>
+            Marvel Studios releases its films in groupd called "Phases", with the first three phases collectively known as "The Infinity Saga" and the following three phases
+            as "The Multiverse Saga". The first MCU film, <a href="#">Iron Man</a> (2008), began <a href="#">Phase One</a>, which culminated in the 2012 crossover film
+            <a href="#">The Avengers</a>. <a href="#">Phase Two</a> begain with <a href="#">Iron Man 3</a> (2013) and concluded with <a href="#">Ant-Man</a> (2015), while
+            <a href="#">Phase Three</a> began with <a href="#">Captain America: Civil War</a> (2016) and concluded with <a href="#">Spider-Man: Far From Home</a> (2019).
+            <a href="#">Black Widow</a> (2021) is the first film in <a href="#">Phase Four</a>, which concluded with <a href="#">Black Panther: Wakanda Forever</a> (2022),
+            while <a href="#">Phase Five</a> began with <a href="#">Ant-Man and the Wasp: Quantumania</a> (2023) and will conclude with <a href="#">Thunderbolts*</a> (2025).
+            <a href="#">Phase Six</a> will begin with <a href="#">The Fantastic Four: First Steps</a> (2025) and it will conclude with <a href="#">Avengers: Doomsday</a>
+            (2026) and <a href="#">Avengers: Secret Wars</a> (2027).
+          </p>
+          <p>
+            <a href="#">Marvel Television</a> expanded the universe to network television with <a href="#">Agents of S.H.I.E.L.D.</a> on <a href="#">ABC</a> in 2013 before
+            further expanding to streaming television on <a href="#">Netflix</a> and <a href="#">Hulu</a> and to cable television on Freeform. They also produced the digital
+            series <a href="#">Agents of S.H.I.E.L.D.: Slingshot</a> (2016). Marvel Studios began producing their own television series for streaming on <a href="#">Disney +</a>,
+            starting with <a href="#">WandaVision</a> in 2021, as the beginning of Phase Four. That phase also saw the studio expand to television specials, known as
+            <a href="#">Marvel Studios Special Presentation</a>, starting with <a href="#">Werewolf by Night</a> (2022). The MCU includes <a href="#">various tie-in comics</a> 
+            published by Marvel Comics, a series of direct-to-video short films called <a href="#">Marvel One-Shots</a> from 2011 to 2014, and viral marketing campaigns for some
+            films featruing the faux news program <a href="#">WHIH Newsfront</a> (2015-2016) and <a href="#">The Daily Bugle</a> (2019-2022).
+          </p>
+          <p>
+            The franchise has been commercially successful, becoming one of the highest-grossing media franchises of all time, and it has received generally positive reviews
+            from critics. The studio attributed several Multiverse Saga projects performing below expectations to the increase in the amount of content being produced after the
+            2019 film <a href="#">Avengers: Endgame</a>, and began decreasing its content output from 2024. The MCU has inspired other film and television studios to attempt
+            similar shared universes and has also inspired several themed attractions, an art exhibit, television specials, literary material, multiple tie-in video games,
+            and commercials.
           </p>
         </div>
       </main>
