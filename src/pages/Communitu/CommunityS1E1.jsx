27185514c23aa28e39e41cd317c7cc5f3806ccb4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-08-29 15:48:01 -0700

    Update CommunityS1E1.jsx

--- src/pages/Communitu/CommunityS1E1.jsx
+++ src/pages/Communitu/CommunityS1E1.jsx
@@ -1,1 +1,69 @@
+const CommunityS1E1 = () =>
+{
+  return(
+    <>
+    <nav className="sidebar bg-dark">
 
+    </nav>
+    <main className="main px-5 py-4">
+      <div id="top">
+        <h1 className="article-heading">Pilot (Community)</h1>
+      </div>
+      <div className="pt-2">
+        <p>
+          Pilot is the first episode of the first season of the NBC sitcom <a href="#">Community</a>. It aired in the United
+          States on September 17, 2009. Written by <a href="#">Dan Harmon</a>, the show's creator, the episode was directed
+          by <a href="#">Anthony Russon and Joe Russo</a>. The episode introduces <a href="#">Jeffrey Winger</a> 
+          (<a href="">Joel McHale</a>), a disbarred lawyer who is forced to attend community college to get his license back.
+          He tries to expolit his friendship with one of the faculty members for easy credits, but fails, and is forced to
+          join a Spanish study group. The members of the study group make up the main cast of the series.
+        </p>
+        <p>
+          The show takes place at the fictional Greendal Community College in Greendale, Colorado. Harmon based the show on
+          his own experiences in community college and partly modeled the character of Jeff Winger on himself. He emphasized
+          the cast's importance to the show and also credited some of the actors for their improvisational skills. The episode
+          attracted generally favorable reviews from Rotten Tomatoes, which praised both the actors and the comedy writing.
+          The show received good viewership and stood out in NBC's Thursday lineup, where the network's other shows suffered
+          declines in ratings.
+        </p>
+      </div>
+      <div>
+        <h2 className="article-heading" id="1">Plot</h2>
+        <p>
+          Jeff Winger, a lawyer disbarred for having falsified his degree, has enrolled at Greendale Community College as a
+          condition for getting his license back. His plan is to use his friendship with <a href="#">Dr. Ian Duncan</a>, a
+          professor at the college and Jeff's client, to obtain test answers. In the cafeteria, Jeff meets the attractive
+          <a href="#">Britta Perry</a> (<a href="">Gillian Jacobs</a>) and, in an attempt to get closer to her, convinces
+          her to join his non-existent Spanish study group. As the study group convenes, Jeff pretends the other members
+          simply did not show up. Britta, however, has invited <a href="#">Abed Nadir</a> (<a href="">Danny Pudi</a>) to
+          join them. Jeff unsuccessfully tries to make him leave and then gets called to the football field by Duncan, who
+          makes a futile attempt to appeal to Jeff's conscience and eventually agrees to provide the answers. Jeff returns
+          to the study group, discovering that Abed has also invited <a href="#">Troy</a> (<a href="#">Donald Glover</a>),
+          <a href="#">Shirley</a> (<a href="#">Yvette Nicole Brown</a>), <a href="#">Pierce</a> (<a href="#">Chevy 
+          Chase</a>), and <a href="#">Annie</a> (<a href="#">Alison Brie</a>). Finding Britta smoking a cigarette outside,
+          he suggests the two study alone, but she persuades him to return.
+        </p>
+        <p>
+          Jeff, eager to get away, deliberately stirs up a fight among the group then leaves once more to get the test
+          answers. Duncan hands him a sealed envelope, but only in return for Jeff's Lexus. When Jeff returns, the group
+          is in turmoil, and Britta promises to go out with him if he can restore order. He does this through an
+          inspirational speech, but Britta sees through his lies and asks him to leave. Jeff then comes clean about
+          everything, adding that since he now has the test answers, he does not need the study group. As the group turns
+          against him, he leaves.
+        </p>
+        <p>
+          Leaving the building, Jeff finds all the pages in the envelope to be blank. He returns to the office of Duncan,
+          who tells him that he wanted to teach him a lesson, although Jeff gets his car keys back. As he is about to leave
+          campus, Jeff runs into Pierce and Troy and shows his ability to make people feel better about themselves through
+          advice and encouragement. The others join, and Jeff admits to being a fraud. He says that he does not have the test
+          answers, and that he will probably flunk next day's test. The group has now taken a liking to him, and they invite
+          him back in. The episode ends with a dedication to the recently deceased John Hughes, whose film The Breakfast
+          Club had been repeated references throughot the episode.
+        </p>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default CommunityS1E1;
