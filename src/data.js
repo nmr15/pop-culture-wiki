fec6447a2d4b197a623c77b683681ea1c2dbdbf4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-11 14:39:04 -0700

    new array

--- src/data.js
+++ src/data.js
@@ -438,3 +438,30 @@ export const communitys1 = [
     "route": "/community/pascal's_triangle_revisited"
   },
 ]
+
+const beatles = [
+  {
+    "id": "btls1",
+    "title": "John Lennon",
+    "file": "JohnLennon",
+    "route": "/beatles/john_lennon"
+  },
+  {
+    "id": "btls2",
+    "title": "Paul McCartney",
+    "file": "PaulMcCartney",
+    "route": "/beatles/paul_mccartney"
+  },
+  {
+    "id": "btls3",
+    "title": "George Harrison",
+    "file": "GeorgeHarrison",
+    "route": "/beatles/george_harrison"
+  },
+  {
+    "id": "btls4",
+    "title": "Ringo Starr",
+    "file": "RingoStarr",
+    "route": "/beatles/ringo_starr"
+  }
+]
