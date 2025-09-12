9d8af49de6340398e8da17a013210e9c795333b1
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-12 08:00:19 -0700

    new folder and file

---
+++ src/data/dataBeatles.js
@@ -0,0 +1,26 @@
+export const beatles = [
+  {
+    "id": "btls1",
+    "title": "John Lennon",
+    "file": "JohnLennon",
+    "route": "/beatles/john_lennon"
+  },
+  {
+    "id": "btls2",
+    "title": "Paul McCartney",
+    "file": "PaulMcCartney",
+    "route": "/beatles/paul_mccartney"
+  },
+  {
+    "id": "btls3",
+    "title": "George Harrison",
+    "file": "GeorgeHarrison",
+    "route": "/beatles/george_harrison"
+  },
+  {
+    "id": "btls4",
+    "title": "Ringo Starr",
+    "file": "RingoStarr",
+    "route": "/beatles/ringo_starr"
+  }
+]
