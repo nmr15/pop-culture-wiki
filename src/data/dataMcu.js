9ac579eb4db3d20fab5e643af95e601444d050e3
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-18 09:10:51 -0700

    new file and array

---
+++ src/data/dataMcu.js
@@ -0,0 +1,466 @@
+export const mcu = [
+  {
+    "id": "mcu1",
+    "title": "Iron Man",
+    "file": "IronManFilm",
+    "route": "/mcu/iron_man_film",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu2",
+    "title": "The Incredible Hulk",
+    "file": "TheIncredibuleHulkFilm",
+    "route": "/mcu/the_incredible_hulk_film",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu3",
+    "title": "Iron Man 2",
+    "file": "IronMan2",
+    "route": "/mcu/iron_man_2",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu4",
+    "title": "Thor",
+    "file": "ThorFilm",
+    "route": "/mcu/thor_film",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu5",
+    "title": "Captain America: The First Avenger",
+    "file": "CaptainAmericaTheFirstAvenger",
+    "route": "/mcu/captain_america_the_first_avenger",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu6",
+    "title": "The Avengers",
+    "file": "TheAvengersFilm",
+    "route": "mcu/the_avengers_film",
+    "media": "film",
+    "phase": 1
+  },
+  {
+    "id": "mcu7",
+    "title": "Iron Man 3",
+    "file": "IronMan3",
+    "route": "/mcu/iron_man_3",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu8",
+    "title": "Thor: The Dark World",
+    "file": "ThorTheDarkWorld",
+    "route": "/mcu/thor_the_dark_world",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu9",
+    "title": "Captain America: The Winter Soldier",
+    "file": "CaptainAmericaTheWinterSoldier",
+    "route": "/mcu/captain_america_the_winter_soldier",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu10",
+    "title": "Guardians of the Galaxy",
+    "file": "GuardiansOfTheGalaxyFilm",
+    "route": "/mcu/guardians_of_the_galaxy_film",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu11",
+    "title": "Avengers: Age of Ultron",
+    "file": "AvengersAgeOfUltron",
+    "route": "/mcu/avengers_age_of_ultron",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu12",
+    "title": "Ant-Man",
+    "file": "AntManFilm",
+    "route": "/mcu/antman_film",
+    "media": "film",
+    "phase": 2
+  },
+  {
+    "id": "mcu13",
+    "title": "Captain America: Civil War",
+    "file": "CaptainAmericaCivilWar",
+    "route": "/mcu/captain_america_civil_war",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu14",
+    "title": "Doctor Strange",
+    "file": "DoctorStrangeFilm",
+    "route": "/mcu/doctor_strange_film",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu15",
+    "title": "Guardians of the Galaxy Vol. 2",
+    "file": "GuardiansOfTheGalaxyVol2",
+    "route": "/mcu/guardians_of_the_galaxy_vol_2",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu16",
+    "title": "Spider-Man: Homecoming",
+    "file": "SpiderManHomecoming",
+    "route": "/mcu/spiderman_homecoming",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu17",
+    "title": "Thor: Ragnarok",
+    "file": "ThorRagnarok",
+    "route": "/mcu/thor_ragnarok",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu18",
+    "title": "Black Panther",
+    "file": "BlackPanther",
+    "route": "/mcu/black_panther",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu19",
+    "title": "Avengers: Infinity War",
+    "file": "AvengersInfinityWar",
+    "route": "/mcu/avengers_infinity_war",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu20",
+    "title": "Ant-Man and the Wasp",
+    "file": "AntManAndTheWasp",
+    "route": "/mcu/antman_and_the_wasp",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu21",
+    "title": "Captain Marvel",
+    "file": "CaptainMarvelFilm",
+    "route": "/mcu/captain_marvel_film",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu22",
+    "title": "Avengers: Endgame",
+    "file": "AvengersEndgame",
+    "route": "/mcu/avengers_endgame",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu23",
+    "title": "Spider-Man: Far From Home",
+    "file": "SpiderManFarFromHome",
+    "route": "/mcu/spiderman_far_from_home",
+    "media": "film",
+    "phase": 3
+  },
+  {
+    "id": "mcu24",
+    "title": "WandaVision",
+    "file": "WandaVision",
+    "route": "/mcu/wandavision",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu25",
+    "title": "The Falcon and the Winter Soldier",
+    "file": "TheFalconAndTheWinterSoldier",
+    "route": "/mcu/the_falcon_and_the_winter_soldier",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu26",
+    "title": "Loki Season 1",
+    "file": "LokiSeries",
+    "route": "/mcu/loki_series",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu27",
+    "title": "Black Widow",
+    "file": "BlackWidowFilm",
+    "route": "/mcu/black_widow_film",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu28",
+    "title": "What If...?",
+    "file": "WhatIf",
+    "route": "/mcu/what_if",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu29",
+    "title": "Shang-Chi and the Legend of the Ten Rings",
+    "file": "ShangChiFilm",
+    "route": "/mcu/shangchi_and_the_legends_of_the_ten_rings",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu30",
+    "title": "Eternals",
+    "file": "Eternals",
+    "route": "/mcu/eternals",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu31",
+    "title": "Hawkeye",
+    "file": "HawkeyeSeries",
+    "route": "/mcu/hawkeye_series",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu32",
+    "title": "Spider-Man: No Way Home",
+    "file": "SpiderManNoWayHome",
+    "route": "/mcu/spiderman_no_way_home",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu33",
+    "title": "Moon Knight",
+    "file": "MoonKnightSeries",
+    "route": "/mcu/moon_knight_series",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu34",
+    "title": "Doctor Strange in the Multiverse of Madness",
+    "file": "DoctorStrangeMultiverseMadness",
+    "route": "/mcu/doctor_strange_in_the_multiverse_of_madness",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu35",
+    "title": "Ms. Marvel",
+    "file": "MsMarvelSeries",
+    "route": "/mcu/ms._marvel_series",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu36",
+    "title": "Thor: Love and Thunder",
+    "file": "ThorLoveAndThunder",
+    "route": "/mcu/thor_love_and_thunder",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu37",
+    "title": "I am Groot Season 1",
+    "file": "IAmGroot",
+    "route": "/mcu/i_am_groot",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu38",
+    "title": "She-Hulk: Attorney at Law",
+    "file": "SheHulkAttorneyAtLaw",
+    "route": "/mcu/shehulk_attorney_at_law",
+    "media": "series",
+    "phase": 4
+  },
+  {
+    "id": "mcu39",
+    "title": "Werewolf by Night",
+    "file": "WerewolfByNight",
+    "route": "/mcu/werewolf_by_night",
+    "media": "special",
+    "phase": 4
+  },
+  {
+    "id": "mcu40",
+    "title": "Black Panther: Wakanda Forever",
+    "file": "BlackPantherWakandaForever",
+    "route": "/mcu/black_panther_wakanda_forever",
+    "media": "film",
+    "phase": 4
+  },
+  {
+    "id": "mcu41",
+    "title": "The Guardians of the Galaxy Holiday Special",
+    "file": "GuardiansOfTheGalaxyHolidaySpecial",
+    "route": "/mcu/the_guardians_of_the_galaxy_holiday_special",
+    "media": "special",
+    "phase": 4
+  },
+  {
+    "id": "mcu42",
+    "title": "Ant-Man and the Wasp: Quantumania",
+    "file": "AntManAndTheWaspQuantumania",
+    "route": "/mcu/antman_and_the_wasp_quantumania",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu43",
+    "title": "Guardians of the Galaxy Vol. 3",
+    "file": "GuardiansOfTheGalaxyVol3",
+    "route": "/mcu/guardians_of_the_galaxy_vol_3",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu44",
+    "title": "Secret Invasion",
+    "file": "SecretInvasion",
+    "route": "/mcu/secret_invasion",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu45",
+    "title": "I Am Groot Season 2",
+    "file": "IAmGroot",
+    "route": "/mcu/i_am_groot",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu46",
+    "title": "Loki Season 2",
+    "file": "LokiSeries",
+    "route": "/mcu/loki_series",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu47",
+    "title": "The Marvels",
+    "file": "TheMarvels",
+    "route": "/mcu/the_marvels",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu48",
+    "title": "What If...? Season 2",
+    "file": "WhatIf",
+    "route": "/mcu/what_if",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu49",
+    "title": "Echo",
+    "file": "Echo",
+    "route": "mcu/echo",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu50",
+    "title": "Deadpool & Wolverine",
+    "file": "DeadpoolAndWolverine",
+    "route": "/mcu/deadpool_&_wolverine",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu51",
+    "title": "Agatha All Along",
+    "file": "AgathaAllAlong",
+    "route": "/mcu/agatha_all_along",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu52",
+    "title": "What If...? Season 3",
+    "file": "WhatIf",
+    "route": "/mcu/what_if",
+    "media": "series 5",
+    "phase": ""
+  },
+  {
+    "id": "mcu53",
+    "title": "Captain America: Brave New World",
+    "file": "CaptainAmericaBraveNewWorld",
+    "route": "/mcu/captain_america_brave_new_world",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu52",
+    "title": "Daredevil: Born Again",
+    "file": "DaredevilBornAgain",
+    "route": "/mcu/daredevil_born_again",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu53",
+    "title": "Thunderbolts*",
+    "file": "Thunderbolts",
+    "route": "/mcu/thunderbolts",
+    "media": "film",
+    "phase": 5
+  },
+  {
+    "id": "mcu54",
+    "title": "Ironheart",
+    "file": "IronheartSeries",
+    "route": "/mcu/ironheart_series",
+    "media": "series",
+    "phase": 5
+  },
+  {
+    "id": "mcu55",
+    "title": "The Fantastic Four: First Steps",
+    "file": "FantasticFourFirstSteps",
+    "route": "/mcu/the_fantastic_four_first_steps",
+    "media": "film",
+    "phase": 6
+  },
+  {
+    "id": "mcu56",
+    "title": "Eyes of Wakanda",
+    "file": "EyesOfWakanda",
+    "route": "/mcu/eyes_of_wakanda",
+    "media": "series",
+    "phase": 6
+  }
+]
