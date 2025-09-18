b5e037930e2b2fd9dfc8d22adcde486ca84fec5e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-18 09:55:31 -0700

    fixed style

--- src/pages/GravityFalls/GravityFalls.jsx
+++ src/pages/GravityFalls/GravityFalls.jsx
@@ -5,7 +5,7 @@ const GravityFalls = () =>
             <nav className="sidebar bg-dark">
 
             </nav>
-            <main className="px-5 py-4">
+            <main className="main px-5 py-4">
                 <div id="top">
                     <h1 className="article-heading">Gravity Falls</h1>
                 </div>
