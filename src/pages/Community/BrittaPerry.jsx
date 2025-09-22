b234de1e48e028da828c4e342389351c3d7f3af9
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-22 16:51:26 -0700

    fixed style

--- src/pages/Community/BrittaPerry.jsx
+++ src/pages/Community/BrittaPerry.jsx
@@ -10,7 +10,7 @@ const BrittaPerry = () =>
                     <li className="py-1"><a href="#top">Top</a></li>
                 </ul>
             </nav>
-            <main className="px-5 py-4" id="top">
+            <main className="main px-5 py-4" id="top">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><Link to="/">Home</Link></li>
