e7616c453fcd35448bdcd520c46165b901eb3425
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-22 22:18:38 -0700

    Update TroyBarnes.jsx

--- src/pages/Community/TroyBarnes.jsx
+++ src/pages/Community/TroyBarnes.jsx
@@ -14,6 +14,7 @@ const TroyBarnes = () =>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                        <li className="breadcrumb-item"><Link to="/community">Community</Link></li>
                         <li class="breadcrumb-item active" aria-current="page">Troy Barnes</li>
                     </ol>
                 </nav>
@@ -27,3 +28,4 @@ const TroyBarnes = () =>
 
 
 export default TroyBarnes;
+
