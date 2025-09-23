34fe60d0ee720bd48302e28f46d773260e717dfd
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-22 22:16:47 -0700

    Update AbedNadir.jsx

--- src/pages/Community/AbedNadir.jsx
+++ src/pages/Community/AbedNadir.jsx
@@ -14,6 +14,7 @@ const AbedNadir = () =>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                        <li className="breadcrumb-item"><Link to="/community">Community</Link></li>
                         <li class="breadcrumb-item active" aria-current="page">Abed Nadir</li>
                     </ol>
                 </nav>
@@ -27,3 +28,4 @@ const AbedNadir = () =>
 
 
 export default AbedNadir;
+
