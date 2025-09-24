fc3c2792c70bd0079013dc58fca647d414fc9a08
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-24 09:33:25 -0700

    new array

---
+++ src/data/dataHimym.js
@@ -0,0 +1,38 @@
+export const HimymChar = [
+  {
+    "id": "himymchar1",
+    "title": "Ted Mosby",
+    "file": "TedMosby",
+    "route": "/himym/ted_mosby"
+  },
+  {
+    "id": "himymchar2",
+    "title": "Marshall Eriksen",
+    "file": "MarshallEriksen",
+    "route": "/himym/marshall_eriksen"
+  },
+  {
+    "id": "himymchar3",
+    "title": "Lily Aldrin",
+    "file": "LilyAldrin",
+    "route": "/himym/lily_aldrin"
+  },
+  {
+    "id": "himymchar4",
+    "title": "Robin Scherbatsky",
+    "file": "RobinScherbatsky",
+    "route": "/himym/robin_scherbatsky"
+  },
+  {
+    "id": "himymchar5",
+    "title": "Barney Stinson",
+    "file": "BarneyStinson",
+    "route": "/himym/barney_stinson"
+  },
+  {
+    "id": "himymchar6",
+    "title": "Tracy McConnell",
+    "file": "TracyMcConnell",
+    "route": "/himym/tracy_mcconnell"
+  }
+];
