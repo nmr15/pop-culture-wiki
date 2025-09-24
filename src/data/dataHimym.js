9ca2a8d825996f8f4b6699104727fb31f08630d0
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-24 13:10:12 -0700

    new array

--- src/data/dataHimym.js
+++ src/data/dataHimym.js
@@ -36,3 +36,138 @@ export const HimymChar = [
     "route": "/himym/tracy_mcconnell"
   }
 ];
+
+export const HimymS1 = [
+  {
+    "id": "himyms1e1",
+    "title": "Pilot",
+    "file": "HimymS1E1",
+    "route": "/himym/pilot"
+  },
+  {
+    "id": "himyms1e2",
+    "title": "Purple Giraffe",
+    "file": "HimymS1E2",
+    "route": "/himym/purple_giraffe"
+  },
+  {
+    "id": "himyms1e3",
+    "title": "Sweet Taste of Liberty",
+    "file": "HimymS1E3",
+    "route": "/himym/sweet_taste_of_liberty"
+  },
+  {
+    "id": "himyms1e4",
+    "title": "Return of the Shirt",
+    "file": "HimymS1E4",
+    "route": "/himym/return_of_the_shirt"
+  },
+  {
+    "id": "himyms1e5",
+    "title": "Okay Awesome",
+    "file": "HimymS1E5",
+    "route": "/himym/okay_awesome"
+  },
+  {
+    "id": "himyms1e6",
+    "title": "Slutty Pumpkin",
+    "file": "HimymS1E6",
+    "route": "/himym/slutty_pumpkin"
+  },
+  {
+    "id": "himyms1e7",
+    "title": "Matchmaker",
+    "file": "HimymS1E7",
+    "route": "/himym/matchmaker"
+  },
+  {
+    "id": "himyms1e8",
+    "title": "The Duel",
+    "file": "HimymS1E8",
+    "route": "/himym/the_duel"
+  },
+  {
+    "id": "himyms1e9",
+    "title": "Belly Full of Turkey",
+    "file": "HimymS1E9",
+    "route": "/himym/belly_full_of_turkey"
+  },
+  {
+    "id": "himyms1e10",
+    "title": "The Pineapple Incident",
+    "file": "HimymS1E10",
+    "route": "/himym/the_pineapple_incident"
+  },
+  {
+    "id": "himyms1e11",
+    "title": "The Limo",
+    "file": "HimymS1E11",
+    "route": "/himym/the_limo"
+  },
+  {
+    "id": "himyms1e12",
+    "title": "The Wedding",
+    "file": "HimymS1E12",
+    "route": "/himym/the_wedding"
+  },
+  {
+    "id": "himyms1e13",
+    "title": "Drumroll, Please",
+    "file": "HimymS1E13",
+    "route": "/himym/drumroll_please"
+  },
+  {
+    "id": "himyms1e14",
+    "title": "Zip, Zip, Zip",
+    "file": "HimymS1E14",
+    "route": "/himym/zip_zip_zip"
+  },
+  {
+    "id": "himyms1e15",
+    "title": "Game Night",
+    "file": "HimymS1E15",
+    "route": "/himym/game_night"
+  },
+  {
+    "id": "himyms1e16",
+    "title": "Cupcake",
+    "file": "HimymS1E16",
+    "route": "/himym/cupcake"
+  },
+  {
+    "id": "himyms1e17",
+    "title": "Life Among the Gorillas",
+    "file": "HimymS1E17",
+    "route": "/himym/life_among_the_gorillas"
+  },
+  {
+    "id": "himyms1e18",
+    "title": "Nothing Good Happens After 2 A.M.",
+    "file": "HimymS1E18",
+    "route": "/himym/nothing_good_happens_after_2am"
+  },
+  {
+    "id": "himyms1e19",
+    "title": "Mary the Paralegal",
+    "file": "HimymS1E19",
+    "route": "/himym/mary_the_paralegal"
+  },
+  {
+    "id": "himyms1e20",
+    "title": "Best Prom Ever",
+    "file": "HimymS1E20",
+    "route": "/himym/best_prom_ever"
+  },
+  {
+    "id": "himyms1e21",
+    "title": "Milk",
+    "file": "HimymS1E21",
+    "route": "/himym/milk"
+  },
+  {
+    "id": "himyms1e22",
+    "title": "Come On",
+    "file": "HimymS1E22",
+    "route": "/himym/come_on"
+  }
+];
