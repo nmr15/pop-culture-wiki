51b4909b2798a6e8c7915c6cebe358ff2b4e2086
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-09-24 09:23:03 -0700

    updated table

--- src/components/TableBeatles.jsx
+++ src/components/TableBeatles.jsx
@@ -52,6 +52,76 @@ const TableBeatles = () =>
                                 </div>
                             </td>
                         </tr>
+                        <tr>
+                            <td>Beatles for Sale</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesBfs.map((Bfs) => (
+                                        <Link to={Bfs.route} className="table-list-link" key={Bfs.id}>{Bfs.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Help</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesHlp.map((Hlp) => (
+                                        <Link to={Hlp.route} className="table-list-link" key={Hlp.id}>{Hlp.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Rubber Soul</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesRbs.map((Rbs) => (
+                                        <Link to={Rbs.route} className="table-list-link" key={Rbs.id}>{Rbs.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Revolver</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesRvlr.map((Rvlr) => (
+                                        <Link to={Rvlr.route} className="table-list-link" key={Rvlr.id}>{Rvlr.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Sgt. Pepper's Lonely Hearts Club Band</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesSgt.map((Sgt) => (
+                                        <Link to={Sgt.route} className="table-list-link" key={Sgt.id}>{Sgt.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Magical Mystery Tour</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesMmt.map((Mmt) => (
+                                        <Link to={Mmt.route} className="table-list-link" key={Mmt.id}>{Mmt.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>The Beatles (The White Album)</td>
+                            <td>
+                                <div className="table-list">
+                                    {beatlesTwa.map((Twa) => (
+                                        <Link to={Twa.route} className="table-list-link" key={Twa.id}>{Twa.title}</Link>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
