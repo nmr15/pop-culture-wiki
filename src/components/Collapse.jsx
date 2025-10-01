aec2863d5b463b932e07d86dd98e977e768c6434
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-01 08:33:23 -0700

    test collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -5,8 +5,8 @@ const Collapse = () =>
   return(
     <>
       <div>
-        <span style={{fontSize: "14px"}}>▼</span>
-        <li className="sidebar-list-item" style={{display: "inline"}}>
+        <span className="sidebar-toggle">▼</span>
+        <li className="sidebar-list-item sidebar-collapse">
           <a href="#">List Item</a>
           <ul>
             <li className="sidebar-list-item">
