c033fb717c1528e5d0933992e13257ee92b0b891
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-03 16:12:50 -0700

    update

--- src/pages/Mcu/IronManFilm.jsx
+++ src/pages/Mcu/IronManFilm.jsx
@@ -1,10 +1,25 @@
+import { Link } from 'react-router-dom'
+import { mcu, marveltv } from '../../data/dataMcu'
+import Card from '../../components/Card'
+import Collapse from '../../components/Collapse'
+import TableMcu from "../../components/TableMcu"
+
 const IronManFilm = () =>
 {
   return(
     <>
-      <div>
-        <h1>IronManFilm</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-ite">
+            <a href="#top">Top</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <div>
+          <h1 className="article-heading">{mcu[0].title}</h1>
+        </div>
+      </main>
     </>
   )
 }
