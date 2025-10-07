dfcf2f438027216ca31b7ae26d51bdd50d0c4021
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-07 13:26:12 -0700

    update

--- src/pages/Mcu/IronMan2.jsx
+++ src/pages/Mcu/IronMan2.jsx
@@ -43,6 +43,24 @@ const IronMan2 = () =>
           <li className="sidebar-list-item"><a href="#8">Sequel</a></li>
         </ul>
       </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Iron Man 2</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Iron Man 2</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/e/ed/Iron_Man_2_poster.jpg" 
+            alt="Iron Man 2" 
+            className="article-heading-img" 
+          />
+        </div>
+        
+      </main>
     </>
   )
 }
