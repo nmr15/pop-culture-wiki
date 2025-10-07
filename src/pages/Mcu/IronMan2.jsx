635989a49510d4e3f871b2708e3dc11cf43b4dc4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-07 13:21:29 -0700

    update

--- src/pages/Mcu/IronMan2.jsx
+++ src/pages/Mcu/IronMan2.jsx
@@ -1,10 +1,48 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import TableMcu from "../../components/TableMcu"
+
 const IronMan2 = () =>
 {
   return(
     <>
-      <div>
-        <h1>IronMan2</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical reception"},
+              {id: "7.3", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Sequel</a></li>
+        </ul>
+      </aside>
     </>
   )
 }
