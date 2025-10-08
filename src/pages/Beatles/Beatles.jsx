1dcd6563402f8a9c6be2b04fb2f19221a5990d81
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-08 16:05:19 -0700

    update Beatles

--- src/pages/Beatles/Beatles.jsx
+++ src/pages/Beatles/Beatles.jsx
@@ -68,18 +68,22 @@ const Beatles = () =>
           <li className="sidebar-list-item"><a href="#9">Concert tours</a></li>
         </ul>
       </aside>
-      <main className="main px-5 py-4">
-        <div id="top">
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>The Beatles</li>
+        </ul>
+        <div>
           <h1 className="article-heading">The Beatles</h1>
         </div>
-        <div className="img-right pt-2">
+        <div className="img-right img-top">
           <img 
             src="https://1000logos.net/wp-content/uploads/2017/06/Beatles-Logo-768x370.png" 
             alt="The Beatles" 
             className="article-heading-img"
           />
         </div>
-        <div className="pt-2">
+        <div className="div-top">
           <p>
             <b>The Beatles</b> were an English rock band formed in Liverpool in 1960. The core lineup of the band comprised <a href="#">John Lennon</a>, <a href="#">Paul McCartney</a>,
             <a href="#">George Harrison</a>, and <a href="#">Ringo Starr</a>. They are widely regarded as the most influential band in Western popular music and were integral to the
@@ -88,6 +92,66 @@ const Beatles = () =>
             recording, songwriting, and artistic presentation, the Beatles revolutionised many aspects of the music industry and were often publicised as leaders of the era's youth and
             sociocultural movements
           </p>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="1">History</h2>
+          <div>
+            <h3 className="article-heading-3" id="1.1">1956-1963: Formation</h3>
+            <h4 className="article-heading-4" id="1.1.1">The Quarrymen and name change</h4>
+            <h4 className="article-heading-4" id="1.1.2">Early residencies and UK popularity</h4>
+            <h4 className="article-heading-4" id="1.1.3">First EMI recordings</h4>
+            <h3 className="article-heading-3" id="1.2">1963-1966: Beatlemania and touring years</h3>
+            <h4 className="article-heading-4" id="1.2.1">Please Plase Me and With the Beatles</h4>
+            <h4 className="article-heading-4" id="1.2.1">First visit to the United States and the British Invasion</h4>
+            <h4 className="article-heading-4" id="1.2.1">A Hard Day's Night</h4>
+            <h4 className="article-heading-4" id="1.2.1">1964 world tour, meeting Bob Dylan and stand on civil rights</h4>
+            <h4 className="article-heading-4" id="1.2.1">Beatles for Sale, Help!, and Rubber Soul</h4>
+            <h4 className="article-heading-4" id="1.2.1">Controversies, Revolver, and final tour</h4>
+            <h3 className="article-heading-3" id="1.3">1966-1970: Studio years</h3>
+            <h4 className="article-heading-4" id="1.3.1">Sgt. Pepper's Lonely Hearts Club Band</h4>
+            <h4 className="article-heading-4" id="1.3.2">Magical Mystery Tour and Yellow Submarine</h4>
+            <h4 className="article-heading-4" id="1.3.3">India retreat, Apple Corps, and the White Album</h4>
+            <h4 className="article-heading-4" id="1.3.4">Abbey Road, Let it Be, and separation"</h4>
+            <h3 className="article-heading-3" id="1.4">After the breakup</h3>
+            <h4 className="article-heading-4" id="1.4.1">1970s</h4>
+            <h4 className="article-heading-4" id="1.4.2">1980s</h4>
+            <h4 className="article-heading-4" id="1.4.3">1990s</h4>
+            <h4 className="article-heading-4" id="1.4.4">2000s</h4>
+            <h4 className="article-heading-4" id="1.4.5">2010s</h4>
+            <h4 className="article-heading-4" id="1.4.6">2020s</h4>
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="2">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="3">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="4">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="5">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="6">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="7">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="8">History</h2>
+          <div></div>
+        </div>
+        <div>
+          <h2 className="article-heading2" id="9">History</h2>
+          <div></div>
         </div>
       <TableBeatles />
       </main>
