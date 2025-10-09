5eb67e6b7c3f7a59e639a01274a75aa20b8e924e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-09 16:50:40 -0700

    update Himym

--- src/pages/Himym/Himym.jsx
+++ src/pages/Himym/Himym.jsx
@@ -5,25 +5,23 @@ const Himym = () =>
 {
   return (
     <>
-      <nav className="sidebar bg-dark">
-        <ul>
-          <li className="py-1">
-            <a href="#top">Top</a>
-          </li>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-link"><a href="#top">Top</a></li>
         </ul>
-      </nav>
-      <main className="main px-5 py-4">
-        <div id="top">
+      </aside>
+      <main className="main" id="top">
+        <div>
           <h1 className="article-heading">How I Met Your Mother</h1>
         </div>
-        <div className="img-right pt-2">
+        <div className="img-right img-top">
           <img 
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/HowIMetYourMother.svg/440px-HowIMetYourMother.svg.png" 
             alt="How I Met Your Mother" 
             className="article-heading-img"
           />
         </div>
-        <div className="pt-2">
+        <div className="div-top">
           <p>
             <b>How I Met Your Mother</b> is an American sitcom created by Craig Thomas and Carter Bays for CBS. The series, which aired from September 19, 2005,
             to March 31, 2014, follows main character <a href="#">Ted Mosby</a> and his group of friends in New York City's Manhattan. As a frame story, Ted (in 2030)
