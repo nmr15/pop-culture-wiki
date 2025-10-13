f03c468ef9c11587de50b88471b9f5fa44b16b20
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-13 15:43:22 -0700

    update AT

--- src/pages/AdventureTime/AdventureTime.jsx
+++ src/pages/AdventureTime/AdventureTime.jsx
@@ -5,7 +5,45 @@ const AdventureTime = () =>
 {
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Main characters</a></li>
+          <Collapse 
+            id="2"
+            item="Television series"
+            sub={[
+              {id: "2.1", title: "Adventure Time Shorts (2007)"},
+              {id: "2.1", title: "Adventure Time (2010-2018)"},
+              {id: "2.1", title: "Adventure Time: Distant Lands (2020-2021)"},
+              {id: "2.1", title: "Adventure Time: Fionna and Cake (2023-present)"},
+              {id: "2.1", title: "Side Quest"},
+              {id: "2.1", title: "Heyo BMO"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="References in other media"
+            sub={[
+              {id: "3.1", title: "Cameo in Futurama"},
+              {id: "3.2", title: "Parody in The Simpsons"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Comic series"
+            sub={[
+              {id: "4.1", title: "Comic books", sub: [
+                {id: "4.1.1", title: "Trade Paperback collections"},
+                {id: "4.1.2", title: "Sugary Shorts"},
+                {id: "4.1.3", title: "Miniseries"},
+                {id: "4.1.4", title: "Fionna and Cake comics"},
+              ]},
+              {id: "4.2", title: "Graphic novels"}
+            ]}
+          />
+        </ul>
+      </aside>
       <main className="main" id="top">
         <ul className="breadcrumb">
           <li><Link to="/">Home</Link></li>
