1e24828617f2eb19d86204bae07d9ea4c1d82221
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-13 15:01:40 -0700

    update adventuretime

--- src/pages/AdventureTime/AdventureTime.jsx
+++ src/pages/AdventureTime/AdventureTime.jsx
@@ -3,6 +3,28 @@ const AdventureTime = () =>
   return(
     <>
       <aside className="sidebar"></aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Adventure Time (franchise)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Adventure Time (franchise)</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://1000logos.net/wp-content/uploads/2020/09/Adventure-Time-Logo-1280x800.png" 
+            alt="Adventure Time logo" 
+            className="article-heading-img"
+          />
+        </div>
+        <div className="div-top">
+          <p>
+            Adventure Time is an American animated fantasy franchise created by <Link>Pendleton Ward</Link>, set in the post-apocalyptic Land
+            of Ooo.
+          </p>
+        </div>
+      </main>
     </>
   )
 }
