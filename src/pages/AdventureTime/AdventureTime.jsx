ca9a306f406739dbcb7b26f49ce7970ceb79781e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-13 16:00:13 -0700

    update AT

--- src/pages/AdventureTime/AdventureTime.jsx
+++ src/pages/AdventureTime/AdventureTime.jsx
@@ -33,13 +33,41 @@ const AdventureTime = () =>
             id="4"
             item="Comic series"
             sub={[
-              {id: "4.1", title: "Comic books", sub: [
+              {id: "4.1", title: "Comic books", sub2: [
                 {id: "4.1.1", title: "Trade Paperback collections"},
                 {id: "4.1.2", title: "Sugary Shorts"},
                 {id: "4.1.3", title: "Miniseries"},
                 {id: "4.1.4", title: "Fionna and Cake comics"},
               ]},
               {id: "4.2", title: "Graphic novels"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Video games"
+            sub={[
+              {id: "5.1", title: "Adventure Time: Hey Ice King! Why'd You Steal Our Garbage?!! (2012)"},
+              {id: "5.2", title: "Adventure Time: Explore the Dungeon Because I Don't Know! (2013)"},
+              {id: "5.3", title: "Adventure Time: The Secret of the Nameless Kingdom (2014)"},
+              {id: "5.4", title: "Adventure Time: Finn & Jake Investigations (2015)"},
+              {id: "5.5", title: "Adventure Time: Pirates of the Enchiridion (2018)"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Books"
+            sub={[
+              {id: "6.1", title: "Epic Tales from Adventure Time (2014-2016"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Merchandise"
+            sub={[
+              {id: "7.1", title: "Home release"},
+              {id: "7.2", title: "Board and card games"},
+              {id: "7.3", title: "Lego"},
+              {id: "7.4", title: "Other"},
             ]}
           />
         </ul>
