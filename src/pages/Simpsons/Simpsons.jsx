01a25fa0fb9112067f96990001f553448f03a4cc
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-14 08:57:27 -0700

    update simpsons

--- src/pages/Simpsons/Simpsons.jsx
+++ src/pages/Simpsons/Simpsons.jsx
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+
 const Simpsons = () =>
 {
   return(
     <>
       <aside className="sidebar"></aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>The Simpsons (franchise)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">The Simpsons</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/The_Simpsons_yellow_logo.svg/220px-The_Simpsons_yellow_logo.svg.png" 
+            alt="The Simpsons logo" 
+            className="article-heading-img"
+          />
+        </div>
+        <div className="div-top">
+          <p>
+            The Simpsons is an American animated comedy franchise whose eponymous family consists of <Link>Homer</Link>, <Link>Marge</Link>,
+            <Link> Bart</Link>, <Link>Lisa</Link>, and <Link>Maggie</Link>.
+          </p>
+        </div>
+      </main>
     </>
   )
 }
