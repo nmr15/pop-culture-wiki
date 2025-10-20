1993028c0755c9919791fdb36063643819c355f7
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 16:18:14 -0700

    update gtaiv

--- src/pages/Gta/GtaIV.jsx
+++ src/pages/Gta/GtaIV.jsx
@@ -7,7 +7,55 @@ const GtaIV = () =>
   return(
     <>
       <aside className="sidebar">
-
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Gameplay</a></li>
+          <Collapse 
+            id="2"
+            item="Synopsis"
+            sub={[
+              {id: "2.1", title: "Setting"},
+              {id: "2.2", title: "Plot"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Development"
+            sub={[
+              {id: "3.1", title: "Research and open world design"},
+              {id: "3.2", title: "Story and character development"},
+              {id: "3.3", title: "Art design"},
+              {id: "3.4", title: "Technical and gameplay design"},
+              {id: "3.5", title: "Music production"},
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Release"
+            sub={[
+              {id: "4.1", title: "Promotion"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Reception"
+            sub={[
+              {id: "5.1", title: "Critical response", sub2: [
+                {id: "5.1.1", title: "Windows version"}
+              ]},
+              {id: "5.2", title: "Awards"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#6">Sales</a></li>
+          <Collapse 
+            id="7"
+            item="Controversies"
+            sub={[
+              {id: "7.1", title: "Pre-release"},
+              {id: "7.2", title: "Post-release"}
+            ]}
+          />
+        </ul>
       </aside>
       <main className="main" id="top">
         <ul className="breadcrumb">
