701342d357fcce19dcac43d16a4efc4f576450b4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 14:51:47 -0700

    update gtaiv

--- src/pages/Gta/GtaIV.jsx
+++ src/pages/Gta/GtaIV.jsx
@@ -1,8 +1,31 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import TableGta from '../../components/TableGta'
+
 const GtaIV = () =>
 {
   return(
     <>
-      <div>GtaIV</div>
+      <aside className="sidebar">
+
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/gta">Grand Theft Auto (franchise)</Link></li>
+          <li>Grand Theft Auto IV</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Grand Theft Auto IV</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/Grand_Theft_Auto_IV_cover.jpg/250px-Grand_Theft_Auto_IV_cover.jpg" 
+            alt="Grand Theft Auto IV logo" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
