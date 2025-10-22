9856cb0d148e6bbb4c3c599cc8e4773c8cdc1e94
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-22 11:27:47 -0700

    update gtalcs

--- src/pages/Gta/GtaLCS.jsx
+++ src/pages/Gta/GtaLCS.jsx
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import TableGta from '../../components/TableGta'
+
 const GtaLCS = () =>
 {
   return(
     <>
-      <div>GtaLCS</div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/gta">Grand Theft Auto (franchise)</Link></li>
+          <li>Grand Theft Auto: Liberty City Stories</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Grand Theft Auto: Liberty City Stories</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/e/ea/Grand_Theft_Auto_Liberty_City_Stories_box.jpg/250px-Grand_Theft_Auto_Liberty_City_Stories_box.jpg" 
+            alt="Grand Theft Auto: Liberty City Stories logo" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
