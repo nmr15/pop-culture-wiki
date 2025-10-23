5d28e17b589c2b7ea2b59c729169c34d3c67f642
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-23 16:37:09 -0700

    update asoue

--- src/pages/Asoue/Asoue.jsx
+++ src/pages/Asoue/Asoue.jsx
@@ -1,8 +1,74 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+
 const Asoue = () =>
 {
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Background</a></li>
+          <Collapse 
+            id="2"
+            item="Series overview"
+            sub={[
+              {id: "2.1", title: "Plot"},
+              {id: "2.2", title: "Setting"},
+              {id: "2.3", title: "Characters"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Literary analysis and themes"
+            sub={[
+              {id: "3.1", title: "Allusions"},
+              {id: "3.2", title: "Genre"},
+              {id: "3.3", title: "Morality in the series"},
+              {id: "3.4", title: "Narration style"},
+              {id: "3.5", title: "Repetition"},
+              {id: "3.6", title: "Secrets in the series"},
+              {id: "3.7", title: "Clues"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Distribution"
+            sub={[
+              {id: "4.1", title: "Books"},
+              {id: "4.2", title: "Appearance"},
+              {id: "4.3", title: "Accompanying books"},
+              {id: "4.4", title: "All the Wrong Questions"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="In other media"
+            sub={[
+              {id: "5.1", title: "Television"},
+              {id: "5.2", title: "Film"},
+              {id: "5.3", title: "Video game"},
+              {id: "5.4", title: "Board games"},
+              {id: "5.5", title: "Card games"},
+              {id: "5.6", title: "Audio", sub2: [
+                {id: "5.6.1", title: "Audio books"},
+                {id: "5.6.2", title: "Album"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Reception"
+            sub={[
+              {id: "6.1", title: "Reviews"},
+              {id: "6.2", title: "Criticism"},
+              {id: "6.3", title: "Sales"},
+              {id: "6.4", title: "Awards"},
+              {id: "6.5", title: "Book lists"}
+            ]}
+          />
+        </ul>
+      </aside>
     </>
   )
 }
