4735cc1af24dad95a1524986eee993161b690fc6
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-23 14:13:50 -0700

    updated array

--- src/data.js
+++ src/data.js
@@ -2,180 +2,240 @@ export const topics = [
   {
     "id": 1,
     "title": "Marvel Cinematic Universe",
+    "file": "Mcu",
+    "folder": "Mcu",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Marvel_Cinematic_Universe_logo.png/250px-Marvel_Cinematic_Universe_logo.png",
     "route": "/mcu"
   },
   {
     "id": 2,
     "title": "Marvel Comics",
+    "file": "MarvelComics",
+    "folder": "MarvelComics",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Marvel_Comics_2024_logo.svg/220px-Marvel_Comics_2024_logo.svg.png",
     "route": "/marvelcomics"
   },
   {
     "id": 3,
     "title": "Star Wars",
+    "file": "StarWars",
+    "folder": "StarWars",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/220px-Star_wars2.svg.png",
     "route": "/starwars"
   },
   {
     "id": 4,
     "title": "DC Comics",
+    "file": "DCComics",
+    "folder": "DCComics",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/DC_Comics_2024.svg/120px-DC_Comics_2024.svg.png",
     "route": "/dccomics"
   },
   {
     "id": 5,
     "title": "Community",
+    "file": "Community",
+    "folder": "Community",
     "img": "https://oyster.ignimgs.com/mediawiki/apis.ign.com/community-tv/a/a3/Community-logo.jpg",
     "route": "/community"
   },
   {
     "id": 6,
     "title": "The Beatles",
+    "file": "Beatles",
+    "folder": "Beatles",
     "img": "https://1000logos.net/wp-content/uploads/2017/06/Beatles-Logo-768x370.png",
     "route": "/beatles"
   },
   {
     "id": 7,
     "title": "Spongebob Squarepants",
+    "file": "Spongebob",
+    "folder": "Spongebob",
     "img": "https://1000logos.net/wp-content/uploads/2020/09/SpongeBob_SquarePants_logo-1536x802.png",
     "route": "/spongebob"
   },
   {
     "id": 8,
     "title": "How I Met Your Mother",
+    "file": "Himym",
+    "folder": "Himym",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/HowIMetYourMother.svg/440px-HowIMetYourMother.svg.png",
     "route": "/himym"
   },
   {
     "id": 9,
     "title": "Grand Theft Auto",
+    "file": "Gta",
+    "folder": "Gta",
     "img": "https://1000logos.net/wp-content/uploads/2018/03/GTA-Logo.png",
     "route": "/gta"
   },
   {
     "id": 10,
     "title": "Assassin's Creed",
+    "file": "AssassinsCreed",
+    "folder": "AssassinsCreed",
     "img": "https://1000logos.net/wp-content/uploads/2017/08/Assassins-Creed-Logo-1536x966.png",
     "route": "/assassinscreed"
   },
   {
     "id": 11,
     "title": "Gravity Falls",
+    "file": "GravityFalls",
+    "folder": "GravityFalls",
     "img": "https://1000logos.net/wp-content/uploads/2021/12/Gravity-Falls-Logo-1536x864.png",
     "route": "/gravityfalls"
   },
   {
     "id": 12,
     "title": "Adventure Time",
+    "file": "AdventureTime",
+    "folder": "AdventureTime",
     "img": "https://1000logos.net/wp-content/uploads/2020/09/Adventure-Time-Logo-1280x800.png",
     "route": "/adventuretime"
   },
   {
     "id": 13,
     "title": "The Simpsons",
+    "file": "Simpsons",
+    "folder": "Simpsons",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/The_Simpsons_yellow_logo.svg/220px-The_Simpsons_yellow_logo.svg.png",
     "route": "/simpsons"
   },
   {
     "id": 14,
     "title": "Futurama",
+    "file": "Futurama",
+    "folder": "Futurama",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Futurama_1999_logo.svg/440px-Futurama_1999_logo.svg.png",
     "route": "/futurama"
   },
   {
     "id": 15,
     "title": "Pixar",
+    "file": "Pixar",
+    "folder": "Pixar",
     "img": "https://1000logos.net/wp-content/uploads/2023/01/Pixar-logo-1536x864.png",
     "route": "/pixar"
   },
   {
     "id": 16,
     "title": "Harry Potter (Books)",
+    "file": "HarryPotterBooks",
+    "folder": "HarryPotter",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Harry_Potter_%28Bloomsbury_Publishing%29_series_logo.webp/500px-Harry_Potter_%28Bloomsbury_Publishing%29_series_logo.webp.png",
     "route": "/hpbooks"
   },
   {
     "id": 16,
     "title": "Wizarding World",
+    "file": "WizardingWorld",
+    "folder": "HarryPotter",
     "img": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e4/Wizarding_World_logo.svg/500px-Wizarding_World_logo.svg.png",
     "route": "/wizardingworld"
   },
   {
     "id": 17,
     "title": "Halo",
+    "file": "Halo",
+    "folder": "Halo",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Halo_%28series%29_logo.svg/580px-Halo_%28series%29_logo.svg.png",
     "route": "/halo"
   },
   {
     "id": 18,
     "title": "Call of Duty",
+    "file": "CallofDuty",
+    "folder": "CallofDuty",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Call_of_Duty_logo_2023.svg/960px-Call_of_Duty_logo_2023.svg.png",
     "route": "/call_of_duty"
   },
   {
     "id": 19,
     "title": "Battlefield",
+    "file": "Battlefield",
+    "folder": "Battlefield",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/BattlefieldLogo.svg/500px-BattlefieldLogo.svg.png",
     "route": "/battlefield"
   },
   {
     "id": 20,
     "title": "Phineas and Ferb",
+    "file": "PhineasandFerb",
+    "folder": "PhineasandFerb",
     "img": "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Phineas_and_Ferb_logo.svg/500px-Phineas_and_Ferb_logo.svg.png",
     "route": "/phinease_and_ferb"
   },
   {
     "id": 21,
     "title": "The Amazing World of Gumball",
+    "file": "Gumball",
+    "folder": "Gumball",
     "img": "https://upload.wikimedia.org/wikipedia/en/b/b8/The_Amazing_World_of_Gumball_logo.png",
     "route": "/the_amazing_world_of_gumball"
   },
   {
     "id": 22,
     "title": "Rick and Morty",
+    "file": "RickandMorty",
+    "folder": "RickandMorty",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/500px-Rick_and_Morty.svg.png",
     "route": "/rick_and_morty"
   },
   {
     "id": 23,
     "title": "Bojack Horseman",
+    "file": "BojackHorseman",
+    "folder": "BojackHorseman",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/BoJack_Horseman_Logo.svg/500px-BoJack_Horseman_Logo.svg.png",
     "route": "/bojack_horseman"
   },
   {
     "id": 24,
     "title": "A Series of Unfortunate Events",
+    "file": "Asoue",
+    "folder": "Asoue",
     "img": "https://upload.wikimedia.org/wikipedia/commons/3/31/A_Series_of_Unfortunate_Events_Logo.jpg",
     "route": "/a_series_of_unfortunate_events"
   },
   {
     "id": 25,
     "title": "Non-MCU Spider-Man films",
+    "file": "SpidermanFilms",
+    "folder": "SpidermanFilms",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Spider-Man-Logo.svg/700px-Spider-Man-Logo.svg.png",
     "route": "/spiderman_films"
   },
   {
     "id": 26,
     "title": "X-Men films",
+    "file": "XmenFilms",
+    "folder": "XmenFilms",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo_X-Men_films.svg/500px-Logo_X-Men_films.svg.png",
     "route": "/x-men_films"
   },
   {
     "id": 27,
     "title": "Just Cause",
+    "file": "JustCause",
+    "folder": "JustCause",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Just_Cause_logo.svg/580px-Just_Cause_logo.svg.png",
     "route": "/just_cause"
   },
   {
     "id": 28,
     "title": "Red Dead",
+    "file": "RedDead",
+    "folder": "RedDead",
     "img": "https://upload.wikimedia.org/wikipedia/en/2/21/Official_Red_Dead_logo.png",
     "route": "/red_dead"
   },
   {
     "id": 29,
     "title": "The Sims",
+    "file": "TheSims",
+    "folder": "TheSims",
     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/The_Sims_series_logo.webp/500px-The_Sims_series_logo.webp.png",
     "route": "/the_sims"
   }
