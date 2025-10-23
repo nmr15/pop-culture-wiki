81b51291c607b349a22499f672cf70358fd21126
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-23 16:05:42 -0700

    new array

---
+++ src/data/dataStarWars.js
@@ -0,0 +1,212 @@
+export const starwars = [
+  {
+    "id": "starwars1",
+    "title": "Star Wars Episode IV: A New Hope",
+    "file": "ANewHope",
+    "media": "film",
+    "type": "live-action",
+    "setting": "original",
+    "img": "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
+    "route": "/starwars/a_new_hope"
+  },
+  {
+    "id": "starwars2",
+    "title": "Star Wars Episode V: The Empire Strikes Back",
+    "file": "EmpireStrikesBack",
+    "media": "film",
+    "type": "live-action",
+    "setting": "original",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/3f/The_Empire_Strikes_Back_%281980_film%29.jpg",
+    "route": "/starwars/the_empire_strikes_back"
+  },
+  {
+    "id": "starwars3",
+    "title": "Star Wars Episode VI: Return of the Jedi",
+    "file": "ReturnOfTheJedi",
+    "media": "film",
+    "type": "live-action",
+    "setting": "original",
+    "img": "https://upload.wikimedia.org/wikipedia/en/b/b2/ReturnOfTheJediPoster1983.jpg",
+    "route": "/starwars/return_of_the_jedi"
+  },
+  {
+    "id": "starwars4",
+    "title": "Star Wars Episode I: The Phantom Menace",
+    "file": "PhantomMenace",
+    "media": "film",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg",
+    "route": "/starwars/the_phantom_menace"
+  },
+  {
+    "id": "starwars5",
+    "title": "Star Wars Episode II: Attack of the Clones",
+    "file": "AttackOfTheClones",
+    "media": "film",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/32/Star_Wars_-_Episode_II_Attack_of_the_Clones_%28movie_poster%29.jpg",
+    "route": "/starwars/attack_of_the_clones"
+  },
+  {
+    "id": "starwars6",
+    "title": "Star Wars Episode III: Revenge of the Sith",
+    "file": "RevengeOfTheSith",
+    "media": "film",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/93/Star_Wars_Episode_III_Revenge_of_the_Sith_poster.jpg",
+    "route": "/starwars/revenge_of_the_sith"
+  },
+  {
+    "id": "starwars7",
+    "title": "Star Wars Episode VII: The Force Awakens",
+    "file": "ForceAwakens",
+    "media": "film",
+    "type": "live-action",
+    "setting": "sequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
+    "route": "/starwars/the_force_awakens"
+  },
+  {
+    "id": "starwars8",
+    "title": "Star Wars Episode VIII: The Last Jedi",
+    "file": "LastJedi",
+    "media": "film",
+    "type": "live-action",
+    "setting": "sequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg",
+    "route": "/starwars/the_last_jedi"
+  },
+  {
+    "id": "starwars9",
+    "title": "Star Wars Episode IX: The Rise of Skywalker",
+    "file": "RiseOfSkywalker",
+    "media": "film",
+    "type": "live-action",
+    "setting": "sequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/a/af/Star_Wars_The_Rise_of_Skywalker_poster.jpg",
+    "route": "/starwars/the_rise_of_skywalker"
+  },
+  {
+    "id": "starwars10",
+    "title": "Star Wars: The Clone Wars (film)",
+    "file": "CloneWarsFilm",
+    "media": "film",
+    "type": "animated",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/7/72/Star_wars_the_clone_wars.jpg",
+    "route": "/starwars/the_clone_wars_film"
+  },
+  {
+    "id": "starwars11",
+    "title": "Rogue One: A Star Wars Story",
+    "file": "RogueOne",
+    "media": "film",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png",
+    "route": "/starwars/rogue_one"
+  },
+  {
+    "id": "starwars12",
+    "title": "Solo: A Star Wars Story",
+    "file": "SoloFilm",
+    "media": "film",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/54/Solo_A_Star_Wars_Story_poster.jpg",
+    "route": "/starwars/solo_film"
+  },
+  {
+    "id": "starwars13",
+    "title": "Star Wars: The Clone Wars (series)",
+    "file": "CloneWarsSeries",
+    "media": "series",
+    "type": "animated",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Star_Wars_The_Clone_Wars.svg/640px-Star_Wars_The_Clone_Wars.svg.png",
+    "route": "/starwars/the_clone_wars_series"
+  },
+  {
+    "id": "starwars14",
+    "title": "Star Wars Rebels",
+    "file": "RebelsSeries",
+    "media": "series",
+    "type": "animated",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/1/16/Star_Wars_Rebels_logo.png",
+    "route": "/starwars/rebels_series"
+  },
+  {
+    "id": "starwars15",
+    "title": "Star Wars: The Bad Batch",
+    "file": "BadBatch",
+    "media": "series",
+    "type": "animated",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Star_Wars_-_The_Bad_Batch.svg/640px-Star_Wars_-_The_Bad_Batch.svg.png",
+    "route": "/starwars/the_bad_batch"
+  },
+  {
+    "id": "starwars16",
+    "title": "Star Wars Tales",
+    "file": "TalesSeries",
+    "media": "series",
+    "type": "animated",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Tales_logo.png/640px-Tales_logo.png",
+    "route": "/starwars/tales_series"
+  },
+  {
+    "id": "starwars17",
+    "title": "The Mandalorian",
+    "file": "TheMandalorian",
+    "media": "series",
+    "type": "live-action",
+    "setting": "sequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/The_Mandalorian.svg/500px-The_Mandalorian.svg.png",
+    "route": "/starwars/the_mandalorian"
+  },
+  {
+    "id": "starwars18",
+    "title": "The Book of Boba Fett",
+    "file": "BookOfBobaFett",
+    "media": "series",
+    "type": "live-action",
+    "setting": "sequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/The_Book_of_Boba_Fett.svg/640px-The_Book_of_Boba_Fett.svg.png",
+    "route": "/starwars/the_book_of_boba_fett"
+  },
+  {
+    "id": "starwars19",
+    "title": "Obi-Wan Kenobi (series)",
+    "file": "ObiWanKenobiSeries",
+    "media": "series",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Obi_Wan_Kenobi_series_logo_simplified.svg/640px-Obi_Wan_Kenobi_series_logo_simplified.svg.png",
+    "route": "/starwars/obiwan_kenobi_series"
+  },
+  {
+    "id": "starwars20",
+    "title": "Andor (series)",
+    "file": "AndorSeries",
+    "media": "series",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Andor.svg/640px-Andor.svg.png",
+    "route": "/starwars/andor_series"
+  },
+  {
+    "id": "starwars21",
+    "title": "Ahsoka (series)",
+    "file": "AhsokaSeries",
+    "media": "series",
+    "type": "live-action",
+    "setting": "prequel",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Ahsoka.svg/500px-Ahsoka.svg.png",
+    "route": "/starwars/ahsoka_series"
+  }
+]
