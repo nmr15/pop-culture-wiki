798d63eecc9128fe6501465a6f3112364432d17b
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-24 13:45:35 -0700

    new array

---
+++ src/data/dataAsoue.js
@@ -0,0 +1,93 @@
+export const asoue = [
+  {
+    "id": "asoue1",
+    "title": "The Bad Beginning",
+    "file": "BadBeginning",
+    "img": "https://upload.wikimedia.org/wikipedia/en/6/68/BadBeginning.jpg",
+    "route": "/asoue/the_bad_beginning"
+  },
+  {
+    "id": "asoue2",
+    "title": "The Reptile Room",
+    "file": "ReptileRoom",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/5a/The_Reptile_Room_USA.PNG",
+    "route": "/asoue/the_reptile_room"
+  },
+  {
+    "id": "asoue3",
+    "title": "The Wide Window",
+    "file": "WideWindow",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/06/The_Wide_Window_USA.PNG",
+    "route": "/asoue/the_wide_window"
+  },
+  {
+    "id": "asoue4",
+    "title": "The Miserable Mill",
+    "file": "MiserableMill",
+    "img": "https://upload.wikimedia.org/wikipedia/en/2/25/The_Miserable_Mill_USA.PNG",
+    "route": "/asoue/the_miserable_mill"
+  },
+  {
+    "id": "asoue5",
+    "title": "The Austere Academy",
+    "file": "AustereAcademy",
+    "img": "https://upload.wikimedia.org/wikipedia/en/b/bf/Austere_academy.jpg",
+    "route": "/asoue/the_austere_academy"
+  },
+  {
+    "id": "asoue6",
+    "title": "The Ersatz Elevator",
+    "file": "ErsatzElevator",
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d7/Ersatz_elavator.jpg",
+    "route": "/asoue/the_ersatz_elevator"
+  },
+  {
+    "id": "asoue7",
+    "title": "The Vile Village",
+    "file": "VileVillage",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/35/Vile_village.jpg",
+    "route": "/asoue/the_vile_village"
+  },
+  {
+    "id": "asoue8",
+    "title": "The Hostile Hospital",
+    "file": "HostileHospital",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/5d/Hostile_hospital.jpg",
+    "route": "/asoue/the_hostile_hospital"
+  },
+  {
+    "id": "asoue9",
+    "title": "The Carniviorous Carnival",
+    "file": "CarniverousCarneval",
+    "img": "https://upload.wikimedia.org/wikipedia/en/8/84/Carnivorous_Carnival.PNG",
+    "route": "/asoue/the_carnivorous_carnival"
+  },
+  {
+    "id": "asoue10",
+    "title": "The Slippery Slope",
+    "file": "SlipperySlope",
+    "img": "https://upload.wikimedia.org/wikipedia/en/2/21/Slipperyslope.jpg",
+    "route": "/asoue/the_slippery_slope"
+  },
+  {
+    "id": "asoue11",
+    "title": "The Grim Grotto",
+    "file": "GrimGrotto",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/92/Grimgrotto.jpg",
+    "route": "/asoue/the_grim_grotto"
+  },
+  {
+    "id": "asoue12",
+    "title": "The Penultimate Peril",
+    "file": "PenultimatePeril",
+    "img": "https://upload.wikimedia.org/wikipedia/en/8/81/ThePenultimatePeril.JPG",
+    "route": "/asoue/the_penultimate_peril"
+  },
+  {
+    "id": "asoue13",
+    "title": "The End (book)",
+    "file": "TheEndBook",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/52/TheEndBook.jpg",
+    "route": "/asoue/the_end"
+  }
+]
