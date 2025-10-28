4bee99f23f68d52966d5867941145f93641fffdc
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-28 08:34:40 -0700

    update bb

--- src/pages/Asoue/BadBeginning.jsx
+++ src/pages/Asoue/BadBeginning.jsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import TableAsoue from '../../components/TableAsoue'
+
 const BadBeginning = () =>
 {
   return(
@@ -7,6 +11,23 @@ const BadBeginning = () =>
 
         </ul>
       </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/asoue">A Series of Unfortunate Events</Link></li>
+          <li>The Bad Beginning</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">The Bad Beginning</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/6/68/BadBeginning.jpg" 
+            alt="The Bad Beginning cover" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
