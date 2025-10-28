6559783281fb3940b65bb14feb66fcd2c5532037
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-28 15:05:48 -0700

    rollback update

--- src/data/dataBeatles.js
+++ src/data/dataBeatles.js
@@ -308,6 +308,93 @@ export const beatlesSongs = [
 export const beatlesPPM = beatlesSongs.filter((ppm) => ppm.album == "Please Please Me");
 export const beatlesWTB = beatlesSongs.filter((wtb) => wtb.album == "Please Please Me");
 
+export const beatlesWtb = [
+  {
+    "id": "btlsWtb1",
+    "title": "It Won't Be Long",
+    "file": "ItWontBeLong",
+    "route": "/beatles/it_won't_be_long"
+  },
+  {
+    "id": "btlsWtb2",
+    "title": "All I've Got to Do",
+    "file": "AllIveGotToDo",
+    "route": "/beatles/all_i've_got_to_do"
+  },
+  {
+    "id": "btlsWtb3",
+    "title": "All My Loving",
+    "file": "AllMyLoving",
+    "route": "/beatles/all_my_loving"
+  },
+  {
+    "id": "btlsWtb4",
+    "title": "Don't Bother Me",
+    "file": "DontBotherMe",
+    "route": "/beatles/don't_bother_me"
+  },
+  {
+    "id": "btlsWtb5",
+    "title": "Little Child",
+    "file": "LittleChild",
+    "route": "/beatles/little_child"
+  },
+  {
+    "id": "btlsWtb6",
+    "title": "Till There Was You",
+    "file": "TillThereWasYou",
+    "route": "/beatles/till_there_was_you"
+  },
+  {
+    "id": "btlsWtb7",
+    "title": "Please Mr. Postman",
+    "file": "PleaseMrPostman",
+    "route": "/beatles/please_mr_postman"
+  },
+  {
+    "id": "btlsWtb8",
+    "title": "Roll Over Beethoven",
+    "file": "RollOverBeethoven",
+    "route": "/beatles/roll_over_beethoven"
+  },
+  {
+    "id": "btlsWtb9",
+    "title": "Hold Me Tight",
+    "file": "HoldMeTight",
+    "route": "/beatles/hold_me_tight"
+  },
+  {
+    "id": "btlsWtb10",
+    "title": "You Really Got a Hold on Me",
+    "file": "YouReallyGotAHoldOnMe",
+    "route": "/beatles/you_really_got_a_hold_on_me"
+  },
+  {
+    "id": "btlsWtb11",
+    "title": "I Wanna Be Your Man",
+    "file": "IWannaBeYourMan",
+    "route": "/beatles/i_wanna_be_your_man"
+  },
+  {
+    "id": "btlsWtb12",
+    "title": "Devil in Her Heart",
+    "file": "DevilInHerHeart",
+    "route": "/beatles/devil_in_her_heart"
+  },
+  {
+    "id": "btlsWtb13",
+    "title": "Not a Second Time",
+    "file": "NotASecondTime",
+    "route": "/beatles/not_a_second_time"
+  },
+  {
+    "id": "btlsWtb14",
+    "title": "Money (That's What I Want)",
+    "file": "MoneyTWIW",
+    "route": "/beatles/money_that's_what_i_want"
+  }
+]
+
 export const beatlesAnh = [
   {
     "id": "btlsAhn1",
