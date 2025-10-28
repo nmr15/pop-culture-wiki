f3d26f0ac78e2eddda00e7a0a8aadcb2bb947c14
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-28 09:31:51 -0700

    update bb

--- src/pages/Asoue/BadBeginning.jsx
+++ src/pages/Asoue/BadBeginning.jsx
@@ -9,7 +9,7 @@ const BadBeginning = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
-          <li className="sidebar-list-item"><a href="#1">Dear Reader</a></li>
+          <li className="sidebar-list-item"><a href="#1">Dear reader</a></li>
           <li className="sidebar-list-item"><a href="#2">Dedication</a></li>
           <li className="sidebar-list-item"><a href="#3">Plot</a></li>
           <Collapse 
@@ -68,6 +68,53 @@ const BadBeginning = () =>
             where they are treated like slaves, physically and emotionally abused, and under threats of death. They also discover he is scheming to
             steal their inherited <Link>fortune</Link> and will not hesitate to kill all three of them once he obtains it.
           </p>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="1">Dear reader</h2>
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="2">Dedication</h2>
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="3">Plot</h2>
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="4">Foreshadowing</h2>
+          <div>
+            <h3 className="article-heading-3" id="4.1">Final picture</h3>
+            <h3 className="article-heading-3" id="4.2">Letter to the editor</h3>
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="5">Word definitions</h2>
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="6">References</h2>
+          <div>
+            <h3 className="article-heading-3" id="6.1">V.F.D. references</h3>
+            <h3 className="article-heading-3" id="6.2">References to the real world</h3>
+            <h3 className="article-heading-3" id="6.3">Name allusions</h3>
+            <h3 className="article-heading-3" id="6.4">Setting allusions</h3>
+            <h3 className="article-heading-3" id="6.5">Sunny's allusions</h3>
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="7">Reception</h2>
+          <div>
+            <h3 className="article-heading-3" id="7.1">Controversy</h3>
+          </div>
         </div>
       </main>
     </>
