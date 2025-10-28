fe0bc5f6614dc1bc1cc39f2bc4963dd4abe48f28
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-28 08:50:53 -0700

    update bb

--- src/pages/Asoue/BadBeginning.jsx
+++ src/pages/Asoue/BadBeginning.jsx
@@ -8,13 +8,43 @@ const BadBeginning = () =>
     <>
       <aside className="sidebar">
         <ul className="sidebar-list">
-
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Dear Reader</a></li>
+          <li className="sidebar-list-item"><a href="#2">Dedication</a></li>
+          <li className="sidebar-list-item"><a href="#3">Plot</a></li>
+          <Collapse 
+            id="4"
+            item="Foreshadowing"
+            sub={[
+              {id: "4.1", title: "Final picture"},
+              {id: "4.2", title: "Letter to the editor"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Word definitions</a></li>
+          <Collapse 
+            id="6"
+            item="References"
+            sub={[
+              {id: "6.1", title: "V.F.D. references"},
+              {id: "6.2", title: "References to the real world"},
+              {id: "6.3", title: "Name allusions"},
+              {id: "6.4", title: "Setting allusions"},
+              {id: "6.5", title: "Sunny's allusions"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Controversy"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
         <ul className="breadcrumb">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/a_series_of_unfortunate_events">A Series of Unfortunate Events</Link></li>
+          <li><Link to="/asoue">A Series of Unfortunate Events</Link></li>
           <li>The Bad Beginning</li>
         </ul>
         <div>
