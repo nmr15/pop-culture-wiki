0c2d292c241d63fedc0e9441036d968fd09caeb7
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-04 08:47:11 -0800

    update battlefield

--- src/pages/Battlefield/Battlefield.jsx
+++ src/pages/Battlefield/Battlefield.jsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+
 const Battlefield = () =>
 {
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Gameplay</a></li>
+          <li className="sidebar-list-item"><a href="#2">Games</a></li>
+          <li className="sidebar-list-item"><a href="#3">Development History</a></li>
+          <li className="sidebar-list-item"><a href="#4">TV series</a></li>
+        </ul>
+      </aside>
     </>
   )
 }
