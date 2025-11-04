e6205910019a799223b5d527d504b056b157e761
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-04 09:02:20 -0800

    update battlefield

--- src/pages/Battlefield/Battlefield.jsx
+++ src/pages/Battlefield/Battlefield.jsx
@@ -14,6 +14,22 @@ const Battlefield = () =>
           <li className="sidebar-list-item"><a href="#4">TV series</a></li>
         </ul>
       </aside>
+      <main className="main" id="to">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Battlefield (franchise)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Battlefield (franchise)</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/BattlefieldLogo.svg/500px-BattlefieldLogo.svg.png" 
+            alt="Battlefield logo" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
