35bb37946f528896732138e13927ac6451419538
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-04 13:10:45 -0800

    new array

---
+++ src/data/dataMarvel.js
@@ -0,0 +1,233 @@
+export const marvel = [
+  {
+    "id": "marvel1",
+    "title": "Spider-Man (Peter Parker)",
+    "file": "SpiderMan",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/2/21/Web_of_Spider-Man_Vol_1_129-1.png/240px-Web_of_Spider-Man_Vol_1_129-1.png",
+    "route": "/marvelcomics/spider_man"
+  },
+  {
+    "id": "marvel2",
+    "title": "Iron Man (Tony Stark)",
+    "file": "IronMan",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Iron_Man_%28circa_2018%29.png/220px-Iron_Man_%28circa_2018%29.png",
+    "route": "/marvelcomics/iron_man"
+  },
+  {
+    "id": "marvel3",
+    "title": "Captain America (Steve Rogers)",
+    "file": "CaptainAmerica",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/CaptainAmericaHughes.jpg/220px-CaptainAmericaHughes.jpg",
+    "route": "/marvelcomics/captain_america"
+  },
+  {
+    "id": "marvel4",
+    "title": "The Hulk (Bruce Banner)",
+    "file": "TheHulk",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Hulk_%28circa_2019%29.png/220px-Hulk_%28circa_2019%29.png",
+    "route": "/marvelcomics/hulk"
+  },
+  {
+    "id": "marvel5",
+    "title": "Thor",
+    "file": "Thor",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Thor_by_Olivier_Coipel.png/220px-Thor_by_Olivier_Coipel.png",
+    "route": "/marvelcomics/thor"
+  },
+  {
+    "id": "marvel6",
+    "title": "Hawkeye (Clint Barton)",
+    "file": "Hawkeye",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/99/Hawkeye_%28Clinton_Barton%29.png",
+    "route": "/marvelcomics/hawkeye"
+  },
+  {
+    "id": "marvel7",
+    "title": "Black Widow (Natasha Romanova)",
+    "file": "BlackWidow",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Black_Widow_1.png/220px-Black_Widow_1.png",
+    "route": "/marvelcomics/black_widow"
+  },
+  {
+    "id": "marvel8",
+    "title": "Vision",
+    "file": "Vision",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/e/ea/Avengers_Vol_4_24.1.jpg/250px-Avengers_Vol_4_24.1.jpg",
+    "route": "/marvelcomics/vision"
+  },
+  {
+    "id": "marvel9",
+    "title": "Ant-Man (Hank Pym)",
+    "file": "AntMan",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/Hank_pym_1.jpg/220px-Hank_pym_1.jpg",
+    "route": "/marvelcomics/ant_man"
+  },
+  {
+    "id": "marvel10",
+    "title": "Wasp (Janet van Dyne)",
+    "file": "Wasp",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/AVEN071.jpg/220px-AVEN071.jpg",
+    "route": "/marvelcomics/wasp"
+  },
+  {
+    "id": "marvel11",
+    "title": "Black Panther (T'Challa)",
+    "file": "BlackPanther",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Black_Panther_OS_Vol_1_2.png/220px-Black_Panther_OS_Vol_1_2.png",
+    "route": "/marvelcomics/black_panther"
+  },
+  {
+    "id": "marvel12",
+    "title": "Captain Marvel (Carol Danvers)",
+    "file": "CaptainMarvel",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Carol_Danvers_-_%28evolution%29.jpg/220px-Carol_Danvers_-_%28evolution%29.jpg",
+    "route": "/marvelcomics/captain_marvel"
+  },
+  {
+    "id": "marvel13",
+    "title": "Mister Fantastic (Reed Richards)",
+    "file": "MisterFantastic",
+    "img": "https://upload.wikimedia.org/wikipedia/en/b/ba/Reedr.jpg",
+    "route": "/marvelcomics/mister_fantastic"
+  },
+  {
+    "id": "marvel14",
+    "title": "Invisible Woman (Susan Storm)",
+    "file": "InvisibleWoman",
+    "img": "https://upload.wikimedia.org/wikipedia/en/e/e4/Invisible_Woman.png",
+    "route": "/marvelcomics/invisible_woman"
+  },
+  {
+    "id": "marvel15",
+    "title": "The Human Torch (Johnny Storm)",
+    "file": "HumanTorch",
+    "img": "https://upload.wikimedia.org/wikipedia/en/c/c7/Human_Torch_%28Johnny_Storm%29.png",
+    "route": "/marvelcomics/human_torch"
+  },
+  {
+    "id": "marvel16",
+    "title": "The Thing (Ben Grimm)",
+    "file": "Thing",
+    "img": "https://upload.wikimedia.org/wikipedia/en/2/28/Thing_Dark_Reign_Fantastic_Four.png",
+    "route": "/marvelcomics/thing"
+  },
+  {
+    "id": "marvel17",
+    "title": "Wolverine (James Howlett)",
+    "file": "Wolverine",
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d3/Wolverine_%28circa_2024%29.jpg",
+    "route": "/marvelcomics/wolverine"
+  },
+  {
+    "id": "marvel18",
+    "title": "Cyclops (Scott Summers)",
+    "file": "Cyclops",
+    "img": "https://upload.wikimedia.org/wikipedia/en/e/e9/Cyclops_%28Scott_Summers_circa_2019%29.png",
+    "route": "/marvelcomics/cyclops"
+  },
+  {
+    "id": "marvel19",
+    "title": "Professor X (Charles Xavier)",
+    "file": "ProfessorX",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/9e/Professor_X%2C_New_Avengers%2C_Illuminati.jpg",
+    "route": "/marvelcomics/professor_x"
+  },
+  {
+    "id": "marvel20",
+    "title": "Jean Grey",
+    "file": "JeanGrey",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Jean_Grey_%28Modern%29.webp/256px-Jean_Grey_%28Modern%29.webp.png",
+    "route": "/marvelcomics/jean_grey"
+  },
+  {
+    "id": "marvel21",
+    "title": "Iceman (Bobby Drake)",
+    "file": "Iceman",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/03/Iceman.png",
+    "route": "/marvelcomics/iceman"
+  },
+  {
+    "id": "marvel22",
+    "title": "Nightcrawler (Kurt Wagner)",
+    "file": "Nightcrawler",
+    "img": "https://upload.wikimedia.org/wikipedia/en/7/7b/Nightcrawler_%28Kurt_Wagner_circa_2018%29.png",
+    "route": "/marvelcomics/nightcrawler"
+  },
+  {
+    "id": "marvel23",
+    "title": "Beast (Hank McCoy)",
+    "file": "Beast",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/0b/Beast_%28Hank_McCoy_-_circa_2019%29.png",
+    "route": "/marvelcomics/beast"
+  },
+  {
+    "id": "marvel24",
+    "title": "Storm (Ororo Munroe)",
+    "file": "Storm",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/34/Storm_%28Ororo_Munroe%29.png",
+    "route": "/marvelcomics/storm"
+  },
+  {
+    "id": "marvel25",
+    "title": "Shadowcat (Kitty Pryde)",
+    "file": "Shadowcat",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/3c/Kitty_Pryde_%28Marvel_Comics_character%29.png",
+    "route": "/marvelcomics/shadowcat"
+  },
+  {
+    "id": "marvel26",
+    "title": "Rogue",
+    "file": "Rogue",
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d3/Excalibur_2019_-18.jpeg",
+    "route": "/marvelcomics/rogue"
+  },
+  {
+    "id": "marvel27",
+    "title": "Gambit (Remy LeBeau)",
+    "file": "Gambit",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/94/Gambit_%28Marvel_Comics%29.png",
+    "route": "/marvelcomics/gambit"
+  },
+  {
+    "id": "marvel28",
+    "title": "Deadpool (Wade Wilson)",
+    "file": "Deadpool",
+    "img": "https://upload.wikimedia.org/wikipedia/en/c/ca/Deadpool.png",
+    "route": "/marvelcomics/deadpool"
+  },
+  {
+    "id": "marvel29",
+    "title": "Daredevil (Matt Murdock)",
+    "file": "Daredevil",
+    "img": "https://upload.wikimedia.org/wikipedia/en/1/14/Daredevil_65.jpg",
+    "route": "/marvelcomics/daredevil"
+  },
+  {
+    "id": "marvel30",
+    "title": "Elektra Natchios",
+    "file": "Elektra",
+    "img": "https://upload.wikimedia.org/wikipedia/en/a/af/Elektra_3.jpg",
+    "route": "/marvelcomics/elektra"
+  },
+  {
+    "id": "marvel31",
+    "title": "Luke Cage",
+    "file": "LukeCage",
+    "img": "https://upload.wikimedia.org/wikipedia/en/f/f9/Luke_Cage_by_Stuart_Immonen.png",
+    "route": "/marvelcomics/luke_cage"
+  },
+  {
+    "id": "marvel32",
+    "title": "Iron Fist (Danny Rand)",
+    "file": "IronFist",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/00/Iron_Fist_%28Danny_Rand%29.jpg",
+    "route": "/marvelcomics/iron_fist"
+  },
+  {
+    "id": "marvel33",
+    "title": "Jessica Jones",
+    "file": "JessicaJones",
+    "img": "https://upload.wikimedia.org/wikipedia/en/6/6e/Jessica_Jones_by_Mike_Mayhew.jpg",
+    "route": "/marvelcomics/jessica_jones"
+  }
+]
