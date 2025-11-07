418899a7cf675807b26f02406245d3d5dc214404
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-07 15:11:15 -0800

    update spongebob

--- src/pages/Spongebob/Spongebob.jsx
+++ src/pages/Spongebob/Spongebob.jsx
@@ -1,31 +1,62 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+
 const Spongebob = () => 
 {
   return (
     <>
-      <nav className="sidebar bg-dark">
-        <ul>
-          <li className="py-1">
-            <a href="#top">Top</a>
-          </li>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Development"
+            sub={[
+              {id: "1.1", title: "Early inspirations"},
+              {id: "1.2", title: "Conception"},
+              {id: "1.3", title: "Assembling the crew"},
+              {id: "1.4", title: "Pitching"}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Television series"
+            sub={[
+              {id: "2.1", title: "Spongebob Squarepants (1999-present)"},
+              {id: "2.2", title: "Kamp Koral!: Spongebob's Under Years (2021-2024)"},
+              {id: "2.3", title: "The Patrick Star Show (2021-present)"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Films"
+            sub={[
+              {id: "4.1", title: "The Spongebob Squarepants Movie (2004)"},
+              {id: "4.2", title: "The Spongebob Movie: Sponge Out of Water (2015)"},
+              {id: "4.3", title: "The Spongebob Movie: Sponge on the Run (2020)"},
+              {id: "4.4", title: "In development", sub2: [
+                {id: "4.4.1", title: "The Spongebob Movie: Search for Squarepants (2025)"}
+              ]},
+              {id: "4.5", title: "Spin-off films"},
+              {id: "4.6", title: "Fans' short films"},
+            ]}
+          />
         </ul>
-      </nav>
-      <main className="main px-5 py-4">
-        <div id="top">
-          <h1 className="article-heading">Spongebob Squarepants</h1>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Spongebob Squarepants (franchise)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Spongebob Squarepants (franchise)</h1>
         </div>
-        <div className="img-right pt-2">
+        <div className="img-right img-top">
           <img 
-            src="https://1000logos.net/wp-content/uploads/2020/09/SpongeBob_SquarePants_logo-1536x802.png" 
-            alt="Spongebob Squarepants" 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/220px-Star_wars2.svg.png" 
+            alt="Spongebob Squarepants logo"
             className="article-heading-img"
           />
-        </div>
-        <div className="pt-2">
-          <p>
-            <b>Spongebob Squarepants</b> is an American animated television series created by marine science educator and animator <a href="#">Stephen Hillenburg</a>
-            for <a href="#">Nickelodeon</a>. It first aired as a sneak peek after the 1999 Kids' Choice Awards on May 1, 1999, and officially premiered on
-            July 17, 1999. It chronicles the adventures of <a href="#">Spongebob Squarepants</a> and his aquatic friends in the underwater city of <a href="#">Bikini Bottom</a>.
-          </p>
         </div>
       </main>
     </>
