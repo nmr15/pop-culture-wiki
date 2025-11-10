3e9ee82746d3e76ea4765bdb4263cb665fcc9907
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-10 10:56:12 -0800

    update spidermanfilms

--- src/pages/SpidermanFilms/SpidermanFilms.jsx
+++ src/pages/SpidermanFilms/SpidermanFilms.jsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom'
+import Card from '../../components/Card'
+import { spidermanFilms } from '../../data/dataSpidermanFilms'
+
 const SpidermanFilms = () =>
 {
   return(
@@ -15,6 +19,21 @@ const SpidermanFilms = () =>
         <div>
           <h1 className="article-heading">Spider-Man films</h1>
         </div>
+        <div className="div-top">
+                    <div className="row">
+                        {spidermanFilms.map((spfilm) => (
+                            <div className="col-3" key={spfilm.id}>
+                                <Card 
+                                    route={spfilm.route}
+                                    img={spfilm.img}
+                                    alt={spfilm.title}
+                                    imgStyle="card-img-vertical"
+                                    title={spfilm.title}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
       </main>
     </>
   )
