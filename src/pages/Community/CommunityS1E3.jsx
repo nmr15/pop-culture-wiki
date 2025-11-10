43063bd3e16d1735f1d9c4e6155107a9c413e47c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-10 13:46:18 -0800

    update comms1e3

--- src/pages/Community/CommunityS1E3.jsx
+++ src/pages/Community/CommunityS1E3.jsx
@@ -38,6 +38,48 @@ const CommunityS1E3 = () =>
             reviews and garnered 5.86 million viewers upon its premiere.
           </p>
         </div>
+        <div>
+          <h2 className="article-heading-2" id="1">Plot</h2>
+          <div>
+            <p>
+              Jeff attends an accounting class taught by <Link>Professor Whitman</Link> which is less about learning and more about the philosophy
+              of "seizing the day". Over in <Link>the study room</Link>, the <Link>group</Link> is irritated at
+              <Link to="/community/pierce_hawthorne"> Pierce</Link> who cannot operate his cell phone. Jeff then arrives excited about the blow off
+              class he just found and suggests the group sign up too. <Link to="/community/abed_nadir">Abed</Link> declines, explaining that his dad
+              forbids him taking any class that won't help him run the family falafel business. <Link to="/community/britta_perry">Britta</Link>
+              decides to give Abed the money to pay for a film class he actually wants to take. He suggests that they all get back to studying but is
+              subsequently distracted by <Link to="/community/troy_barnes">Troy</Link>'s funny sneeze.
+            </p>
+            <div className="img-right">
+              <img src="https://static.wikia.nocookie.net/community-sitcom/images/f/fc/Seize_the_day%21.png/revision/latest/scale-to-width-down/300?cb=20120523174602" 
+              alt="Seize the day" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="2">End tag</h2>
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="3">Themes</h2>
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="4">Trivia</h2>
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <h2 className="article-heading-2" id="5">Quotes</h2>
+          <div>
+            
+          </div>
+        </div>
       </main>
     </>
   )
