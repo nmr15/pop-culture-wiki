b2e4a9487518f9e90a50653d7b530b46dbe80462
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-10 15:40:46 -0800

    update comms1e3

--- src/pages/Community/CommunityS1E3.jsx
+++ src/pages/Community/CommunityS1E3.jsx
@@ -41,6 +41,12 @@ const CommunityS1E3 = () =>
         <div>
           <h2 className="article-heading-2" id="1">Plot</h2>
           <div>
+            <div className="img-right">
+              <img 
+                src="https://static.wikia.nocookie.net/community-sitcom/images/f/fc/Seize_the_day%21.png" 
+                alt="Seize the day" 
+              />
+            </div>
             <p>
               Jeff attends an accounting class taught by <Link>Professor Whitman</Link> which is less about learning and more about the philosophy
               of "seizing the day". Over in <Link>the study room</Link>, the <Link>group</Link> is irritated at
@@ -50,15 +56,15 @@ const CommunityS1E3 = () =>
               decides to give Abed the money to pay for a film class he actually wants to take. He suggests that they all get back to studying but is
               subsequently distracted by <Link to="/community/troy_barnes">Troy</Link>'s funny sneeze.
             </p>
-            <div className="img-right">
+            <div className="img-left">
               <img 
-                src="https://static.wikia.nocookie.net/community-sitcom/images/f/fc/Seize_the_day%21.png" 
-                alt="Seize the day" 
+                src="https://static.wikia.nocookie.net/community-sitcom/images/7/7f/ITF_Living_in_the_moment_by_standing_on_desks.png" 
+                alt="Living_in_the_moment_by_standing_on_desks" 
               />
             </div>
             <p>
               The study group, minus Britta and Abed, join Jeff for Professor Whitman's accounting course the following day. After
-              <Link to="/community/shirley_bennett">Shirley</Link> introduces herself to the class, Whitman encourages her to have an emotional
+              <Link to="/community/shirley_bennett"> Shirley</Link> introduces herself to the class, Whitman encourages her to have an emotional
               breakthrough regarding her husband. He then commands Annie along with the rest of the students to stand on their desks. After one of the
               students gets injured when her desk collapses, Whitman gives out their homework assignments and dismisses them. He asks that Jeff stay behind
               and hells him he believes his motivations for taking the class are insincere. If by the end of the week he doesn't try to genuinely "live in
@@ -66,10 +72,31 @@ const CommunityS1E3 = () =>
             </p>
             <div className="img-right">
               <img 
-                src="https://static.wikia.nocookie.net/community-sitcom/images/7/7f/ITF_Living_in_the_moment_by_standing_on_desks.png" 
-                alt="Living_in_the_moment_by_standing_on_desks" 
+                src="https://static.wikia.nocookie.net/community-sitcom/images/d/de/ITF_it_was_difficult_talking_to_him_before_now_wehave_this_between_us.png" 
+                alt="it_was_difficult_talking_to_him_before_now_wehave_this_between_us" 
               />
             </div>
+            <p>
+              She asks him how his class is going, and he mentions that he is working on a documentary. It is then that <Link>Abed's father</Link> shows
+              up and confronts Britta. He tells her that he is responsible for Abed and doesn't need her help raising him. The two begin to argue, and
+              when it becomes physical, Jeff steps in to separate them. Jeff tells Mr. Nadir that Abed is an adult now and can make his own decisions.
+              Fed up with the situation, Abed's father respons that Abed is their responsibility now and leaves the campus. The next day in the
+              <Link> cafeteria</Link>, Britta runs into Jeff who is dressed in a ridicoulous outfit and waiting for Whitman to arrive for his morning
+              coffee.
+            </p>
+            <div className="img-left">
+              <img 
+                src="https://static.wikia.nocookie.net/community-sitcom/images/a/a3/ITF_Shazbot.png" 
+                alt="Shazbot" 
+              />
+            </div>
+            <p>
+              Britta has accepted the role of Abed's provider and is trying to calculate his expenses for the semester. Abed arrives with an expensive
+              new camera bought with Britta's money. He continues to film his documentary, casting Jeff and Britta in the roles of his parents. Distracted
+              by Abed's wasteful spending. Jeff fails to notice Whitman's arrival. The professor disapproves of Jeff's outfit before ordering a birthday
+              cake from the <Link>Hot & Brown</Link> coffee shop. Meanwhile, in the <Link>student lounge</Link>, Pierce offers to teach Troy how to sneeze
+              strongly and with intent. Elsewhere, Jeff concots a more elaborate scheme to impress Whitman.
+            </p>
           </div>
         </div>
         <div>
