a2501bd8f106c869e59a1553de47050f9ed1d3e5
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-11 13:55:45 -0800

    new array

---
+++ src/data/dataGravityFalls.js
@@ -0,0 +1,402 @@
+export const gfEpisodes = [
+  {
+    "id": "gfs1e1",
+    "title": "Tourist Trapped",
+    "season": 1,
+    "episode": 1,
+    "file": "GFS1E1",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/a/a1/S1e1_Book_3_eyepiece_2.png",
+    "route": "/gravity_falls/tourist_trapped",
+    "desc": "Young twins Dipper and Mabel Pines arrive to spend their summer break with their Great Uncle Stan, aka Grunkle Stan, in the remote town of Gravity Falls."
+  },
+  {
+    "id": "gfs1e2",
+    "title": "The Legend of the Gobblewonker",
+    "season": 1,
+    "episode": 2,
+    "file": "GFS1E2",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/d/da/S1e2_dipper_pines_taking_picture.png",
+    "route": "/gravity_falls/the_legend_of_the_gobberwonkle",
+    "desc": "When Dipper and Mabel hear rumors that a sea monster lives at the bottom of the local lake , they enlist Soos to take them on a boating expedition to prove it really does exist."
+  },
+  {
+    "id": "gfs1e3",
+    "title": "Headhunters",
+    "season": 1,
+    "episode": 3,
+    "file": "GFS1E3",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/8/8b/S1e3_dipper_holding_3.png/revision/latest?cb=20120704081627",
+    "route": "/gravity_falls/headhunters",
+    "desc": "When someone tampers with Mabel's life-size wax statue, she and Dipper set out to find the culprit. Meanwhile, Grunkle Stan mourns over the loss of his wax doppelganger."
+  },
+  {
+    "id": "gfs1e4",
+    "title": "The Hand That Rocks the Mabel",
+    "season": 1,
+    "episode": 4,
+    "file": "GFS1E4",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/d/de/S1e4_burning_picture.png",
+    "route": "/gravity_falls/the_hand_that_rocks_the_mabel",
+    "desc": "When Grunkle Stan convinces Mabel to go on a date with their seemingly cherubic new neighbor, Gideon Gleeful, to question his authenticity as a psychic, she soon learns that his cuteness can immediately vanish."
+  },
+  {
+    "id": "gfs1e5",
+    "title": "The Inconveniencing",
+    "season": 1,
+    "episode": 5,
+    "file": "GFS1E5",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/4/47/S1e5_behold_the_body_outlines_of_Ma_and_Pa.png",
+    "route": "/gravity_falls/the_inconveniencing",
+    "desc": "When Dipper tries to impress Wendy by acting older than he really is, he's invited to join a group of teenagers on a trek that leads to an old abandoned convenience store rumored to be haunted."
+  },
+  {
+    "id": "gfs1e6",
+    "title": "Dipper vs. Maliness",
+    "season": 1,
+    "episode": 6,
+    "file": "GFS1E6",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/1/19/S1e6_dipper_spear.png",
+    "route": "/gravity_falls/dipper_vs_manliness",
+    "desc": "In a quest to attain manliness, Dipper ventures into the forest to battle the notorious Multi-Bear. Meanwhile, Mabel tries to teach Grunkle Stan how to become an attractive man to women, particularly Lazy Susan."
+  },
+  {
+    "id": "gfs1e7",
+    "title": "Double Dipper",
+    "season": 1,
+    "episode": 7,
+    "file": "GFS1E7",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/0/0c/S1e7_dipper_meets_tyrone.png",
+    "route": "/gravity_falls/double_dipper",
+    "desc": "When Stan throws a party, Dipper uses an off-kiltter copy machine to make clones of himself so he can impress Wendy. Meanwhile, Mabel stands up for her friends, and  she fights for the Party Queen title in their honor."
+  },
+  {
+    "id": "gfs1e8",
+    "title": "Irrational Treasure",
+    "season": 1,
+    "episode": 8,
+    "file": "GFS1E8",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/c/cc/S1e8_dipper_mabel_hat.png",
+    "route": "/gravity_falls/irrational_treasure",
+    "desc": "When Dipper and Mabel discover evidence that the reported town dounder is a hoax, they set out to expose the historical cover-up and to prove that Mabel's silliness is not just an embarrassing personality trait."
+  },
+  {
+    "id": "gfs1e9",
+    "title": "The Time Traveler's Pig",
+    "season": 1,
+    "episode": 9,
+    "file": "GFS1E9",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/6/62/S1e9_the_gang_going_in_time.png",
+    "route": "/gravity_falls/the_time_travelers_pig",
+    "desc": "When Dipper wishes he could go back in time and undo a mistake, he discovers that there is a real time machine for his upper advantages. Meanwhile, Mabel wins a pet pig, which she names Waddles, at the fair."
+  },
+  {
+    "id": "gfs1e10",
+    "title": "Fight Fighters",
+    "season": 1,
+    "episode": 10,
+    "file": "GFS1E10",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/1/1a/S1e10_dipper_and_rumble_face_to_face.png",
+    "route": "/gravity_falls/fight_fighters",
+    "desc": "After being challenged to a video game battle by Wendy's boyfriend Robbie, Dipper is shocked when his favorite playable fighter, Rumble McSkirmish, comes to life to defend him. However, Dipper loses control over Rumble, and has to find a way to stop him."
+  },
+  {
+    "id": "gfs1e11",
+    "title": "Little Dipper",
+    "season": 1,
+    "episode": 11,
+    "file": "GFS1E11",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/7/71/S1e11_tiny_twins.png",
+    "route": "/gravity_falls/little_dipper",
+    "desc": "Dipper feels self-conscious that Mabel is taller than him, and she is suddenly becoming the alpha twin. So, he seeks out a magical way to grow taller. However, his plan backfires tremendously."
+  },
+  {
+    "id": "gfs1e12",
+    "title": "Summerween",
+    "season": 1,
+    "episode": 12,
+    "file": "GFS1E12",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/1/1d/S1e12_knock_knock.png",
+    "route": "/gravity_falls/summerween",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e13",
+    "title": "Boss Mabel",
+    "season": 1,
+    "episode": 13,
+    "file": "GFS1E13",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/6/6f/S1e13_Boss_Mabel.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e14",
+    "title": "Bottomless Pit",
+    "season": 1,
+    "episode": 14,
+    "file": "GFS1E14",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/0/05/S1e14_falling_into_the_pit.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e15",
+    "title": "The Deep End",
+    "season": 1,
+    "episode": 15,
+    "file": "GFS1E15",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/e/e5/S1e15_free_willy_ref.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e16",
+    "title": "Carpet Diem",
+    "season": 1,
+    "episode": 16,
+    "file": "GFS1E16",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/4/44/S1e16_body_swapped.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e17",
+    "title": "Boyz Crazy",
+    "season": 1,
+    "episode": 17,
+    "file": "GFS1E17",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/8/8f/S1e17_brain_bout_to_explode.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e18",
+    "title": "Land Before Swine",
+    "season": 1,
+    "episode": 18,
+    "file": "GFS1E18",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/6/68/S1e18_what%27s_that.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e19",
+    "title": "Dreamscapers",
+    "season": 1,
+    "episode": 19,
+    "file": "GFS1E19",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/3/3e/S1e19_holy_heck_its_bill.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs1e20",
+    "title": "Gideon Rises",
+    "season": 1,
+    "episode": 20,
+    "file": "GFS1E20",
+    "img": "https://static.wikia.nocookie.net/gravityfalls/images/5/53/S1e20_Ultimate_Gideon.png",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e1",
+    "title": "Scary-oke",
+    "season": 2,
+    "episode": 21,
+    "file": "GFS2E1",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e2",
+    "title": "Into the Bunker",
+    "season": 2,
+    "episode": 22,
+    "file": "GFS2E2",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e3",
+    "title": "The Golf War",
+    "season": 2,
+    "episode": 23,
+    "file": "GFS2E3",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e4",
+    "title": "Sock Opera",
+    "season": 2,
+    "episode": 24,
+    "file": "GFS2E4",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e5",
+    "title": "Soos and the Real Girl",
+    "season": 2,
+    "episode": 25,
+    "file": "GFS2E5",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e6",
+    "title": "Little Gift Shop of Horrors",
+    "season": 2,
+    "episode": 26,
+    "file": "GFS2E6",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e7",
+    "title": "Society of the Blind Eye",
+    "season": 2,
+    "episode": 27,
+    "file": "GFS2E7",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e8",
+    "title": "Blendin's Game",
+    "season": 2,
+    "episode": 28,
+    "file": "GFS2E8",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e9",
+    "title": "The Love God",
+    "season": 2,
+    "episode": 29,
+    "file": "GFS2E9",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e10",
+    "title": "Northwest Mansion Mystery",
+    "season": 2,
+    "episode": 30,
+    "file": "GFS2E10",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e11",
+    "title": "Now What He Seems",
+    "season": 2,
+    "episode": 31,
+    "file": "GFS2E11",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e12",
+    "title": "A Tale of Two Stans",
+    "season": 2,
+    "episode": 32,
+    "file": "GFS2E12",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e13",
+    "title": "Dungeons, Dungeons & More Dungeons",
+    "season": 2,
+    "episode": 33,
+    "file": "GFS2E13",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e14",
+    "title": "The Stanchurian Candidate",
+    "season": 2,
+    "episode": 34,
+    "file": "GFS2E14",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e15",
+    "title": "The Last Mabelcorn",
+    "season": 2,
+    "episode": 35,
+    "file": "GFS2E15",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e16",
+    "title": "Roadside Attraction",
+    "season": 2,
+    "episode": 36,
+    "file": "GFS2E16",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e17",
+    "title": "Dipper and Mabel vs. the Future",
+    "season": 2,
+    "episode": 37,
+    "file": "GFS2E17",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e18",
+    "title": "Weirdmageddon Part 1",
+    "season": 2,
+    "episode": 38,
+    "file": "GFS2E18",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e19",
+    "title": "Weirdmageddon 2: Escape from Reality",
+    "season": 2,
+    "episode": 39,
+    "file": "GFS2E19",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  },
+  {
+    "id": "gfs2e20",
+    "title": "Weirdmageddon 3: Take Back the Falls",
+    "season": 2,
+    "episode": 40,
+    "file": "GFS2E20",
+    "img": "",
+    "route": "/gravity_falls/",
+    "desc": ""
+  }
+]
