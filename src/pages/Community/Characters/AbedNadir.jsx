0b90aaa92409bdfd413db50b231b6c6c8c87ce06
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-12 08:08:16 -0800

    new folders

--- src/pages/Community/Characters/AbedNadir.jsx
+++ src/pages/Community/Characters/AbedNadir.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import TableCommunity from '../../components/TableCommunity'
+import TableCommunity from '../../../components/TableCommunity'
 
 const AbedNadir = () =>
 {
