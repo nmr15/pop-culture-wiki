de432d1ba020947ccf8d77c8254c0eec88917290
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-13 09:52:28 -0800

    update Halo

--- src/pages/Halo/Halo.jsx
+++ src/pages/Halo/Halo.jsx
@@ -1,8 +1,31 @@
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+
 const Halo = () =>
 {
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Halo (franchise)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Halo (franchise)</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Halo_%28series%29_logo.svg/580px-Halo_%28series%29_logo.svg.png" 
+            alt="Halo logo" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
