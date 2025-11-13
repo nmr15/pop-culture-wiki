d4b9f0eab471b0b8ac5c79435ba71538428f0d88
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-13 11:29:49 -0800

    new array

---
+++ src/data/dataHarryPotter.js
@@ -0,0 +1,51 @@
+export const hpbooks = [
+  {
+    "id": "hpbook1",
+    "title": "Harry Potter and the Philosopher's Stone",
+    "file": "PhilosophersStoneBook",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/3/3e/Harry_Potter_and_the_Sorcerer%27s_Stone_cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_philosophers_stone"
+  },
+  {
+    "id": "hpbook2",
+    "title": "Harry Potter and the Chamber of Secrets",
+    "file": "ChamberOfSecretsBook",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/e/e0/Chamber_of_secrets-cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_chamber_of_secrets"
+  },
+  {
+    "id": "hpbook3",
+    "title": "Harry Potter and the Prisoner of Azkaban",
+    "file": "PrisonerOfAzkaban",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/1/11/Prisoner_of_Azkaban_cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_prisoner_of_azkaban"
+  },
+  {
+    "id": "hpbook4",
+    "title": "Harry Potter and the Goblet of Fire",
+    "file": "GobletOfFireBook",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/d/d9/Goblet_fire_cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_goblet_of_fire"
+  },
+  {
+    "id": "hpbook5",
+    "title": "Harry Potter and the Order of the Phoenix",
+    "file": "OrderOfThePhoenixBook",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/d/d3/OOTP_US_cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_order_of_the_phoenix"
+  },
+  {
+    "id": "hpbook6",
+    "title": "Harry Potter and the Half-Blood Prince",
+    "file": "HalfBloodPrince",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/c/c6/Harry_potter_HBP_Scholastic_edition.jpg",
+    "route": "/harry_potter/harry_potter_and_the_half_blood_prince"
+  },
+  {
+    "id": "hpbook7",
+    "title": "Harry Potter and the Deathly Hallows",
+    "file": "DeathlyHallowsBook",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/1/14/Harry_Potter_and_the_Deathly_Hallows_US_cover.jpg",
+    "route": "/harry_potter/harry_potter_and_the_deathly_hallows"
+  }
+]
