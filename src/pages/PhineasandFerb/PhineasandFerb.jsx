cc2f03e22c34856a7a20c8d5476296e31569a3e3
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-20 13:23:14 -0800

    update pf

--- src/pages/PhineasandFerb/PhineasandFerb.jsx
+++ src/pages/PhineasandFerb/PhineasandFerb.jsx
@@ -5,7 +5,27 @@ const PhineasandFerb = () =>
 {
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Phineas and Ferb</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Phineas and Ferb</h1>
+        </div>
+        <div className="img-right img-top">
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Phineas_and_Ferb_logo.svg/500px-Phineas_and_Ferb_logo.svg.png" 
+            alt="Phineas and Ferb logo" 
+            className="article-heading-img"
+          />
+        </div>
+      </main>
     </>
   )
 }
