3de996460a3062230a7fce89eae8bc940aed0380
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-22 10:29:28 -0800

    Create GFS1E9.jsx

---
+++ src/pages/GravityFalls/Season1/GFS1E9.jsx
@@ -0,0 +1,15 @@
+const GFS1E9 = () =>
+{
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list"></ul>
+      </aside>
+      <main className="main" id="top">
+        
+      </main>
+    </>
+  )
+}
+
+export default GFS1E9;
