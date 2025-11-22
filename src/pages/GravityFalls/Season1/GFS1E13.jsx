2d2afb41d9cb51bd0c38e3bba98035bca7d0ce48
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-11-22 10:35:49 -0800

    Update GFS1E13.jsx

--- src/pages/GravityFalls/Season1/GFS1E13.jsx
+++ src/pages/GravityFalls/Season1/GFS1E13.jsx
@@ -1,1 +1,16 @@
 
+const GFS1E13 = () =>
+{
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list"></ul>
+      </aside>
+      <main className="main" id="top">
+        
+      </main>
+    </>
+  )
+}
+
+export default GFS1E13;
