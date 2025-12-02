cce274b4a52e64c4fdabecb80fa96f5131c49ed5
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-02 14:52:01 -0800

    updta ca

--- src/pages/Mcu/Films/CaptainAmericaTheFirstAvenger.jsx
+++ src/pages/Mcu/Films/CaptainAmericaTheFirstAvenger.jsx
@@ -8,9 +8,162 @@ const CaptainAmericaTheFirstAvenger = () =>
 {
   return(
     <>
-      <div>
-        <h1>CaptainAmericaTheFirstAvenger</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="8"
+            item="Future"
+            sub={[
+              {id: "8.1", title: "Sequels", sub2: [
+                {id: "8.1.1", title: "Captain America: The Winter Soldier"},
+                {id: "8.1.2", title: "Captain America: Civil War"}
+              ]},
+              {id: "8.2", title: "Marvel One-Shot"}
+            ]}
+          />
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Captain America: The First Avenger</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Captain America: The First Avenger</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Captain America: The First Avenger</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg" alt="Captain America: The First Avenger poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>Joe Johnston</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/thor_film">Thor</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="mcu/the_avengers_film">The Avengers</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+
+        </div>
+        <div>
+          <Heading2 id={1} title={"Plot"} />
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <Heading2 id={2} title={"Cast"} />
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <Heading2 id={3} title={"Production"} />
+          <div>
+            <h3 className="article-heading-3" id="3.1">Development</h3>
+            <h3 className="article-heading-3" id="3.2">Pre-production</h3>
+            <h3 className="article-heading-3" id="3.3">Filming</h3>
+            <h3 className="article-heading-3" id="3.4">Post-production</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={4} title={"Music"} />
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <Heading2 id={5} title={"Marketing"} />
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <Heading2 id={6} title={"Release"} />
+          <div>
+            <h3 className="article-heading-3" id="6.1">Theatrical</h3>
+            <h3 className="article-heading-3" id="6.2">Home media</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={7} title={"Reception"} />
+          <div>
+            <h3 className="article-heading-3" id="7.1">Box office</h3>
+            <h3 className="article-heading-3" id="7.2">Critical response</h3>
+            <h3 className="article-heading-3" id="7.3">Accolades</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={8} title={"Future"} />
+          <div>
+            <h3 className="article-heading-3" id="8.1">Sequels</h3>
+            <h4 className="article-heading-4" id="8.1.1">Captain America: The Winter Soldier</h4>
+            <h4 className="article-heading-4" id="8.1.2">Captain America: Civil War</h4>
+            <h3 className="article-heading-3" id="8.1">Marvel One-Shot</h3>
+          </div>
+        </div>
+        <div>
+          <TableMcu />
+        </div>
+      </main>
     </>
   )
 }
