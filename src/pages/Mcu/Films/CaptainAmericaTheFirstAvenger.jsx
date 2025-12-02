ed783c854cc26bfeba3d2717ddeee4ed49cc1229
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-02 14:42:18 -0800

    update captain america 1

--- src/pages/Mcu/Films/CaptainAmericaTheFirstAvenger.jsx
+++ src/pages/Mcu/Films/CaptainAmericaTheFirstAvenger.jsx
@@ -1,4 +1,8 @@
-
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
 
 const CaptainAmericaTheFirstAvenger = () =>
 {
