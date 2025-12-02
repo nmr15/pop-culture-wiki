7725d77acc0cfcf3a15654853f19f30a62fa7647
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-02 15:06:09 -0800

    update avengers

--- src/pages/Mcu/Films/TheAvengersFilm.jsx
+++ src/pages/Mcu/Films/TheAvengersFilm.jsx
@@ -8,9 +8,80 @@ const TheAvengersFilm = () =>
 {
   return(
     <>
-      <div>
-        <h1>TheAvengersFilm</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <Collapse 
+            id="4"
+            item="Marketing"
+            sub={[
+              {id: "4.1", title: "Trailers"},
+              {id: "4.2", title: "Tie-in comic"},
+              {id: "4.3", title: "Promotional partners"},
+              {id: "4.4", title: "Video game"}
+            ]}
+          />
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>The Avengers (film)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">The Avengers</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>The Avengers (film)</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg" alt="The Avengers poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>Joss Whedon</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/captain_america_the_first_avenger">Captain America: The First Avenger</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/iron_man_3">Iron Man 3</Link></td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </>
   )
 }
