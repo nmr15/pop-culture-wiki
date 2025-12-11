a58fa565c7171f36d91b2fad26f45b8c778aa43d
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-11 15:52:03 -0800

    update st

--- src/pages/StarWars/StarWars.jsx
+++ src/pages/StarWars/StarWars.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
 
 const StarWars = () => 
 {
+  useEffect(() =>
+  {
+    document.title = "Star Wars (franchise) - Pop Culture Wiki";
+  }, []);
+
   return (
     <>
       <aside className="sidebar">
