d4d6112969eab06884cc30f0501b6c1da97376ea
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-12 14:09:43 -0800

    update simpsons

--- src/pages/Simpsons/Simpsons.jsx
+++ src/pages/Simpsons/Simpsons.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Collapse from '../../components/Collapse'
 import Heading2 from '../../components/Heading2'
 
 const Simpsons = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "The Simpsons (franchise) - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
       <aside className="sidebar">
