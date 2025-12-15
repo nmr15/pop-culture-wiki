c546cbc11520616c4bb638ab9e3b34e6bc718fdf
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-15 09:55:40 -0800

    new file

---
+++ src/Template.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const Template = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Template - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Template</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Template</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Template</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="" alt="Template" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Template;
