896248144c5062da4171fdcb24e000e78dac9f72
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-15 10:14:10 -0800

    new files

---
+++ src/pages/Mcu/Characters/SteveRogersMCU.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
+const SteveRogersMCU = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Steve Rogers (MCU) - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Steve Rogers (MCU)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Steve Rogers (MCU)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Steve Rogers</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/b/b7/Steve_Rogers_Infobox.jpg" alt="Steve Rogers" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default SteveRogersMCU;
