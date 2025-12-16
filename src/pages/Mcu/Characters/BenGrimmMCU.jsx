dea28edc5ff1f7fcdddd00d3a208eab65e2df592
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-12-16 10:40:33 -0800

    new files

---
+++ src/pages/Mcu/Characters/BenGrimmMCU.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const BenGrimmMCU = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Ben Grimm (MCU) - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Ben Grimm (MCU)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Ben Grimm (MCU)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Ben Grimm</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/2/2a/Thing_Infobox.png" alt="Ben Grimm" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default BenGrimmMCU;
