6e16d102813e52eb9c9bae51820a53c47f4c9161
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-06 14:25:00 -0800

    new array

--- src/data/dataStarWars.js
+++ src/data/dataStarWars.js
@@ -211,5 +211,328 @@ export const starwars = [
   }
 ]
 
+export const starwarsChars = [
+  {
+    "id": "starwarsChar1",
+    "title": "Luke Skywalker",
+    "file": "LukeSkywalker",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/6/6c/LukeSkywalker-RotJAVA.png",
+    "route": "/starwars/luke_skywalker"
+  },
+  {
+    "id": "starwarsChar2",
+    "title": "Leia Organa",
+    "file": "LeiaOrgana",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/9/9b/Princessleiaheadwithgun.jpg",
+    "route": "/starwars/leia_organa"
+  },
+  {
+    "id": "starwarsChar3",
+    "title": "Anakin Skywalker",
+    "file": "AnakinSkywalker",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
+    "route": "/starwars/anakin_skywalker"
+  },
+  {
+    "id": "starwarsChar4",
+    "title": "Obi-Wan Kenobi",
+    "file": "ObiWanKenobi",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/c/c5/ObiSWC.png",
+    "route": "/starwars/obi_wan_kenobi"
+  },
+  {
+    "id": "starwarsChar5",
+    "title": "Han Solo",
+    "file": "HanSolo",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/4/49/HanSolo1BBY-TheStarWarsBook.png",
+    "route": "/starwars/han_solo"
+  },
+  {
+    "id": "starwarsChar6",
+    "title": "R2-D2",
+    "file": "R2D2",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/9/95/R2-D2-TROSOCE.png",
+    "route": "/starwars/r2d2"
+  },
+  {
+    "id": "starwarsChar7",
+    "title": "C-3PO",
+    "file": "C3PO",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/a/a2/C-3PO-TROSTGG.png",
+    "route": "/starwars/c3po"
+  },
+  {
+    "id": "starwarsChar8",
+    "title": "Chewbacca",
+    "file": "Chewbacca",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png",
+    "route": "/starwars/chewbacca"
+  },
+  {
+    "id": "starwarsChar9",
+    "title": "Ahsoka Tano",
+    "file": "AhsokaTano",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/5/5c/Ahsoka-TCW.png",
+    "route": "/starwars/ahsoka_tano"
+  },
+  {
+    "id": "starwarsChar10",
+    "title": "Yoda",
+    "file": "Yoda",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png",
+    "route": "/starwars/yoda"
+  },
+  {
+    "id": "starwarsChar11",
+    "title": "Sheev Palpatine",
+    "file": "SheevPalpatine",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/a/ab/Palpatine32BBY-Db.png",
+    "route": "/starwars/sheev_palpatine"
+  },
+  {
+    "id": "starwarsChar12",
+    "title": "Lando Calrissian",
+    "file": "LandoCalrissian",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/8/8f/Lando_ROTJ.png",
+    "route": "/starwars/lando_calrissian"
+  },
+  {
+    "id": "starwarsChar13",
+    "title": "Padm� Amidala",
+    "file": "Padm�Amidala",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/b/b2/Padmegreenscrshot.jpg",
+    "route": "/starwars/padm�_amidala"
+  },
+  {
+    "id": "starwarsChar14",
+    "title": "Mace Windu",
+    "file": "MaceWindu",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/2/27/MaceWindu_-WoSW.png",
+    "route": "/starwars/mace_windu"
+  },
+  {
+    "id": "starwarsChar15",
+    "title": "Boba Fett",
+    "file": "BobaFett",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/4/46/BobaFett-SWI206.png",
+    "route": "/starwars/boba_fett"
+  },
+  {
+    "id": "starwarsChar16",
+    "title": "Qui-Gon Jinn",
+    "file": "QuiGonJinn",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/f/f6/Qui-Gon_Jinn_Headshot_TPM.jpg",
+    "route": "/starwars/qui_gon_jinn"
+  },
+  {
+    "id": "starwarsChar17",
+    "title": "Darth Maul",
+    "file": "DarthMaul",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/5/50/Darth_Maul_profile.png",
+    "route": "/starwars/darth_maul"
+  },
+  {
+    "id": "starwarsChar18",
+    "title": "Count Dooku",
+    "file": "CountDooku",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/5/5f/Dooku-SWBC61.png",
+    "route": "/starwars/count_dooku"
+  },
+  {
+    "id": "starwarsChar19",
+    "title": "General Grievous",
+    "file": "GeneralGrievous",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/4/45/GrievousStand-SWHC821.png",
+    "route": "/starwars/general_grievous"
+  },
+  {
+    "id": "starwarsChar20",
+    "title": "Jango Fett",
+    "file": "JangoFett",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/8/81/JangoInHelmet.png",
+    "route": "/starwars/jango_fett"
+  },
+  {
+    "id": "starwarsChar21",
+    "title": "Jar Jar Binks",
+    "file": "JarJarBinks",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/e/ed/Binks22BBY.png",
+    "route": "/starwars/jar_jar_binks"
+  },
+  {
+    "id": "starwarsChar22",
+    "title": "Admiral Ackbar",
+    "file": "AdmiralAckbar",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/f/fb/Ackbar_HS.jpg",
+    "route": "/starwars/admiral_ackbar"
+  },
+  {
+    "id": "starwarsChar23",
+    "title": "Jabba the Hutt",
+    "file": "JabbaTheHutt",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/f/fe/Jabba_MMSWCA.png",
+    "route": "/starwars/jabba_the_hutt"
+  },
+  {
+    "id": "starwarsChar24",
+    "title": "Captain Rex",
+    "file": "CaptainRex",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/9/97/Rex-CGSWG.png",
+    "route": "/starwars/captain_rex"
+  },
+  {
+    "id": "starwarsChar25",
+    "title": "Asajj Ventress",
+    "file": "AsajjVentress",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/a/af/AsajjVentress-LD-SWCT.png",
+    "route": "/starwars/asajj_ventress"
+  },
+  {
+    "id": "starwarsChar26",
+    "title": "Ezra Bridger",
+    "file": "EzraBridger",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/3/30/Ezra_HS.png",
+    "route": "/starwars/ezra_bridger"
+  },
+  {
+    "id": "starwarsChar27",
+    "title": "Kanan Jarrus",
+    "file": "KananJarrus",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/a/a7/KananJarrus.png",
+    "route": "/starwars/kanan_jarrus"
+  },
+  {
+    "id": "starwarsChar28",
+    "title": "Hera Syndulla",
+    "file": "HeraSyndulla",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/8/86/HeraSyndulla.png",
+    "route": "/starwars/hera_syndulla"
+  },
+  {
+    "id": "starwarsChar29",
+    "title": "Sabine Wren",
+    "file": "SabineWren",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/5/51/SabineSeasonOnePromo2.png",
+    "route": "/starwars/sabine_wren"
+  },
+  {
+    "id": "starwarsChar30",
+    "title": "Garazeb Orrellos",
+    "file": "GarazebOrrellos",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/5/5c/GarazebSeasonOnePromo.jpg",
+    "route": "/starwars/garazeb_orrelos"
+  },
+  {
+    "id": "starwarsChar31",
+    "title": "Chopper",
+    "file": "Chopper",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/b/bb/Chopper_Live_Action.png",
+    "route": "/starwars/chopper"
+  },
+  {
+    "id": "starwarsChar32",
+    "title": "Jyn Erso",
+    "file": "JynErso",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/b/b3/JynErsoPromo.png",
+    "route": "/starwars/jyn_erso"
+  },
+  {
+    "id": "starwarsChar33",
+    "title": "Cassian Andor",
+    "file": "CassianAndor",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/3/31/CassianAndor-Fathead.png",
+    "route": "/starwars/cassian_andor"
+  },
+  {
+    "id": "starwarsChar34",
+    "title": "K-2SO",
+    "file": "K2SO",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/2/23/K2SO-SWBC35cvr.png",
+    "route": "/starwars/k2so"
+  },
+  {
+    "id": "starwarsChar35",
+    "title": "Saw Gerrera",
+    "file": "SawGerrera",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/0/06/SawGerrera-Platinum2025.png",
+    "route": "/starwars/saw_gerrera"
+  },
+  {
+    "id": "starwarsChar36",
+    "title": "Grand Moff Tarkin",
+    "file": "GrandMoffTarkin",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/c/c1/Tarkininfobox.jpg",
+    "route": "/starwars/grand_moff_tarkin"
+  },
+  {
+    "id": "starwarsChar37",
+    "title": "Cal Kestis",
+    "file": "CalKestis",
+    "media": "game",
+    "img": "https://static.wikia.nocookie.net/starwars/images/b/b0/9BBY_Cal.png",
+    "route": "/starwars/cal_kestis"
+  },
+  {
+    "id": "starwarsChar38",
+    "title": "Grand Admiral Thrawn",
+    "file": "GrandAdmiralThrawn",
+    "media": "series",
+    "img": "https://static.wikia.nocookie.net/starwars/images/f/fe/Thrawn_Character_poster.png",
+    "route": "/starwars/grand_admiral_thrawn"
+  },
+  {
+    "id": "starwarsChar39",
+    "title": "Mon Mothma",
+    "file": "MonMothma",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/c/c8/MonMothma-RotJAVA.png",
+    "route": "/starwars/mon_mothma"
+  },
+  {
+    "id": "starwarsChar40",
+    "title": "Orson Krennic",
+    "file": "OrsonKrennic",
+    "media": "film",
+    "img": "https://static.wikia.nocookie.net/starwars/images/0/05/OrsonKrennic-SWI171.png",
+    "route": "/starwars/orson_krennic"
+  }
+]
+
 export const starwarsFilms = starwars.filter((sw) => sw.media == "film");
 export const starwarsSeries = starwars.filter((swSeries) => swSeries.media == "series")
