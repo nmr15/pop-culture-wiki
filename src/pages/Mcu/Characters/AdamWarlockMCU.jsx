454b410ab7954a0f5e1f405f72212104ec8bd837
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-06 15:59:58 -0800

    update awmcu

--- src/pages/Mcu/Characters/AdamWarlockMCU.jsx
+++ src/pages/Mcu/Characters/AdamWarlockMCU.jsx
@@ -16,6 +16,35 @@ const AdamWarlockMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Creation"},
+              {id: "1.2", title: "The Warlock"},
+              {id: "1.3", title: "The Next Guardians"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#2">Personality</a></li>
+          <Collapse 
+            id="3"
+            item="Powers and abilities"
+            sub={[
+              {id: "3.1", title: "Powers"},
+              {id: "3.2", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Equipment"
+            sub={[
+              {id: "4.1", title: "Vehicles"},
+              {id: "4.2", title: "Other equipment"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Facilities</a></li>
+          <li className="sidebar-list-item"><a href="#6">Trivia</a></li>
+          <li className="sidebar-list-item"><a href="#7">Behind the scenes</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
