0b5243b3a266cf05e64d152e44f7837317a65398
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-13 11:10:20 -0800

    update amtw

--- src/pages/Mcu/Films/AntManAndTheWasp.jsx
+++ src/pages/Mcu/Films/AntManAndTheWasp.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 import TableMcu from '../../../components/TableMcu'
 
 const AntManAndTheWasp = () =>
@@ -60,51 +61,94 @@ const AntManAndTheWasp = () =>
             className="article-heading-img" 
           />
         </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Ant-Man and the Wasp</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/2/2c/Ant-Man_and_the_Wasp_poster.jpg" alt="Ant-Man and the Wasp poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>Peyton Ree</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/avengers_infinity_war">Avengers: Infinity War</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/captain_marvel_film">Captain Marvel</Link></td>
+            </tr>
+          </tbody>
+        </table>
         <div className="div-top">
           
         </div>
         <div>
-          <h2 className="article-heading-2" id="1">Plot</h2>
+          <Heading2 id={1} title={"Plot"} />
           <div>
 
           </div>
         </div>
         <div>
-          <h2 className="article-heading-2" id="2">Cast</h2>
+          <Heading2 id={2} title={"Cast"} />
           <div>
             
           </div>
         </div>
         <div>
-          <h2 className="article-heading-2" id="3">Production</h2>
+          <Heading2 id={3} title={"Production"} />
           <div>
             <h3 className="article-heading-3" id="3.1">Development</h3>
+
             <h3 className="article-heading-3" id="3.2">Pre-production</h3>
+
             <h3 className="article-heading-3" id="3.3">Filming</h3>
+
             <h3 className="article-heading-3" id="3.4">Post-production</h3>
             <h4 className="article-heading-4" id="3.4.1">Visual effects</h4>
           </div>
         </div>
         <div>
-          <h2 className="article-heading-2" id="4">Music</h2>
+          <Heading2 id={4} title={"Music"} />
           <div>
             
           </div>
         </div>
         <div>
+          <Heading2 id={5} title={"Marketing"} />
           <h2 className="article-heading-2" id="5">Marketing</h2>
           <div>
             
           </div>
         </div>
         <div>
+          <Heading2 id={6} title={"Release"} />
           <h2 className="article-heading-2" id="6">Release</h2>
           <div>
             <h3 className="article-heading-3" id="6.1">Theatrical</h3>
+
             <h3 className="article-heading-3" id="6.2">Home media</h3>
           </div>
         </div>
         <div>
+          <Heading2 id={7} title={"Reception"} />
           <h2 className="article-heading-2" id="7">Reception</h2>
           <div>
             <h3 className="article-heading-3" id="7.1">Box office</h3>
@@ -112,6 +156,7 @@ const AntManAndTheWasp = () =>
           </div>
         </div>
         <div>
+          <Heading2 id={8} title={"Sequel"} />
           <h2 className="article-heading-2" id="8">Sequel</h2>
           <div>
             
