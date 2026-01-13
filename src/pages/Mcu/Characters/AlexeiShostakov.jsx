10284e5e64074f38ee271073f055533631ea0668
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-13 13:39:34 -0800

    update alexi

--- src/pages/Mcu/Characters/AlexeiShostakov.jsx
+++ src/pages/Mcu/Characters/AlexeiShostakov.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
 
 const AlexeiShostakov = () =>
 {
@@ -13,6 +16,34 @@ const AlexeiShostakov = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life"},
+              {id: "1.2", title: "Russian agent"},
+              {id: "1.3", title: "Family reunion"},
+              {id: "1.4", title: "Search for Glory"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#2">Personality</a></li>
+          <Collapse 
+            id="3"
+            item="Powers and abilities"
+            sub={[
+              {id: "3.1", ttle: "Powers"},
+              {id: "3.2", ttle: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Equipment"
+            sub={[
+              {id: "4.1", title: "Weapons"},
+              {id: "4.2", title: "Other equipment"},
+              {id: "4.3", title: "Vehicles"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
