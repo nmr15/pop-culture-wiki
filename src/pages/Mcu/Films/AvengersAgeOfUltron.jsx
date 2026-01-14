cab6477ee5b08321ba56651eafa74a544061a8c7
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-14 11:16:16 -0800

    update aou

--- src/pages/Mcu/Films/AvengersAgeOfUltron.jsx
+++ src/pages/Mcu/Films/AvengersAgeOfUltron.jsx
@@ -109,7 +109,27 @@ const AvengersAgeOfUltron = () =>
          <div>
           <Heading2 id={1} title={"Plot"} />
           <div>
-
+            <p>
+              <b>Avengers: Age of Ultron</b> is a 2015 American superhero film based on the <Link to="/marvelcomics">Marvel Comics</Link> superhero team
+              the <Link>Avengers</Link>. Produced by <Link>Marvel Studios</Link> and distributed by Walt Disney Studios Motion Pictures. It is the sequel
+              to <Link to="mcu/the_avengers_film">The Avengers</Link> (2012) and the 11th film in the <Link to="/mcu">Marvel Cinematic Universe</Link>
+              (MCU). Written and directed by <Link>Joss Whedon</Link>, the film features an ensemble cast including <Link>Robert Downey Jr.</Link>,
+              <Link> Chris Hemsworth</Link>, <Link>Mark Ruffalo</Link>, <Link>Chris Evans</Link>, <Link>Scarlett Johansson</Link>, <Link>Jeremy
+              Renner</Link>, <Link>Don Cheadle</Link>, <Link>Aaron Taylor-Johnson</Link>, <Link>Elizabeth Olsen</Link>, <Link>Paul Bettany</Link>,
+              <Link> Cobie Smulders</Link>, <Link>Anthony Mackie</Link>, <Link>Hayley Atwell</Link>, <Link>Idris Elba</Link>, <Link>Linda Cardellini</Link>,
+              <Link>Stellan Skarsgård</Link>, <Link>James Spader</Link>, and <Link>Samuel L. Jackson</Link>. In the film, the <Link>Avengers</Link>
+              fight <Link>Ultron</Link> (Spader), an artifical intelligence created by <Link to="/mcu/tony_stark">Tony Stark</Link> (Downey) and
+              <Link to="/mcu/bruce_banner"> Bruce Banner</Link> (Ruffalo), who plans to bring about world peace by causing human extinction.
+            </p>
+            <p>
+              A sequel was announced in May 2012 after the successful release of The Avengers, with Whedon set to return as writer and director in August.
+              He updated Ultron's origin for the film to invlove the MCU's Avengers team and introduced the characters
+              <Link to="/mcu/piertro_maximoff">Pietro</Link> (Taylor-Johnson) and <Link to="/mcu/wanda_maximoff">Wanda Maximoff</Link> (Olsen), to whom
+              Marvel shared the rights with 20th Century Fox. Casting began in June 2013 with Downey re-signing. Second unit filming began in February 2014
+              in South Africa, with principal photography taking place between March and August, primarily at Shepperton Studios in Surrey, England.
+              Additional footage was filmed in Italy, South Korea, Bangladesh, the state of New York, and around England. With an estimated net production
+              budget of $365 million, the film is one of the most expensive ever made.
+            </p>
           </div>
         </div>
         <div>
