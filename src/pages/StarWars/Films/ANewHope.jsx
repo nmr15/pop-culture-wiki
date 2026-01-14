f0c18f8115442de3a59b9c27bfc6d76ae655af5a
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-14 14:28:56 -0800

    update anh

--- src/pages/StarWars/Films/ANewHope.jsx
+++ src/pages/StarWars/Films/ANewHope.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 
 const ANewHope = () =>
 {
@@ -13,6 +15,61 @@ const ANewHope = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Writing"},
+              {id: "3.3", title: "Casting"},
+              {id: "3.4", title: "Design"},
+              {id: "3.5", title: "Filming"},
+              {id: "3.6", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Soundrack</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "MPAA rating"},
+              {id: "6.2", title: "First public screening"},
+              {id: "6.3", title: "Premiere and initial release"},
+              {id: "6.4", title: "Box office"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Critical response"},
+              {id: "7.2", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="8"
+            item="Post-release"
+            sub={[
+              {id: "8.1", title: "Theatrical re-releases", sub2: [
+                {id: "8.1.1", title: "Special Edition"}
+              ]},
+              {id: "8.2", title: "Home media"},
+              {id: "8.3", title: "Merchandising"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#9">Cinematic and literary allusions</a></li>
+          <Collapse 
+            id="10"
+            item="Legacy and influence"
+            sub={[
+              {id: "10.1", title: "In popular culture"},
+              {id: "10.2", title: "Cinematic influence"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#11">Sequels, prequels, and adatations</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -45,6 +102,12 @@ const ANewHope = () =>
         </table>
         <div className="div-top">
           
+        </div>
+        <div>
+          <Heading2 id={1} title={"Plot"} />
+          <div>
+            
+          </div>
         </div>
       </main>
     </>
