83e220cd9d289804e12cbb4d267661af72950f24
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-13 16:19:23 -0800

    update files

--- src/pages/Mcu/Films/AvengersAgeOfUltron.jsx
+++ src/pages/Mcu/Films/AvengersAgeOfUltron.jsx
@@ -1,10 +1,55 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const AvengersAgeOfUltron = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Avengers: Age of Ultron - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>AvengersAgeOfUltron</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Avengers: Age of Ultron</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Avengers: Age of Ultron</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Avengers: Age of Ultron</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="/mcu/avengers_age_of_ultron" alt="Avengers: Age of Ultron" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
