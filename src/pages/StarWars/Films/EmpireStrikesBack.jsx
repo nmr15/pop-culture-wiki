a73e58efef19268aebdee366ed987d69d5e3f4db
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-14 16:06:48 -0800

    update esb

--- src/pages/StarWars/Films/EmpireStrikesBack.jsx
+++ src/pages/StarWars/Films/EmpireStrikesBack.jsx
@@ -15,6 +15,75 @@ const EmpireStrikesBack = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Writing"},
+              {id: "3.3", title: "Rewrite"},
+              {id: "3.4", title: "Casting"},
+              {id: "3.5", title: "Pre-production"},
+              {id: "3.6", title: "Music"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Filming"
+            sub={[
+              {id: "4.1", title: "Commencement in Norway"},
+              {id: "4.2", title: "Filming at Elstree Studios"},
+              {id: "4.3", title: "The duel, Dagobah, and conclusion"},
+              {id: "4.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Special effects and design</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Context"},
+              {id: "6.2", title: "Credits and title"},
+              {id: "6.3", title: "Box office"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Critical response"},
+              {id: "7.2", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="8"
+            item="Post-release"
+            sub={[
+              {id: "8.1", title: "Special Edition and other changes"},
+              {id: "8.2", title: "Home media"},
+              {id: "8.3", title: "Other media"}
+            ]}
+          />
+          <Collapse 
+            id="9"
+            item="Thematic analysis"
+            sub={[
+              {id: "9.1", title: "Mythology and inspirations"},
+              {id: "9.2", title: "Religion"},
+              {id: "9.3", title: "Duality and evil"}
+            ]}
+          />
+          <Collapse 
+            id="10"
+            item="Legacy"
+            sub={[
+              {id: "10.1", title: "Critical reassessment"},
+              {id: "10.2", title: "Cultural influence"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#11">Sequels, prequels, and adaptations</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
