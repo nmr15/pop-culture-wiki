ca3a1aa53d8c9ce25ce46eaf91b9aac0a08e0242
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-14 16:33:20 -0800

    update rots

--- src/pages/StarWars/Films/ReturnOfTheJedi.jsx
+++ src/pages/StarWars/Films/ReturnOfTheJedi.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 
 const ReturnOfTheJedi = () =>
 {
@@ -13,6 +15,55 @@ const ReturnOfTheJedi = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Casting"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <Collapse 
+            id="5"
+            item="Release"
+            sub={[
+              {id: "5.1", title: "Title change"},
+              {id: "5.2", title: "Box office"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Reception"
+            sub={[
+              {id: "6.1", title: "Critical response"},
+              {id: "6.2", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Post-release"
+            sub={[
+              {id: "7.1", title: "Re-releases"},
+              {id: "7.2", title: "Home media"},
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Legacy</a></li>
+          <Collapse 
+            id="9"
+            item="Marketing"
+            sub={[
+              {id: "9.1", title: "Novelization"},
+              {id: "9.2", title: "Radio drama"},
+              {id: "9.3", title: "Comic book adaptation"},
+              {id: "9.4", title: "Book-and-record set"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#10">Sequels and prequels</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
