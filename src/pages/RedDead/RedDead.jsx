635a101b4a4ae609553468cb3901da424fd92d69
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-16 11:47:51 -0800

    update reddead

--- src/pages/RedDead/RedDead.jsx
+++ src/pages/RedDead/RedDead.jsx
@@ -15,6 +15,35 @@ const RedDead = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Games"
+            sub={[
+              {id: "1.1", title: "Red Dead Revolver"},
+              {id: "1.2", title: "Red Dead Redemption"},
+              {id: "1.3", title: "Red Dead Redemption 2"},
+              {id: "1.4", title: "Future"}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item={"Common elements"}
+            sub={[
+              {id: "2.1", title: "Gameplay", sub2: [
+                {id: "2.1.1", title: "Dead Eye"},
+                {id: "2.1.2", title: "Honor"}
+              ]},
+              {id: "2.2", title: "Setting"},
+              {id: "2.3", title: "Characters"},
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Reception"
+            sub={[
+              {id: "3.1", title: "Sales"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
