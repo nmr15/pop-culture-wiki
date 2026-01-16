3c91edb5e4ab57b113d28c794b4c5870adc88c7b
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-16 08:15:26 -0800

    update ave

--- src/pages/Mcu/Films/AvengersEndgame.jsx
+++ src/pages/Mcu/Films/AvengersEndgame.jsx
@@ -1,10 +1,71 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const AvengersEndgame = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Avengers: Endgame - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>AvengersEndgame</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Avengers: Endgame</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Avengers: Endgame</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Avengers: Endgame</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg" alt="Avengers: Endgame poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>Anthony Russo</Link> <br /> <Link>Joe Russo</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/captain_marvel_film">Captain Marvel</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/spiderman_far_from_home">Spider-Man: Far From Home</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
