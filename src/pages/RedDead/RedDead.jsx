e90093c100d5b28ae681e4b44111b036c4237e40
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-16 11:40:40 -0800

    update Red Dead

--- src/pages/RedDead/RedDead.jsx
+++ src/pages/RedDead/RedDead.jsx
@@ -1,14 +1,53 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
+
 const RedDead = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Red Dead - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
       <aside className="sidebar">
         <ul className="sidebar-list">
-
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
-        <h1>RedDead</h1>
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Red Dead</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Red Dead</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Red Dead</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/2/21/Official_Red_Dead_logo.png" alt="Red Dead logo" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
       </main>
     </>
   )
