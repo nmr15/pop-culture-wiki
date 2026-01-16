c862b37e13379f152be871f192f2562329c1bb49
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-01-15 16:15:20 -0800

    update tpm

--- src/pages/StarWars/Films/PhantomMenace.jsx
+++ src/pages/StarWars/Films/PhantomMenace.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 
 const PhantomMenace = () =>
 {
@@ -13,6 +15,63 @@ const PhantomMenace = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production and design"},
+              {id: "3.3", title: "Casting"},
+              {id: "3.4", title: "Filming"},
+              {id: "3.5", title: "Effects"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Themes and analysis"
+            sub={[
+              {id: "4.1", title: "References: Cinematic and literary allusions"},
+              {id: "4.2", title: "Dramatic irony"},
+              {id: "4.3", title: "Parallels"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Music</a></li>
+          <Collapse 
+            id="6"
+            item="Marketing"
+            sub={[
+              {id: "6.1", title: "Promotion"},
+              {id: "6.2", title: "Adaptations"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Release"
+            sub={[
+              {id: "7.1", title: "Theatrical"},
+              {id: "7.2", title: "Home media"},
+              {id: "7.3", title: "Theatrical re-releases"}
+            ]}
+          />
+          <Collapse 
+            id="8"
+            item="Reception"
+            sub={[
+              {id: "8.1", title: "Critical response"},
+              {id: "8.2", title: "Box office"},
+              {id: "8.3", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="9"
+            item="Legacy"
+            sub={[
+              {id: "9.1", title: "Sequels"},
+              {id: "9.2", title: "Cast and crew responses"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
