4478f3b80df0d1ffcefa6f9b1e4d21db242b1308
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-02 11:26:39 -0800

    new table

---
+++ src/components/TableStarWars.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const TableStarWars = () =>
+{
+  return(
+    <>
+      <div>
+        <table className="table-center">
+          
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default TableStarWars;
