261aa1b64f9d1a6f253a02e11dc06bf1e0f943ea
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-03 11:21:34 -0800

    new array

---
+++ src/data/dataFuturama.js
@@ -0,0 +1,65 @@
+export const futuramaChars = [
+  {
+    "id": "futuramachar1",
+    "title": "Philip J. Fry",
+    "file": "PhilipJFry",
+    "img": "",
+    "route": "/futurama/philip_j_fry"
+  },
+  {
+    "id": "futuramachar2",
+    "title": "Turanga Leela",
+    "file": "TurangaLeela",
+    "img": "",
+    "route": "/futurama/turanga_leela"
+  },
+  {
+    "id": "futuramachar3",
+    "title": "Bender",
+    "file": "BenderChar",
+    "img": "",
+    "route": "/futurama/bender"
+  },
+  {
+    "id": "futuramachar4",
+    "title": "Professor Farnsworth",
+    "file": "ProfessorFarnsworth",
+    "img": "",
+    "route": "/futurama/professor_farnsworth"
+  },
+  {
+    "id": "futuramachar5",
+    "title": "Zoidberg",
+    "file": "Zoidberg",
+    "img": "",
+    "route": "/futurama/zoidberg"
+  },
+  {
+    "id": "futuramachar6",
+    "title": "Amy Wong",
+    "file": "AmyWong",
+    "img": "",
+    "route": "/futurama/amy_wong"
+  },
+  {
+    "id": "futuramachar7",
+    "title": "Hermes Conrad",
+    "file": "HermesConrad",
+    "img": "",
+    "route": "/futurama/hermes_conrad"
+  },
+  {
+    "id": "futuramachar8",
+    "title": "Zapp Brannigan",
+    "file": "ZappBrannigan",
+    "img": "",
+    "route": "/futurama/zapp_brannigan"
+  },
+  {
+    "id": "futuramachar9",
+    "title": "Kif Kroker",
+    "file": "KifKroker",
+    "img": "",
+    "route": "/futurama/kif_kroker"
+  }
+]
