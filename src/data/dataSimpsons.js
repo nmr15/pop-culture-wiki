4d0b3b5262d2fb73cda48ebb667622436865d8ca
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-03 10:53:28 -0800

    new array

---
+++ src/data/dataSimpsons.js
@@ -0,0 +1,37 @@
+export const simpsonsChars = [
+  {
+    "id": "simpsonschar1",
+    "title": "Homer Simpson",
+    "file": "HomerSimpson",
+    "img": "",
+    "route": "/simpsons/homer_simpson"
+  },
+  {
+    "id": "simpsonschar2",
+    "title": "Marge Simpson",
+    "file": "MargeSimpson",
+    "img": "",
+    "route": "/simpsons/marge_simpson"
+  },
+  {
+    "id": "simpsonschar3",
+    "title": "Bart Simpson",
+    "file": "BartSimpson",
+    "img": "",
+    "route": "/simpsons/bart_simpson"
+  },
+  {
+    "id": "simpsonschar4",
+    "title": "Lisa Simpson",
+    "file": "LisaSimpson",
+    "img": "",
+    "route": "/simpsons/lisa_simpson"
+  },
+  {
+    "id": "simpsonschar5",
+    "title": "Maggie Simpson",
+    "file": "MaggieSimpson",
+    "img": "",
+    "route": "/simpsons/maggie_simpson"
+  }
+]
