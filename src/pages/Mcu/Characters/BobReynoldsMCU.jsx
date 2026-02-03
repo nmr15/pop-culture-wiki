b5b9d18f6f1494c3fe53c6101f96698451393f4f
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-03 14:00:38 -0800

    update bbr

--- src/pages/Mcu/Characters/BobReynoldsMCU.jsx
+++ src/pages/Mcu/Characters/BobReynoldsMCU.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
 
 const BobReynoldsMCU = () =>
 {
@@ -13,6 +16,57 @@ const BobReynoldsMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life", sub2: [
+                {id: "1.1.1", title: "Troubled childhood"},
+                {id: "1.1.2", title: "Staying afloat"}
+              ]},
+              {id: "1.2", title: "Project Sentry", sub2: [
+                {id: "1.2.1", title: "Volunteering for O.X.E."},
+                {id: "1.2.2", title: "Waking up"},
+                {id: "1.2.3", title: "Escaping the vault"},
+                {id: "1.2.4", title: "Discovering his powers"}
+              ]},
+              {id: "1.3", title: "Becoming a hero", sub2: [
+                {id: "1.3.1", title: "Meeting the Contessa"},
+                {id: "1.3.2", title: "Fighting his friends"},
+                {id: "1.3.3", title: "Attack on Manhattan"},
+                {id: "1.3.4", title: "Comforted by Yelena Bolova"},
+                {id: "1.3.5", title: "Facing Void"},
+                {id: "1.3.6", title: "New team"}
+              ]},
+              {id: "1.4", title: "New Avengers", sub2: [
+                {id: "1.4.1", title: "Witnessing a spaceship"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Personalities"
+            sub={[
+              {id: "2.1", title: "Bob Reynolds' personality"},
+              {id: "2.2", title: "Void's personality"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Powers and abilities"
+            sub={[
+              {id: "3.1", title: "Powers"},
+              {id: "3.2", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Equipment"
+            sub={[
+              {id: "4.1", title: "Other equipment"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -38,8 +92,12 @@ const BobReynoldsMCU = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data"><Link>Lewis Pullman</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Appearances</th>
+              <td className="infobox-data"><Link>Thunderbolts*</Link></td>
             </tr>
           </tbody>
         </table>
