20f1f4af39d80d0daa559db104c3f5d5e0b6af9d
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-03 10:58:51 -0800

    new array

---
+++ src/data/dataAdventureTime.js
@@ -0,0 +1,37 @@
+export const adventuretimeChars = [
+  {
+    "id": "adventuretimechar1",
+    "title": "Finn Mertens",
+    "file": "FinnMertens",
+    "img": "",
+    "route": "/adventure_time/finn_mertens"
+  },
+  {
+    "id": "adventuretimechar2",
+    "title": "Jake The Dog",
+    "file": "JakeTheDog",
+    "img": "",
+    "route": "/adventure_time/jake_the_dog"
+  },
+  {
+    "id": "adventuretimechar3",
+    "title": "Princess Bubblegum",
+    "file": "PrincessBubblegum",
+    "img": "",
+    "route": "/adventure_time/princess_bubblegum"
+  },
+  {
+    "id": "adventuretimechar4",
+    "title": "Simon Petrikov",
+    "file": "SimonPetrikov",
+    "img": "",
+    "route": "/adventure_time/simon_petrikov"
+  },
+  {
+    "id": "adventuretimechar5",
+    "title": "Marceline Abadeer",
+    "file": "MarcelineAbadeer",
+    "img": "",
+    "route": "/adventure_time/marceline_abadeer"
+  }
+]
