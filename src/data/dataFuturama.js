3f35bed7a93b2f38bb60354e163bc36e59559410
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-04 11:22:32 -0800

    update array

--- src/data/dataFuturama.js
+++ src/data/dataFuturama.js
@@ -3,63 +3,63 @@ export const futuramaChars = [
     "id": "futuramachar1",
     "title": "Philip J. Fry",
     "file": "PhilipJFry",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/4/4c/Character_Fry.png",
     "route": "/futurama/philip_j_fry"
   },
   {
     "id": "futuramachar2",
     "title": "Turanga Leela",
     "file": "TurangaLeela",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/3/3d/Character_Leela.png",
     "route": "/futurama/turanga_leela"
   },
   {
     "id": "futuramachar3",
     "title": "Bender",
     "file": "BenderChar",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/3/3d/Character_Bender.png",
     "route": "/futurama/bender"
   },
   {
     "id": "futuramachar4",
     "title": "Professor Farnsworth",
     "file": "ProfessorFarnsworth",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/3/35/Character_The_Professor.png",
     "route": "/futurama/professor_farnsworth"
   },
   {
     "id": "futuramachar5",
     "title": "Zoidberg",
     "file": "Zoidberg",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/1/17/Zoidberg.png",
     "route": "/futurama/zoidberg"
   },
   {
     "id": "futuramachar6",
     "title": "Amy Wong",
     "file": "AmyWong",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/1/1f/Character_Amy.png",
     "route": "/futurama/amy_wong"
   },
   {
     "id": "futuramachar7",
     "title": "Hermes Conrad",
     "file": "HermesConrad",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/f/ff/Character_Hermes.png",
     "route": "/futurama/hermes_conrad"
   },
   {
     "id": "futuramachar8",
     "title": "Zapp Brannigan",
     "file": "ZappBrannigan",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/f/fe/Zapp_Brannigan_-_Official_Character_Promo.jpg",
     "route": "/futurama/zapp_brannigan"
   },
   {
     "id": "futuramachar9",
     "title": "Kif Kroker",
     "file": "KifKroker",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/enfuturama/images/a/ae/Character_Kif.png",
     "route": "/futurama/kif_kroker"
   }
 ]
