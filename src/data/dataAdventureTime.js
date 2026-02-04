f16d3a762b709c208034aeb8199d269423ff8f6e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-04 09:49:32 -0800

    update array

--- src/data/dataAdventureTime.js
+++ src/data/dataAdventureTime.js
@@ -3,35 +3,35 @@ export const adventuretimeChars = [
     "id": "adventuretimechar1",
     "title": "Finn Mertens",
     "file": "FinnMertens",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/7/7e/Finn_with_bionic_arm-0.png",
     "route": "/adventure_time/finn_mertens"
   },
   {
     "id": "adventuretimechar2",
     "title": "Jake The Dog",
     "file": "JakeTheDog",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3b/Jakesalad.png",
     "route": "/adventure_time/jake_the_dog"
   },
   {
     "id": "adventuretimechar3",
     "title": "Princess Bubblegum",
     "file": "PrincessBubblegum",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/00/Princess_Bubblegum.png",
     "route": "/adventure_time/princess_bubblegum"
   },
   {
     "id": "adventuretimechar4",
     "title": "Simon Petrikov",
     "file": "SimonPetrikov",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/6/64/Original_Ice_King.png",
     "route": "/adventure_time/simon_petrikov"
   },
   {
     "id": "adventuretimechar5",
     "title": "Marceline Abadeer",
     "file": "MarcelineAbadeer",
-    "img": "",
+    "img": "https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/8/80/Marceline1.png",
     "route": "/adventure_time/marceline_abadeer"
   }
 ]
