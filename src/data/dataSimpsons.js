c9401c1d5cd53de72043e4f09133ef7d7b94289a
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-04 09:53:24 -0800

    update array

--- src/data/dataSimpsons.js
+++ src/data/dataSimpsons.js
@@ -3,35 +3,35 @@ export const simpsonsChars = [
     "id": "simpsonschar1",
     "title": "Homer Simpson",
     "file": "HomerSimpson",
-    "img": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
     "route": "/simpsons/homer_simpson"
   },
   {
     "id": "simpsonschar2",
     "title": "Marge Simpson",
     "file": "MargeSimpson",
-    "img": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
     "route": "/simpsons/marge_simpson"
   },
   {
     "id": "simpsonschar3",
     "title": "Bart Simpson",
     "file": "BartSimpson",
-    "img": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
     "route": "/simpsons/bart_simpson"
   },
   {
     "id": "simpsonschar4",
     "title": "Lisa Simpson",
     "file": "LisaSimpson",
-    "img": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png",
     "route": "/simpsons/lisa_simpson"
   },
   {
     "id": "simpsonschar5",
     "title": "Maggie Simpson",
     "file": "MaggieSimpson",
-    "img": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png",
     "route": "/simpsons/maggie_simpson"
   }
 ]
