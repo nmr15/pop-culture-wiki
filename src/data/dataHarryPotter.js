086defbf4279c739b9dc355f0381d0ad318f27a3
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-04 11:01:23 -0800

    new array

--- src/data/dataHarryPotter.js
+++ src/data/dataHarryPotter.js
@@ -49,3 +49,132 @@ export const hpbooks = [
     "route": "/harry_potter/harry_potter_and_the_deathly_hallows"
   }
 ]
+
+export const hpchars = [
+  {
+    "id": "hpchars1",
+    "title": "Harry Potter",
+    "file": "HarryPotterChar",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/c/ce/Harry_Potter_DHF1.jpg",
+    "route": "/harry_potter/harry_potter_character"
+  },
+  {
+    "id": "hpchars2",
+    "title": "Ron Weasley",
+    "file": "RonWeasley",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/4/44/Ronald_Weasley_DHF1.jpg",
+    "route": "/harry_potter/ron_weasley"
+  },
+  {
+    "id": "hpchars3",
+    "title": "Hermione Granger",
+    "file": "HermioneGranger",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/3/34/Hermione_Granger.jpg",
+    "route": "/harry_potter/hermione_granger"
+  },
+  {
+    "id": "hpchars4",
+    "title": "Albus Dumbledore",
+    "file": "AlbusDumbledore",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/7/75/Albus_Dumbledore_%28HBPF_promo%29.jpg",
+    "route": "/harry_potter/albus_dumbledore"
+  },
+  {
+    "id": "hpchars5",
+    "title": "Tom Riddle",
+    "file": "TomRiddle",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/9/97/Voldemort_Headshot_DHF1.png",
+    "route": "/harry_potter/tom_riddle"
+  },
+  {
+    "id": "hpchars6",
+    "title": "Severus Snape",
+    "file": "SeverusSnape",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/a/a3/Severus_Snape.jpg",
+    "route": "/harry_potter/severus_snape"
+  },
+  {
+    "id": "hpchars7",
+    "title": "Rubeus Hagrid",
+    "file": "RubeusHagrid",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/f/fe/Rubeus_Hagrid.png",
+    "route": "/harry_potter/rubeus_hagrid"
+  },
+  {
+    "id": "hpchars8",
+    "title": "Minerva McGonagall",
+    "file": "MinervaMcGonagall",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/6/65/ProfessorMcGonagall-HBP.jpg",
+    "route": "/harry_potter/minerva_mcgonagall"
+  },
+  {
+    "id": "hpchars9",
+    "title": "Draco Malfoy",
+    "file": "DracoMalfoy",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/7/7e/Draco_Malfoy_TDH.png",
+    "route": "/harry_potter/draco_malfoy"
+  },
+  {
+    "id": "hpchars10",
+    "title": "Neville Longbottom",
+    "file": "NevilleLongbottom",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/b/ba/Neville_Longbottom_DHF2_promo_2.jpg",
+    "route": "/harry_potter/neville_longbottom"
+  },
+  {
+    "id": "hpchars11",
+    "title": "Fred Weasley",
+    "file": "FredWeasley",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/9/9e/Fred_Weasley_promo_DHF1.jpg",
+    "route": "/harry_potter/fred_weasley"
+  },
+  {
+    "id": "hpchars12",
+    "title": "George Weasley",
+    "file": "GeorgeWeasley",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/b/b5/George_Weasley_promo_DHF1.jpg",
+    "route": "/harry_potter/george_weasley"
+  },
+  {
+    "id": "hpchars13",
+    "title": "Ginny Weasley",
+    "file": "GinnyWeasley",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/8/8b/Ginny_Weasley_hbp_promo.jpg",
+    "route": "/harry_potter/ginny_weasley"
+  },
+  {
+    "id": "hpchars14",
+    "title": "Sirius Black",
+    "file": "SiriusBlack",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/b/bc/OOTP_promo_front_closeup_Sirius_Black.jpg",
+    "route": "/harry_potter/sirius_black"
+  },
+  {
+    "id": "hpchars15",
+    "title": "Remus Lupin",
+    "file": "RemusLupin",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/e/e2/Remus_Lupin_Deathly_Hallows_promo_image.jpg",
+    "route": "/harry_potter/remus_lupin"
+  },
+  {
+    "id": "hpchars16",
+    "title": "Luna Lovegood",
+    "file": "LunaLovegood",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/e/ed/Luna_Lovegood.jpg",
+    "route": "/harry_potter/luna_lovegood"
+  },
+  {
+    "id": "hpchars17",
+    "title": "Dolores Umbridge",
+    "file": "DoloresUmbridge",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/4/4e/Dolores_Umbridge_DHF1_promotional_image.jpg",
+    "route": "/harry_potter/dolores_umbridge"
+  },
+  {
+    "id": "hpchars18",
+    "title": "Bellatrix Lestrange",
+    "file": "BellatrixLestrange",
+    "img": "https://static.wikia.nocookie.net/harrypotter/images/1/14/BellatrixLestrange.png",
+    "route": "/harry_potter/bellatrix_lestrange"
+  }
+]
