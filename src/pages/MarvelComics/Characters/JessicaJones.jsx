61529a86378839c9bed041d2485bc791f19c1369
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-05 16:00:22 -0800

    new files

---
+++ src/pages/MarvelComics/Characters/JessicaJones.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+
+const JessicaJones = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Jessica Jones - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/marvelcomics">Marvel Comics</Link></li>
+          <li>Jessica Jones</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Jessica Jones</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Jessica Jones</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/6/6e/Jessica_Jones_by_Mike_Mayhew.jpg" alt="Jessica Jones" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default JessicaJones;
