acbbfb078047e5135346763d0e57c3b5c14b1c0a
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-11 14:22:33 -0800

    update clba

--- src/pages/Mcu/Characters/ClintBartonMCU.jsx
+++ src/pages/Mcu/Characters/ClintBartonMCU.jsx
@@ -16,6 +16,85 @@ const ClintBartonMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life", sub2: [
+                {id: "1.1.1", title: "The son of a thief"},
+                {id: "1.1.2", title: "Day out at the fair"},
+                {id: "1.1.3", title: "Separated from his mother"}
+              ]},
+              {id: "1.2", title: "Joining S.H.I.E.L.D.", sub2: [
+                {id: "1.2.1", title: "Recruited by Nick Fury"},
+                {id: "1.2.2", title: "Recruiting Black Widow"},
+                {id: "1.2.3", title: "New Mexico mission"},
+                {id: "1.2.4", title: "Security check"}
+              ]},
+              {id: "1.3", title: "Chitauri invasion", sub2: [
+                {id: "1.3.1", title: "Destruction of P.E.G.A.S.U.S."},
+                {id: "1.3.2", title: "Controlled by Loki"},
+                {id: "1.3.3", title: "Attack on the Helicarrier"},
+                {id: "1.3.4", title: "Regaining control"},
+                {id: "1.3.5", title: "Battle of New York"},
+                {id: "1.3.6", title: "Avengers Assembled"},
+                {id: "1.3.7", title: "Eyes on Everything"},
+                {id: "1.3.8", title: "Last stand"},
+                {id: "1.3.9", title: "Invasion aftermath"}
+              ]},
+              {id: "1.4", title: "War on Hydra", sub2: [
+                {id: "1.4.1", title: "Fall of S.H.I.E.L.D."},
+                {id: "1.4.2", title: "Battle of Sudan"},
+                {id: "1.4.3", title: "Attack on the Hydra research base"},
+                {id: "1.4.4", title: "Treated by Doctor Cho"},
+                {id: "1.4.5", title: "The Avengers celebration"}
+              ]},
+              {id: "1.5", title: "Ultron Offensive", sub2: [
+                {id: "1.5.1", title: "Attack on Avengers Tower"},
+                {id: "1.5.2", title: "Avengers argument"},
+                {id: "1.5.3", title: "Looking for Ultron"},
+                {id: "1.5.4", title: "Battle at the Salvage Yard"},
+                {id: "1.5.5", title: "Returning home"},
+                {id: "1.5.6", title: "Place on the Avengers"},
+                {id: "1.5.7", title: "Nick Fury's return"},
+                {id: "1.5.8", title: "One last project"}
+              ]},
+              {id: "1.6", title: "Striking back", sub2: [
+                {id: "1.6.1", title: "Battle of Seoul"},
+                {id: "1.6.2", title: "Birth of Vision"},
+                {id: "1.6.3", title: "Battle of Sokovia"},
+                {id: "1.6.4", title: "Helping Wanda Maximoff"},
+                {id: "1.6.5", title: "Reinforcements"},
+                {id: "1.6.6", title: "Protecting the key"},
+                {id: "1.6.7", title: "Rival turned savior"},
+                {id: "1.6.8", title: "Retirement with family"}
+              ]},
+              {id: "1.7", title: "replace"},
+              {id: "1.8", title: "replace"},
+              {id: "1.9", title: "replace"},
+              {id: "1.10", title: "replace"},
+              {id: "1.11", title: "replace"},
+              {id: "1.12", title: "replace"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#2">Abilities</a></li>
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Uniforms"},
+              {id: "3.2", title: "Weapons"},
+              {id: "3.3", title: "Other equipment"},
+              {id: "3.4", title: "Vehicles"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Facilities"
+            sub={[
+              {id: "4.1", title: "Prisons"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
