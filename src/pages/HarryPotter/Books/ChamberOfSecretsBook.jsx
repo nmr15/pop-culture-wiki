914090ec6239036f7ac4c53f36a853db5d54265f
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-16 14:18:37 -0800

    new files

---
+++ src/pages/HarryPotter/Books/ChamberOfSecretsBook.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+
+const ChamberOfSecretsBook = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Harry Potter and the Chamber of Secrets (book) - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/hpbooks">Harry Potter</Link></li>
+          <li>Harry Potter and the Chamber of Secrets (book)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Harry Potter and the Chamber of Secrets (book)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Harry Potter and the Chamber of Secrets</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://static.wikia.nocookie.net/harrypotter/images/3/3e/Harry_Potter_and_the_Sorcerer%27s_Stone_cover.jpg" alt="Harry Potter and the Chamber of Secrets cover" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default ChamberOfSecretsBook;
