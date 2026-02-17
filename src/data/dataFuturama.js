972be3feb5cb687fb5c12b94c04878558b439a77
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-17 11:22:54 -0800

    new array

--- src/data/dataFuturama.js
+++ src/data/dataFuturama.js
@@ -63,3 +63,326 @@ export const futuramaChars = [
     "route": "/futurama/kif_kroker"
   }
 ]
+
+export const futuramaEps = [
+  {
+    "id": "ftrmas1e1",
+    "title": "Space Pilot 3000",
+    "file": "FuturamaS1E1",
+    "img": "",
+    "season": 1,
+    "episode": 1,
+    "desc": "",
+    "route": "/futurama/space_pilot_3000"
+  },
+  {
+    "id": "ftrmas1e2",
+    "title": "The Series Has Landed",
+    "file": "FuturamaS1E2",
+    "img": "",
+    "season": 1,
+    "episode": 2,
+    "desc": "",
+    "route": "/futurama/the_series_has_landed"
+  },
+  {
+    "id": "ftrmas1e3",
+    "title": "I, Roomate",
+    "file": "FuturamaS1E3",
+    "img": "",
+    "season": 1,
+    "episode": 3,
+    "desc": "",
+    "route": "/futurama/i_roommate"
+  },
+  {
+    "id": "ftrmas1e4",
+    "title": "Love's Labours Lost in Space",
+    "file": "FuturamaS1E4",
+    "img": "",
+    "season": 1,
+    "episode": 4,
+    "desc": "",
+    "route": "/futurama/loves_labours_lost_in_space"
+  },
+  {
+    "id": "ftrmas1e5",
+    "title": "Fear of a Bot Planet",
+    "file": "FuturamaS1E5",
+    "img": "",
+    "season": 1,
+    "episode": 5,
+    "desc": "",
+    "route": "/futurama/fear_of_a_bot_planet"
+  },
+  {
+    "id": "ftrmas1e6",
+    "title": "A Fishfull of Dollars",
+    "file": "FuturamaS1E6",
+    "img": "",
+    "season": 1,
+    "episode": 6,
+    "desc": "",
+    "route": "/futurama/a_fishfull_of_dollars"
+  },
+  {
+    "id": "ftrmas1e7",
+    "title": "My Three Suns",
+    "file": "FuturamaS1E7",
+    "img": "",
+    "season": 1,
+    "episode": 7,
+    "desc": "",
+    "route": "/futurama/my_three_suns"
+  },
+  {
+    "id": "ftrmas1e8",
+    "title": "A Big Piece of Garbage",
+    "file": "FuturamaS1E8",
+    "img": "",
+    "season": 1,
+    "episode": 8,
+    "desc": "",
+    "route": "/futurama/a_big_piece_of_garbage"
+  },
+  {
+    "id": "ftrmas1e9",
+    "title": "Hell is Other Robots",
+    "file": "FuturamaS1E9",
+    "img": "",
+    "season": 1,
+    "episode": 9,
+    "desc": "",
+    "route": "/futurama/hell_is_other_robots"
+  },
+  {
+    "id": "ftrmas1e10",
+    "title": "A Flight to Remember",
+    "file": "FuturamaS1E10",
+    "img": "",
+    "season": 1,
+    "episode": 10,
+    "desc": "",
+    "route": "/futurama/a_flight_to_remember"
+  },
+  {
+    "id": "ftrmas1e11",
+    "title": "Mars University",
+    "file": "FuturamaS1E11",
+    "img": "",
+    "season": 1,
+    "episode": 11,
+    "desc": "",
+    "route": "/futurama/mars_university"
+  },
+  {
+    "id": "ftrmas1e12",
+    "title": "When Aliens Attack",
+    "file": "FuturamaS1E12",
+    "img": "",
+    "season": 1,
+    "episode": 12,
+    "desc": "",
+    "route": "/futurama/when_aliens_attack"
+  },
+  {
+    "id": "ftrmas1e13",
+    "title": "Fry and the Slurm Factory",
+    "file": "FuturamaS1E13",
+    "img": "",
+    "season": 1,
+    "episode": 13,
+    "desc": "",
+    "route": "/futurama/fry_and_the_slurm_factory"
+  },
+  {
+    "id": "ftrmas2e1",
+    "title": "I Second That Emotion",
+    "file": "FuturamaS2E1",
+    "img": "",
+    "season": 2,
+    "episode": 14,
+    "desc": "",
+    "route": "/futurama/i_second_that_emotion"
+  },
+  {
+    "id": "ftrmas2e2",
+    "title": "Brannigan, Begin Again",
+    "file": "FuturamaS2E2",
+    "img": "",
+    "season": 2,
+    "episode": 15,
+    "desc": "",
+    "route": "/futurama/brannigan_begin_again"
+  },
+  {
+    "id": "ftrmas2e3",
+    "title": "A Head in the Polls",
+    "file": "FuturamaS2E3",
+    "img": "",
+    "season": 2,
+    "episode": 16,
+    "desc": "",
+    "route": "/futurama/a_head_in_the_polls"
+  },
+  {
+    "id": "ftrmas2e4",
+    "title": "Xmas Story",
+    "file": "FuturamaS2E4",
+    "img": "",
+    "season": 2,
+    "episode": 17,
+    "desc": "",
+    "route": "/futurama/xmas_story"
+  },
+  {
+    "id": "ftrmas2e5",
+    "title": "Why Must I Be a Crustacean in Love?",
+    "file": "FuturamaS2E5",
+    "img": "",
+    "season": 2,
+    "episode": 18,
+    "desc": "",
+    "route": "/futurama/why_must_i_be_a_crustacean_in_love"
+  },
+  {
+    "id": "ftrmas2e6",
+    "title": "The Lesser of Two Evils",
+    "file": "FuturamaS2E6",
+    "img": "",
+    "season": 2,
+    "episode": 19,
+    "desc": "",
+    "route": "/futurama/the_lesser_of_two_evils"
+  },
+  {
+    "id": "ftrmas2e7",
+    "title": "Put Your Head on My Shoulders",
+    "file": "FuturamaS2E7",
+    "img": "",
+    "season": 2,
+    "episode": 20,
+    "desc": "",
+    "route": "/futurama/put_your_head_on_my_shoulders"
+  },
+  {
+    "id": "ftrmas2e8",
+    "title": "Raging Bender",
+    "file": "FuturamaS2E8",
+    "img": "",
+    "season": 2,
+    "episode": 21,
+    "desc": "",
+    "route": "/futurama/raging_bender"
+  },
+  {
+    "id": "ftrmas2e9",
+    "title": "A Bicyclops Built for Two",
+    "file": "FuturamaS2E9",
+    "img": "",
+    "season": 2,
+    "episode": 22,
+    "desc": "",
+    "route": "/futurama/a_bicyclops_built_for_two"
+  },
+  {
+    "id": "ftrmas2e10",
+    "title": "A Clone of My Own",
+    "file": "FuturamaS2E10",
+    "img": "",
+    "season": 2,
+    "episode": 23,
+    "desc": "",
+    "route": "/futurama/a_clone_of_my_own"
+  },
+  {
+    "id": "ftrmas2e11",
+    "title": "How Hermes Reacquisitioned His Groove Back",
+    "file": "FuturamaS2E11",
+    "img": "",
+    "season": 2,
+    "episode": 24,
+    "desc": "",
+    "route": "/futurama/how_hermes_reacquisitioned_his_groove_back"
+  },
+  {
+    "id": "ftrmas2e12",
+    "title": "The Deep South",
+    "file": "FuturamaS2E12",
+    "img": "",
+    "season": 2,
+    "episode": 25,
+    "desc": "",
+    "route": "/futurama/the_deep_south"
+  },
+  {
+    "id": "ftrmas2e13",
+    "title": "Bender Gets Made",
+    "file": "FuturamaS2E13",
+    "img": "",
+    "season": 2,
+    "episode": 26,
+    "desc": "",
+    "route": "/futurama/bender_gets_made"
+  },
+  {
+    "id": "ftrmas2e14",
+    "title": "Mother's Day",
+    "file": "FuturamaS2E14",
+    "img": "",
+    "season": 2,
+    "episode": 27,
+    "desc": "",
+    "route": "/futurama/mother's_day"
+  },
+  {
+    "id": "ftrmas2e15",
+    "title": "The Problem with Popplers",
+    "file": "FuturamaS2E15",
+    "img": "",
+    "season": 2,
+    "episode": 28,
+    "desc": "",
+    "route": "/futurama/the_problem_with_popplers"
+  },
+  {
+    "id": "ftrmas2e16",
+    "title": "Anthology of Interest I",
+    "file": "FuturamaS2E16",
+    "img": "",
+    "season": 2,
+    "episode": 29,
+    "desc": "",
+    "route": "/futurama/anthology_of_interest_i"
+  },
+  {
+    "id": "ftrmas2e17",
+    "title": "War is the H-Word",
+    "file": "FuturamaS2E17",
+    "img": "",
+    "season": 2,
+    "episode": 30,
+    "desc": "",
+    "route": "/futurama/war_is_the_h-word"
+  },
+  {
+    "id": "ftrmas2e18",
+    "title": "The Honking",
+    "file": "FuturamaS2E18",
+    "img": "",
+    "season": 2,
+    "episode": 31,
+    "desc": "",
+    "route": "/futurama/the_honking"
+  },
+  {
+    "id": "ftrmas2e19",
+    "title": "The Cryonic Woman",
+    "file": "FuturamaS2E19",
+    "img": "",
+    "season": 2,
+    "episode": 32,
+    "desc": "",
+    "route": "/futurama/the_cryonic_woman"
+  }
+]
