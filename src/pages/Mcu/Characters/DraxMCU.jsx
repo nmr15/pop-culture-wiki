6bb773f368155a607d01df1abef65825a52fc2ed
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-17 10:19:04 -0800

    update drax

--- src/pages/Mcu/Characters/DraxMCU.jsx
+++ src/pages/Mcu/Characters/DraxMCU.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 
 const DraxMCU = () =>
 {
@@ -13,6 +15,31 @@ const DraxMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+
+          <Collapse 
+            id="2"
+            item="Powers and abilities"
+            sub={[
+              {id: "2.1", title: "Powers"},
+              {id: "2.2", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Weapons"},
+              {id: "3.2", title: "Other Equipment"},
+              {id: "3.3", title: "Vehicles"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Facilities"
+            sub={[
+              {id: "4.1", title: "Prisons"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
