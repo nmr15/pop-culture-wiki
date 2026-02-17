6a08b80bf69de2a2cfc79f1cdc6875b7f2ddd41c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-17 13:58:31 -0800

    update druig

--- src/pages/Mcu/Characters/DruigMCU.jsx
+++ src/pages/Mcu/Characters/DruigMCU.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
 
 const DruigMCU = () =>
 {
@@ -13,6 +15,50 @@ const DruigMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Creation"},
+              {id: "1.2", title: "Protecting humanity", sub2: [
+                {id: "1.2.1", title: "Arriving on Earth"},
+                {id: "1.2.2", title: "Battle of Helen-Kyo"},
+                {id: "1.2.3", title: "Hide and seek"},
+                {id: "1.2.4", title: "Defeating Kapa"},
+                {id: "1.2.5", title: "Retelling Helen-Kyo"},
+                {id: "1.2.6", title: "Fall of Tenochtitian"},
+                {id: "1.2.7", title: "Starting a community"}
+              ]},
+              {id: "1.3", title: "The Emergence", sub2: [
+                {id: "1.3.1", title: "Reuniting with the Eternals"},
+                {id: "1.3.2", title: "Ambush at the compound"},
+                {id: "1.3.3", title: "Returning to the Domo"},
+                {id: "1.3.4", title: "Stopping the Emergence"}
+              ]},
+              {id: "1.4", title: "Finding other Eternals", sub2: [
+                {id: "1.4.1", title: "Meeting Starfox"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Powers and abilities"
+            sub={[
+              {id: "2.1", title: "Powers"},
+              {id: "2.2", title: "Former powers"},
+              {id: "2.3", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Weapons"},
+              {id: "3.2", title: "Other equipment"},
+              {id: "3.3", title: "Vehicles"},
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -38,8 +84,12 @@ const DruigMCU = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data"><Link>Barry Keoghan</Link></td>
+            </tr>
+            <tr>
+              <td className="infobox-data">Appearances</td>
+              <td className="infobox-data"><Link to="/mcu/eternals">Eternals</Link></td>
             </tr>
           </tbody>
         </table>
