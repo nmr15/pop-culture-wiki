02b4dfad10cff45c874597df1e1049ba46c39e23
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-16 16:22:59 -0800

    update drax

--- src/pages/Mcu/Characters/DraxMCU.jsx
+++ src/pages/Mcu/Characters/DraxMCU.jsx
@@ -38,8 +38,21 @@ const DraxMCU = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data"><Link>Dave Bautistia</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Appearances by</th>
+              <td className="infobox-data">
+                <Link to="/mcu/guardians_of_the_galaxy_film">Guardians of the Galaxy</Link> <br />
+                <Link to="/mcu/guardians_of_the_galaxy_vol_2">Guardians of the Galaxy Vol. 2</Link> <br />
+                <Link to="/mcu/avengers_infinity_war">Avengers: Infinity War</Link> <br />
+                <Link to="/mcu/avengers_endgame">Avengers: Endgame</Link> <br />
+                <Link to="/mcu/thor_love_and_thunder">Thor: Love and Thunder</Link> <br />
+                <Link>I am Groot</Link> (1 episode) <br />
+                <Link to="/mcu/the_guardians_of_the_galaxy_holiday_special">The Guardians of the Galaxy Holiday Special</Link>
+                <Link to="/mcu/guardians_of_the_galaxy_vol_3">Guardians of the Galaxy Vol. 3</Link>
+              </td>
             </tr>
           </tbody>
         </table>
