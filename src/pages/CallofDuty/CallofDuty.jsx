8b5fc8e46e145382caab9aae64390aa0edd9df64
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-18 11:14:04 -0800

    update cod

--- src/pages/CallofDuty/CallofDuty.jsx
+++ src/pages/CallofDuty/CallofDuty.jsx
@@ -7,7 +7,7 @@ const CallofDuty = () =>
 {
   useEffect(() =>
   {
-    document.title = "Call of Duty - Pop Culture Wiki";
+    document.title = "Call of Duty (franchise) - Pop Culture Wiki";
   }, []);
 
   return(
@@ -20,10 +20,10 @@ const CallofDuty = () =>
       <main className="main" id="top">
         <ul className="breadcrumb">
           <li><Link to="/">Home</Link></li>
-          <li>Call of Duty</li>
+          <li>Call of Duty (franchise)</li>
         </ul>
         <div>
-          <h1 className="article-heading">Call of Duty</h1>
+          <h1 className="article-heading">Call of Duty (franchise)</h1>
         </div>
         <table className="infobox">
           <tbody>
@@ -55,7 +55,12 @@ const CallofDuty = () =>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>Call of Duty (CoD)</b> is a first-person shooter military video game series and media franchise published by <Link>Activision</Link>,
+            starting in 2003. The games were first developed by <Link>Infinity Ward</Link>, then by <Link>Treyarch</Link> and Sledgehammer Games.
+            Several spin-off and handheld games were made by other developers. The most recent, <Link>Call of Dity: Black Ops 7</Link>, was released on
+            November 14, 2025.
+          </p>
         </div>
       </main>
     </>
