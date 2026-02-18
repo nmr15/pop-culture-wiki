5dd71b132034060fb6c9fdbba1a919868b679481
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-18 10:05:10 -0800

    update battlefield

--- src/pages/Battlefield/Battlefield.jsx
+++ src/pages/Battlefield/Battlefield.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
 
 const Battlefield = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Battlefield (franchise) - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
       <aside className="sidebar">
@@ -22,13 +29,26 @@ const Battlefield = () =>
         <div>
           <h1 className="article-heading">Battlefield (franchise)</h1>
         </div>
-        <div className="img-right img-top">
-          <img 
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/BattlefieldLogo.svg/500px-BattlefieldLogo.svg.png" 
-            alt="Battlefield logo" 
-            className="article-heading-img"
-          />
-        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Battlefield</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/BattlefieldLogo.svg/500px-BattlefieldLogo.svg.png" alt="Battlefield logo" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
     </>
   )
