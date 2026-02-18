b6b376b457473c3625a25d8ad5758f421bde2b4e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-18 14:46:07 -0800

    update cod

--- src/pages/CallofDuty/CallofDuty.jsx
+++ src/pages/CallofDuty/CallofDuty.jsx
@@ -15,18 +15,48 @@ const CallofDuty = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
-
+          <Collapse 
+            id="1"
+            item="Main series"
+            sub={[
+              {id: "1.1", title: "World War II games", sub2: [
+                {id: "1.1.1", title: "Call of Duty"},
+                {id: "1.1.2", title: "Call of Duty 2"},
+                {id: "1.1.3", title: "Call of Duty 3"},
+                {id: "1.1.4", title: "Call of Duty: WWII"},
+                {id: "1.1.5", title: "Call of Duty: Vanguard"}
+              ]},
+              {id: "1.2", title: "Modern Warfare series", sub2: [
+                {id: "1.2.1", title: "Original trilogy", sub3: [
+                  {id: "1.2.1.1", title: "Call of Duty 4: Modern Warfare"},
+                  {id: "1.2.1.2", title: "Call of Duty: Modern Warfare Remastered"},
+                  {id: "1.2.1.3", title: "Call of Duty: Modern Warfare 2"},
+                  {id: "1.2.1.4", title: "Call of Duty: Modern Warfare 2 Campaign Remastered"},
+                  {id: "1.2.1.5", title: "Call of Duty: Modern Warfare 3"},
+                ]},
+                {id: "1.2.2", title: "Reboot", sub3: [
+                  {id: "1.2.2.1", title: "Call of Duty: Modern Warfare"},
+                  {id: "1.2.2.2", title: "Call of Duty: Modern Warfare II"},
+                  {id: "1.2.2.3", title: "Call of Duty: Modern Warfare III"}
+                ]}
+              ]},
+              {id: "1.3", title: "Black Ops series", sub2: [
+                {id: "1.3."}
+              ]},
+              {id: "1.4", title: "Standalone games"}
+            ]}
+          />
           <li className="sidebar-list-item"><a href="#2">Primary developer rotation</a></li>
           <Collapse 
             id="3"
             item="Free-to-play games"
             sub={[
-              {id: "3.", title: "Call of Duty: "},
-              {id: "3.", title: "Call of Duty: "},
-              {id: "3.", title: "Call of Duty: "},
-              {id: "3.", title: "Call of Duty: "},
-              {id: "3.", title: "Call of Duty: "},
-              {id: "3.", title: "Call of Duty: "}
+              {id: "3.", title: "Call of Duty: Online"},
+              {id: "3.", title: "Call of Duty: Heroes"},
+              {id: "3.", title: "Call of Duty: Mobile"},
+              {id: "3.", title: "Call of Duty: Warzone (2020)"},
+              {id: "3.", title: "Call of Duty: Warzone (2022)"},
+              {id: "3.", title: "Call of Duty: Warzone Mobile"}
             ]}
           />
         </ul>
