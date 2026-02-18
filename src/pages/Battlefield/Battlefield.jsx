d86eb6ce642048996788631d44562a5d3d1e13d4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-18 13:39:41 -0800

    update batt

--- src/pages/Battlefield/Battlefield.jsx
+++ src/pages/Battlefield/Battlefield.jsx
@@ -77,7 +77,39 @@ const Battlefield = () =>
         <div>
           <Heading2 id={1} title={"Gameplay"} />
           <div>
-
+            <p>
+              The Battlefield games usually focus on large-sale, online multiplayer battles between two conflicting military forces; in Battlefield
+              Hardline, the military setting is replaced with a "war on crime" between the police Special Response Units and the criminals. Playing in
+              squads has become a major element of games in the series. Apart from infantry, tanks, airplanes, and other vehicles may participate in
+              these battles. Since <Link to="/battlefield/battlefield_2">Battlefield 2</Link>, the series centrally recorded online stats for each player,
+              allowing users to receive rank promotions and weapon unlocks based on their performance as well as awards such as medals, ribbons, and pins.
+              While continuing to focus on large, online multiplayer battles, 2006's <Link to="/battlefield/bad_company">Battlefield: Bad
+              Company</Link> intoduced full single-player campaigns with more fleshed-out storylines. It was also the first game to use th
+              studio-developed game engine, <Link>Frostbite</Link>, which introduced almost fully destructible environments; a feature that has since
+              become one of the most well-known staples of the series.
+            </p>
+            <p>
+              The series' flagship game mode is "Conquest", where two teams of players attempt to gain control of the most control points until the
+              other team's tickets (respawns) run out. To capture a control point, players must stand within the capture area until their team captures
+              it. Should a control point be captured already by the enemy team, or should any enemy players contest it, players must eliminate all
+              enemy players and/or neutralize the control point first by standing within the capture area. Captured control points may act as respawn
+              points for players, though they can also respawn on a squad member who are not in combat and specialized equipment carried by certain
+              classes. Vehicles cannot spawn at a command post and must be obtained from the team's "headquarters", which act as the team's main spawn
+              point. <Link to="/battlefield/battlefield_v">Battlefield V</Link> introduced the battle royale game mode to the Battlefield series, which
+              marges the francise's "core pillars of destruction, team play, and vehicles" with common elements seen in traditional battle royales, and
+              <Link to="/battlefield/battlefield_6"> Battlefield 6</Link> introduces a knockout elimination mode titled "Gauntlet" where eight squads of
+              four players seek to complete four sets of objectives while trying not to fall into the bottom two teams.
+            </p>
+            <p>
+              Outside of early version of <Link to="/battlefield/battlefield_2042">Battlefield 2042</Link>, the Battlefield series features a class
+              system. Different classes are suited to different roles in combat. Historically, the series featured the Anti-tank, Demolition, Medic,
+              Specialist, and Special Forces classes, before these were merged into other classes in later games. The classes as seen in modern titles
+              was first introduced in <Link to="/battlefield/bad_company_2">Battlefield: Bad Company 2</Link>: the Assault, who specializes in all-round
+              combat, Engineer, who carries out in-field repairs to vehicles while being equipped with anti-vehicular weapons, Medic (later Support), who
+              resupplies and revives fallen allies, and Recon, who specializes in spotting enemies using a sniper rifle to engage in long-distance combat.
+              The ability to engage other players in melee combat with a knife has been present in Battlefield games, but since Battlefield 2142, the
+              series has included an award of dog tags for each player killed using a knife.
+            </p>
           </div>
         </div>
         <div>
