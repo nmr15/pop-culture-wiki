cb232c28062e644db323c084e2840e7ca87cfa4a
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-19 14:23:57 -0800

    update ram

--- src/pages/RickandMorty/RickandMorty.jsx
+++ src/pages/RickandMorty/RickandMorty.jsx
@@ -15,6 +15,58 @@ const RickandMorty = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Premise and main characters</a></li>
+          <li className="sidebar-list-item"><a href="#2">Episodes</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Writing"},
+              {id: "3.3", title: "Animation and voice recording"},
+              {id: "3.4", title: "Roiland's firing"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Themes and analysis"
+            sub={[
+              {id: "4.1", title: "Comedic style"},
+              {id: "4.2", title: "Philosophy"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Release"
+            sub={[
+              {id: "5.1", title: "Broadcast"},
+              {id: "5.2", title: "Streaming service and home media"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Reception"
+            sub={[
+              {id: "6.1", title: "Critical response"},
+              {id: "6.2", title: "Ratings"},
+              {id: "6.3", title: "Industry impact"},
+              {id: "6.4", title: "Awards and nominations"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Other media and products"
+            sub={[
+              {id: "7.1", title: "Possible feature film"},
+              {id: "7.2", title: "Spin-off series"},
+              {id: "7.3", title: "Cameos", sub2: [
+                {id: "7.3.1", title: "Film"},
+                {id: "7.3.2", title: "Television"}
+              ]},
+              {id: "7.4", title: "Comics"},
+              {id: "7.5", title: "Product tie-ins"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
