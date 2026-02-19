3b61cac2cc4a8176e0b6e9e255fcec684cc34b6f
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-19 14:03:17 -0800

    update gum

--- src/pages/Gumball/GumballSeries.jsx
+++ src/pages/Gumball/GumballSeries.jsx
@@ -21,7 +21,38 @@ const GumballSeries = () =>
             item="Episodes"
             sub={[
               {id: "2.1", title: "Crossovers"},
-              {id: "2.2", title: "Shorts and miniseries"}
+              {id: "2.2", title: "Shorts and miniseries", sub2: [
+                {id: "2.2.1", title: "Waiting for Gumball"},
+                {id: "2.2.2", title: "Darwin's Yearbook"},
+                {id: "2.2.3", title: "The Gumball Chronicles"}
+              ]}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#3">Production</a></li>
+          <Collapse 
+            id="4"
+            item="Reception"
+            sub={[
+              {id: "4.1", title, "Ratings"},
+              {id: "4.2", title, "Critical reception"},
+              {id: "4.3", title, "Awards and nominations"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Related media"
+            sub={[
+              {id: "5.1", title: "Comics"},
+              {id: "5.2", title: "Home media"},
+              {id: "5.3", title: "Video games"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Future"
+            sub={[
+              {id: "6.1", title: "Continuation"},
+              {id: "6.2", title: "Feature film"}
             ]}
           />
         </ul>
@@ -48,9 +79,17 @@ const GumballSeries = () =>
                 </span>
               </td>
             </tr>
+             <tr>
+              <th className="infobox-data">Created by</th>
+              <td className="infobox-data">Ben Bocquelet</td>
+            </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Episodes</th>
+              <td className="infobox-data"><Link>List of The Amazing World of Gumball episodes</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link>List of The Amazing World of Gumball character</Link></td>
             </tr>
           </tbody>
         </table>
