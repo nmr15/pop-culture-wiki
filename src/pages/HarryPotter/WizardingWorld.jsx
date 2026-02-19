b5df9693aa63a5ce48c975dbf79562667d92d9b8
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-19 14:36:15 -0800

    update wix

--- src/pages/HarryPotter/WizardingWorld.jsx
+++ src/pages/HarryPotter/WizardingWorld.jsx
@@ -1,8 +1,54 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
+
 const WizardingWorld = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Wizarding World - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li>Wizarding World</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Wizarding World</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Wizarding World</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e4/Wizarding_World_logo.svg/500px-Wizarding_World_logo.svg.png" alt="Wizarding World" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
