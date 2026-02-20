8aab558f4b35d0b3bbf9ae994ed87a8788fd4736
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-20 14:29:25 -0800

    new files

---
+++ src/pages/Futurama/Characters/TurangaLeela.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+
+const TurangaLeela = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Turanga Leela - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/futurama">Futurama</Link></li>
+          <li>Turanga Leela</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Turanga Leela</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Turanga Leela</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://static.wikia.nocookie.net/enfuturama/images/3/3d/Character_Leela.png" alt="Turanga Leela" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Template</th>
+              <td className="infobox-data">Template</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default TurangaLeela;
