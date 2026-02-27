6f115cece9ac14fab5d454985110428c7c7d15b9
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-02-27 11:58:22 -0800

    new array

---
+++ src/data/dataSims.js
@@ -0,0 +1,461 @@
+export const simsGames = [
+  {
+    "id": "sims1",
+    "title": "The Sims",
+    "file": "TheSimsGame",
+    "img": "",
+    "type": "full",
+    "game": 1,
+    "route": "/the_sims/the_sims_game"
+  },
+  {
+    "id": "sims2",
+    "title": "The Sims: Livin' Large",
+    "file": "TheSimsLivinLarge",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_livin'_large"
+  },
+  {
+    "id": "sims3",
+    "title": "The Sims: House Party",
+    "file": "TheSimsHouseParty",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_house_party"
+  },
+  {
+    "id": "sims4",
+    "title": "The Sims: Hot Date",
+    "file": "TheSimsHotDate",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_hot_date"
+  },
+  {
+    "id": "sims5",
+    "title": "The Sims: Vacation",
+    "file": "TheSimsVacation",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_vacation"
+  },
+  {
+    "id": "sims6",
+    "title": "The Sims: Unleashed",
+    "file": "TheSimsUnleashed",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_unleashed"
+  },
+  {
+    "id": "sims7",
+    "title": "The Sims: Superstar",
+    "file": "TheSimsSuperstar",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_superstar"
+  },
+  {
+    "id": "sims8",
+    "title": "The Sims: Makin' Magic",
+    "file": "TheSimsMakinMagic",
+    "img": "",
+    "type": "expansion",
+    "game": 1,
+    "route": "/the_sims/the_sims_makin'_magic"
+  },
+  {
+    "id": "sims9",
+    "title": "The Sims 2",
+    "file": "TheSims2",
+    "img": "",
+    "type": "full",
+    "game": 2,
+    "route": "/the_sims/the_sims_2"
+  },
+  {
+    "id": "sims10",
+    "title": "The Sims 2: University",
+    "file": "TheSims2University",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_university"
+  },
+  {
+    "id": "sims11",
+    "title": "The Sims 2: Nightlife",
+    "file": "TheSims2Nightlife",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_nightlife"
+  },
+  {
+    "id": "sims12",
+    "title": "The Sims 2: Open for Business",
+    "file": "TheSims2OpenForBusiness",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_open_for_business"
+  },
+  {
+    "id": "sims13",
+    "title": "The Sims 2: Pets",
+    "file": "TheSims2Pets",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_pets"
+  },
+  {
+    "id": "sims14",
+    "title": "The Sims 2: Seasons",
+    "file": "TheSimsS2easons",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_seasons"
+  },
+  {
+    "id": "sims15",
+    "title": "The Sims 2: Bon Voyage",
+    "file": "TheSims2BonVoyage",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_bon_voyage"
+  },
+  {
+    "id": "sims16",
+    "title": "The Sims 2: FreeTime",
+    "file": "TheSims2FreeTime",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_free_time"
+  },
+  {
+    "id": "sims17",
+    "title": "The Sims 2: Apartment Life",
+    "file": "TheSims2ApartmentLife",
+    "img": "",
+    "type": "expansion",
+    "game": 2,
+    "route": "/the_sims/the_sims_2_apartment_life"
+  },
+  {
+    "id": "sims18",
+    "title": "The Sims 3",
+    "file": "TheSims3",
+    "img": "",
+    "type": "full",
+    "game": 3,
+    "route": "/the_sims/the_sims_3"
+  },
+  {
+    "id": "sims19",
+    "title": "The Sims 3: World Adventures",
+    "file": "TheSims3WorldAdventures",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_world_adventures"
+  },
+  {
+    "id": "sims20",
+    "title": "The Sims 3: Ambitions",
+    "file": "TheSims3Ambitions",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_ambitions"
+  },
+  {
+    "id": "sims21",
+    "title": "The Sims 3: Late Night",
+    "file": "TheSims3LateNight",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_late_night"
+  },
+  {
+    "id": "sims22",
+    "title": "The Sims 3: Generations",
+    "file": "TheSims3Generations",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_generations"
+  },
+  {
+    "id": "sims23",
+    "title": "The Sims 3: Pets",
+    "file": "TheSims3Pets",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_pets"
+  },
+  {
+    "id": "sims24",
+    "title": "The Sims 3: Showtime",
+    "file": "TheSims3Showtime",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_showtime"
+  },
+  {
+    "id": "sims25",
+    "title": "The Sims 3: Supernatural",
+    "file": "TheSims3Supernatural",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_supernatural"
+  },
+  {
+    "id": "sims26",
+    "title": "The Sims 3: Seasons",
+    "file": "TheSims3Seasons",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_seasons"
+  },
+  {
+    "id": "sims27",
+    "title": "The Sims 3: University Life",
+    "file": "TheSims3UniversityLife",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_university_life"
+  },
+  {
+    "id": "sims28",
+    "title": "The Sims 3: Island Paradise",
+    "file": "TheSims3Island Paradise",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_island_paradise"
+  },
+  {
+    "id": "sims29",
+    "title": "The Sims 3: Into the Future",
+    "file": "TheSims3IntoTheFuture",
+    "img": "",
+    "type": "expansion",
+    "game": 3,
+    "route": "/the_sims/the_sims_3_into_the_future"
+  },
+  {
+    "id": "sims30",
+    "title": "The Sims 4",
+    "file": "TheSims4",
+    "img": "",
+    "type": "full",
+    "game": 4,
+    "route": "/the_sims/the_sims_4"
+  },
+  {
+    "id": "sims31",
+    "title": "The Sims 4: Get to Work",
+    "file": "TheSims4GetToWork",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_get_to_work"
+  },
+  {
+    "id": "sims32",
+    "title": "The Sims 4: Get Together",
+    "file": "TheSims4GetTogether",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_get_together"
+  },
+  {
+    "id": "sims33",
+    "title": "The Sims 4: City Living",
+    "file": "TheSims4CityLiving",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_city_living"
+  },
+  {
+    "id": "sims34",
+    "title": "The Sims 4: Cats & Dogs",
+    "file": "TheSims4CatsDogs",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_cats_&_dogs"
+  },
+  {
+    "id": "sims35",
+    "title": "The Sims 4: Seasons",
+    "file": "TheSims4Seasons",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_seasons"
+  },
+  {
+    "id": "sims36",
+    "title": "The Sims 4: Get Famous",
+    "file": "TheSims4GetFamous",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_get_famous"
+  },
+  {
+    "id": "sims37",
+    "title": "The Sims 4: Island Living",
+    "file": "TheSims4IslandLiving",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_island_living"
+  },
+  {
+    "id": "sims38",
+    "title": "The Sims 4: Discover University",
+    "file": "TheSims4DiscoverUniversity",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_discover_university"
+  },
+  {
+    "id": "sims39",
+    "title": "The Sims 4: Eco Lifestyle",
+    "file": "TheSims4EcoLifestyle",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_eco_lifestyle"
+  },
+  {
+    "id": "sims40",
+    "title": "The Sims 4: Snowy Escape",
+    "file": "TheSims4SnowyEscape",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_snowy_escape"
+  },
+  {
+    "id": "sims41",
+    "title": "The Sims 4: Cottage Living",
+    "file": "TheSims4CottageLiving",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_cottage_living"
+  },
+  {
+    "id": "sims42",
+    "title": "The Sims 4: High School Years",
+    "file": "TheSims4HighSchoolYears",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_high_school_years"
+  },
+  {
+    "id": "sims43",
+    "title": "The Sims 4: Growing Together",
+    "file": "TheSims4GrowingTogether",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_growing_together"
+  },
+  {
+    "id": "sims44",
+    "title": "The Sims 4: Horse Ranch",
+    "file": "TheSims4HorseRanch",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_horse_ranch"
+  },
+  {
+    "id": "sims45",
+    "title": "The Sims 4: For Rent",
+    "file": "TheSims4ForRent",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_for_rent"
+  },
+  {
+    "id": "sims46",
+    "title": "The Sims 4: Lovestruck",
+    "file": "TheSims4Lovestruck",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_lovestruck"
+  },
+  {
+    "id": "sims47",
+    "title": "The Sims 4: Life & Death",
+    "file": "TheSims4LifeDeath",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_life_&_death"
+  },
+  {
+    "id": "sims48",
+    "title": "The Sims 4: Business & Hobbbies",
+    "file": "TheSims4BusinessHobbies",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_business_&_hobbies"
+  },
+  {
+    "id": "sims49",
+    "title": "The Sims 4: Enchanted By Nature",
+    "file": "TheSims4EnchantedByNature",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_enchanted_by_nature"
+  },
+  {
+    "id": "sims50",
+    "title": "The Sims 4: Adventure Awaits",
+    "file": "TheSims4AdventureAwaits",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_adventure_awaits"
+  },
+  {
+    "id": "sims51",
+    "title": "The Sims 4: Royalty & Legacy",
+    "file": "TheSims4RoyaltyLegacy",
+    "img": "",
+    "type": "expansion",
+    "game": 4,
+    "route": "/the_sims/the_sims_4_royalty_&_legacy"
+  }
+]
