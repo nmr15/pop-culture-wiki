313702200acc1bcf1120b954d2a12484f1509524
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-02 13:11:11 -0800

    update cm

--- src/pages/Mcu/Films/CaptainMarvelFilm.jsx
+++ src/pages/Mcu/Films/CaptainMarvelFilm.jsx
@@ -1,10 +1,75 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const CaptainMarvelFilm = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Captain Marvel (film) - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>CaptainMarvelFilm</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Captain Marvel (film)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Captain Marvel (film)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Captain Marvel</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/4/4e/Captain_Marvel_%28film%29_poster.jpg" alt="Captain Marvel poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data">
+                Anna Boden <br />
+                Ryan Fleck
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/antman_and_the_wasp">Ant-Man and the Wasp</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/avengers_endgame">Avengers: Endgame</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
