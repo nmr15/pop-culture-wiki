db2dc6aabaaf97491f69f2a0fe5dd0b59b7c76fd
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-03 13:51:14 -0800

    update capma

--- src/pages/Mcu/Films/CaptainMarvelFilm.jsx
+++ src/pages/Mcu/Films/CaptainMarvelFilm.jsx
@@ -16,6 +16,39 @@ const CaptainMarvelFilm = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Audience response"},
+              {id: "7.4", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Sequel</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
