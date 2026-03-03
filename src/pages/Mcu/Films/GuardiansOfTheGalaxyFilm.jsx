3c6bb9c83c6a700255f7f76a602be94b5e240bce
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-03 13:11:55 -0800

    update files

--- src/pages/Mcu/Films/GuardiansOfTheGalaxyFilm.jsx
+++ src/pages/Mcu/Films/GuardiansOfTheGalaxyFilm.jsx
@@ -1,10 +1,72 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const GuardiansOfTheGalaxyFilm = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Guardians of the Galaxy (film) - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>GuardiansOfTheGalaxyFilm</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Guardians of the Galaxy (film)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Guardians of the Galaxy (film)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Guardians of the Galaxy</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/3/33/Guardians_of_the_Galaxy_%28film%29_poster.jpg" alt="Guardians of the Galaxy poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>James Gunn</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/captain_america_the_winter_soldier">Captain America: The Winter Soldier</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/avengers_age_of_ultron">Avengers: Age of Ultron</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
--- src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
+++ src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
@@ -1,10 +1,72 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const GuardiansOfTheGalaxyVol2 = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Guardians of the Galaxy Vol. 2 - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>GuardiansOfTheGalaxyVol2</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Guardians of the Galaxy Vol. 2</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Guardians of the Galaxy Vol. 2</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Guardians of the Galaxy Vol. 2</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/3/32/Guardians_of_the_Galaxy_Vol._2_poster.jpg" alt="Guardians of the Galaxy Vol. 2 poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>James Gunn</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/doctor_strange_film">Doctor Strange</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/spiderman_homecoming">Spider-Man: Homecoming</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
