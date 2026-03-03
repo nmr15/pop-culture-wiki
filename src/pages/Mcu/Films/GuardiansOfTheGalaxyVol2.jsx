09f5aee5fa94f5c556a0a3df479cfa9370ec62b9
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-03 15:44:03 -0800

    update gotg2

--- src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
+++ src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
@@ -16,6 +16,50 @@ const GuardiansOfTheGalaxyVol2 = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development", sub2: [
+                {id: "3.1.1", title: "Writing"}
+              ]},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production", sub2: [
+                {id: "3.4.1", title: "Visual effects"}
+              ]}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Accolades"}
+            ]}
+          />
+          <Collapse 
+            id="8"
+            item="Future"
+            sub={[
+              {id: "8.1", title: "Sequel"},
+              {id: "8.2", title: "I Am Groot"},
+              {id: "8.3", title: "The Guardians of the Galaxy Holiday Special"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
