b6d669f586537a043b248e50d0d481aa3c49a7ae
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-03 13:16:14 -0800

    update files

--- src/pages/Mcu/Films/IronMan3.jsx
+++ src/pages/Mcu/Films/IronMan3.jsx
@@ -1,10 +1,72 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const IronMan3 = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Iron Man 3 - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>IronMan3</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Iron Man 3</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Iron Man 3</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Iron Man 3</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/1/19/Iron_Man_3_poster.jpg" alt="Iron Man 3 poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>James Gunn</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/the_avengers_film">The Avengers</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/thor_the_dark_world">Thor: The Dark World</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
--- src/pages/Mcu/Films/SpiderManFarFromHome.jsx
+++ src/pages/Mcu/Films/SpiderManFarFromHome.jsx
@@ -1,10 +1,72 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
 const SpiderManFarFromHome = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Spider-Man: Far From Home - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
-      <div>
-        <h1>SpiderManFarFromHome</h1>
-      </div>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Spider-Man: Far From Home</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Spider-Man: Far From Home</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Spider-Man: Far From Home</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg" alt="Spider-Man: Far From Home poster" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>James Gunn</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/avengers_endgame">Avengers: Endgame</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/wandavision">WandaVision</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
     </>
   )
 }
