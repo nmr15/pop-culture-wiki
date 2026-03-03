75f246ed0d98c0ce5b5a780413c3a615aced989c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-03 13:32:10 -0800

    update aveiw

--- src/pages/Mcu/Films/AvengersInfinityWar.jsx
+++ src/pages/Mcu/Films/AvengersInfinityWar.jsx
@@ -38,10 +38,38 @@ const AvengersInfinityWar = () =>
                 {id: "3.3.2", title: "Design"},
                 {id: "3.3.3", title: "Filming"}
               ]},
-              {id: "3.4", title: "Filming"},
-              {id: "3.5", title: "Post-production"}
+              {id: "3.4", title: "Filming", sub2: [
+                {id: "3.4.1", title: "Principal photography"},
+                {id: "3.4.2", title: "Cinematography"}
+              ]},
+              {id: "3.5", title: "Post-production", sub2: [
+                {id: "3.5.1", title: "Editing"},
+                {id: "3.5.2", title: "Additional photography"},
+                {id: "3.5.3", title: "Visual effects"}
+              ]}
             ]}
           />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Audience response"},
+              {id: "7.4", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Sequel</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
