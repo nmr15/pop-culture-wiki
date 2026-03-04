29707ab43034b316729019469055cab3a3b2e98c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-04 08:14:01 -0800

    update file

--- src/pages/Mcu/Films/SpiderManHomecoming.jsx
+++ src/pages/Mcu/Films/SpiderManHomecoming.jsx
@@ -16,6 +16,40 @@ const SpiderManHomecoming = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development"},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production", sub2: [
+                {id: "3.4.1", title: "Visual effects"}
+              ]}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Sequels</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
