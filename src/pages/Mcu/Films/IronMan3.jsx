db38aa3ec43efcfe301c1808e206fd3012847288
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-04 08:09:38 -0800

    update im3

--- src/pages/Mcu/Films/IronMan3.jsx
+++ src/pages/Mcu/Films/IronMan3.jsx
@@ -16,6 +16,48 @@ const IronMan3 = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
+          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <Collapse 
+            id="3"
+            item="Production"
+            sub={[
+              {id: "3.1", title: "Development", sub2: [
+                {id: "3.1.2", title: "Writing"}
+              ]},
+              {id: "3.2", title: "Pre-production"},
+              {id: "3.3", title: "Filming"},
+              {id: "3.4", title: "Post-production"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Music</a></li>
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
+          <Collapse 
+            id="6"
+            item="Release"
+            sub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Themes and analysis</a></li>
+          <Collapse 
+            id="9"
+            item="Future"
+            sub={[
+              {id: "9.1", title: "Potential sequel"},
+              {id: "9.2", title: "Marvel One-Shot"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
