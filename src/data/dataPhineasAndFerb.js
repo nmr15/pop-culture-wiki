090c65d12fda9a4dd2a3fce1726eaa5c4a046067
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-09 14:55:51 -0700

    new array

---
+++ src/data/dataPhineasAndFerb.js
@@ -0,0 +1,86 @@
+export const pfCharacters = [
+  {
+    "id": "pfChar1",
+    "title": "Phineas Flynn",
+    "file": "PhineasFlynn",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/e/ea/Profile_-_Phineas_Flynn.PNG",
+    "route": "/phinease_and_ferb/phineas_flynn"
+  },
+  {
+    "id": "pfChar2",
+    "title": "Ferb Fletcher",
+    "file": "FerbFletcher",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/c/ca/Profile_-_Ferb_Fletcher.PNG",
+    "route": "/phinease_and_ferb/ferb_fletcher"
+  },
+  {
+    "id": "pfChar3",
+    "title": "Candace Flynn",
+    "file": "CandaceFlynn",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/1/18/Profile_-_Candace_Flynn.png",
+    "route": "/phinease_and_ferb/candace_flynn"
+  },
+  {
+    "id": "pfChar4",
+    "title": "Perry the Platypus",
+    "file": "PerryThePlatypus",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/6/66/Profile_-_Perry_the_Platypus.PNG",
+    "route": "/phinease_and_ferb/perry_the_platypus"
+  },
+  {
+    "id": "pfChar5",
+    "title": "Heinz Doofenshmirtz",
+    "file": "HeinzDoofenshmirtz",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/5/5d/Doofenshmirtz_Portrait.jpg",
+    "route": "/phinease_and_ferb/heinz_doofenshmirtz"
+  },
+  {
+    "id": "pfChar6",
+    "title": "Isabella Garcia-Shapiro",
+    "file": "IsabellaGarciaShapiro",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/8/8c/Profile_-_Isabella_Garcia-Shapiro.PNG",
+    "route": "/phinease_and_ferb/isabella_garcia_shapiro"
+  },
+  {
+    "id": "pfChar7",
+    "title": "Baljeet Tjinder",
+    "file": "BaljeetTjinder",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/9/98/Baljeet.JPG",
+    "route": "/phinease_and_ferb/baljeet_tjinder"
+  },
+  {
+    "id": "pfChar8",
+    "title": "Buford van Stomm",
+    "file": "BufordVanStomm",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/b/bd/Profile_-_Buford_Van_Stomm.PNG",
+    "route": "/phinease_and_ferb/buford_van_stomm"
+  },
+  {
+    "id": "pfChar9",
+    "title": "Francis Monogram",
+    "file": "FrancisMonogram",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/d/de/Profile_-_Francis_Monogram.PNG",
+    "route": "/phinease_and_ferb/francis_monogram"
+  },
+  {
+    "id": "pfChar10",
+    "title": "Carl Karl",
+    "file": "CarlKarl",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/8/80/Profile_-_Carl_Karl.PNG",
+    "route": "/phinease_and_ferb/carl_karl"
+  },
+  {
+    "id": "pfChar11",
+    "title": "Linda Flynn-Fletcher",
+    "file": "LindaFlynnFletcher",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/f/fa/Profile_-_Linda_Flynn-Fletcher.PNG",
+    "route": "/phinease_and_ferb/linda_flynn_fletcher"
+  },
+  {
+    "id": "pfChar12",
+    "title": "Lawrence Fletcher",
+    "file": "LawrenceFletcher",
+    "img": "https://static.wikia.nocookie.net/phineasandferb/images/5/50/Profile_-_Lawrence_Fletcher.PNG",
+    "route": "/phinease_and_ferb/lawrence_fletcher"
+  }
+]
