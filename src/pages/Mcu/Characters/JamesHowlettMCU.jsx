a11c8225d83452edbf7189994d34bf01f5e6ad51
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-12 13:23:13 -0700

    update jahow

--- src/pages/Mcu/Characters/JamesHowlettMCU.jsx
+++ src/pages/Mcu/Characters/JamesHowlettMCU.jsx
@@ -15,6 +15,38 @@ const JamesHowlettMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life", sub2: [
+                {id: "1.1.1", title: "Wolverine origins"},
+                {id: "1.1.2", title: "Death of the X-Men"}
+              ]},
+              {id: "1.2", title: "Perservation of Earth-10005", sub2: [
+                {id: "1.2.1", title: "Taken from his world"},
+                {id: "1.2.2", title: "Fight in the Void"},
+                {id: "1.2.3", title: "Escape from Cassandra Nova"},
+                {id: "1.2.4", title: "Meeting the resistance"},
+                {id: "1.2.5", title: "Escaping the Void"},
+                {id: "1.2.6", title: "Fighting the Deadpool Corps"},
+                {id: "1.2.7", title: "Saving all timelines"}
+              ]},
+              {id: "1.3", title: "New world, new life", sub2: [
+                {id: "1.3.1", title: "Celebrating victory"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Powers and abilities"
+            sub={[
+              {id: "2.1", title: "Powers"},
+              {id: "2.2", title: "Abilities"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#3">Equipment</a></li>
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -40,7 +72,7 @@ const JamesHowlettMCU = () =>
                 </span>
               </td>
             </tr>
-            <tr>
+           <tr>
               <th className="infobox-data">Portrayed by</th>
               <td className="infobox-data"><Link>Hugh Jackman</Link></td>
             </tr>
