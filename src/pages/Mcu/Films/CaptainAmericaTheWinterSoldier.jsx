7b3d643d1accb6cf4e1ac37c1f11fc7d727f3355
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-16 08:20:04 -0700

    update catws

--- src/pages/Mcu/Films/CaptainAmericaTheWinterSoldier.jsx
+++ src/pages/Mcu/Films/CaptainAmericaTheWinterSoldier.jsx
@@ -105,7 +105,23 @@ const CaptainAmericaTheWinterSoldier = () =>
             <b>Captain America: The Winter Soldier</b> is a 2014 American superhero film based on the <Link to="/marvelcomics">Marvel Comics</Link>
             character <Link to="/marvelcomics/captain_america">Captain America</Link>, produced by <Link>Marvel Studios</Link> and distributed by Walt
             Disney Studios Motion Pictures. It is the sequel to <Link to="/mcu/captain_america_the_first_avenger">Captain America: The First
-            Avenger</Link> (2011) and the ninth film in the <Link to="/mcu">Marvel Cinematic Universe</Link> (MCU).
+            Avenger</Link> (2011) and the ninth film in the <Link to="/mcu">Marvel Cinematic Universe</Link> (MCU). The film was directed by
+            <Link> Anthony and Joe Russo</Link> from a screenplay by Christopher Markus and Stephen McFeely. It stars <Link>Chris Evans</Link> as
+            <Link to="/mcu/steve_rogers"> Steve Rogers / Captain America</Link> alongside <Link>Scarlett Johansson</Link>, <Link>Sebastian Stan</Link>,
+            <Link>Anthony Mackie</Link>, <Link>Cobie Smulders</Link>, <Link>Frank Grilo</Link>, <Link>Emily VanCamp</Link>, <Link>Hayley Atwell</Link>,
+            <Link> Toby Jones</Link>, Jenny Agutter, <Link>Robert Redford</Link>, and <Link>Samuel L. Jackson</Link>. In the film, Rogers,
+            <Link to="/mcu/natasha_romanoff">Natasha Romanoff</Link> (Johansson), and <Link to="/mcu/sam_wilson">Sam Wilson</Link> (Mackie) uncover a conspiracy
+            within the spy agency <Link>S.H.I.E.L.D.</Link> while facing the <Link to="/mcu/bucky_barnes">Winter Soldier</Link> (Stan), a mysterious assassin.
+          </p>
+          <p>
+            Markus and McFeely began writing the sequel around the release of The First Avenger in July 2011. The script draws from the Winter Soldier story arc in
+            the comic book written by Ed Brubaker, as well as conspiracy fiction from the 1970s such as the film Three Days of the Condor (1975). The Winter
+            Soldier explores S.H.I.E.L.D. in a similar way to how the first film explored the U.S. military, after Rogers was shown working for S.H.I.E.L.D. in
+            the MCU crossover film <Link to="/mcu/the_avengers_film">The Avengers</Link> (2012). The Russo brothers signed on to direct in June 2012, and casting
+            began the following month. They wnted the film to be a political thriller that focused on topical ideas such as drone warfare, targeted killing, and
+            global surveillance. Filming began in April 2013 in Los Angeles, California, before moving to Washington, D.C., and Cleveland, Ohio. Though the directors
+            prioritized practical effects and intense stunt work to differentiate from previous Captain America portrayals, visual effects were created by six
+            companies for 2,500 shots including the climactic <Link>Helicarrier</Link> sequence.
           </p>
         </div>
       </main>
