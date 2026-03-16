57f9e75237b054a99f63adb357c76e33f1c26420
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-16 13:44:07 -0700

    new file

---
+++ src/pages/Mcu/Films/BlackWidowFilm.jsx
@@ -0,0 +1,74 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableMcu from '../../../components/TableMcu'
+
+const BlackWidowFilm = () =>
+{
+  useEffect(() =>
+  {
+    document.title = "Black Widow (film) - Pop Culture Wiki";
+  }, []);
+
+  return(
+    <>
+      <aside className="sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+        </ul>
+      </aside>
+      <main className="main" id="top">
+        <ul className="breadcrumb">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/mcu">Marvel Cinematic Universe</Link></li>
+          <li>Black Widow (film)</li>
+        </ul>
+        <div>
+          <h1 className="article-heading">Black Widow (film)</h1>
+        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Black Widow</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/e/e9/Black_Widow_%282021_film%29_poster.jpg" alt="Black Widow" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Directed by</th>
+              <td className="infobox-data"><Link>Cate Shortland</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Projects</th>
+              <td className="infobox-data"><Link>List of MCU projects</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link to="/mcu/characters">List of MCU character</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/loki_series">Loki</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next MCU project</th>
+              <td className="infobox-data"><Link to="/mcu/what_if">What If...?</Link></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="div-top">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default BlackWidowFilm;
