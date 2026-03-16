3b899ddea0ad5f670b45f3f9bdb4fdbe3241b1a7
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-16 11:53:17 -0700

    update gotg1

--- src/pages/Mcu/Films/GuardiansOfTheGalaxyFilm.jsx
+++ src/pages/Mcu/Films/GuardiansOfTheGalaxyFilm.jsx
@@ -118,7 +118,32 @@ const GuardiansOfTheGalaxyFilm = () =>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>Guardians of the Galaxy</b> (retroactively referred to as <b>Guardians of the Galaxy Vol. 1</b>) is a 2014 American superhero film based on the
+            <Link to="/marvelcomics"> Marvel Comics</Link> superhero team <Link>of the same name</Link>. Produced by <Link>Marvel Studios</Link> and
+            distrubited by Walt Disney Studios Motion Pictures, it is the 10th film in the <Link to="/mcu">Marvel Cinematic Universe</Link> (MCU). Directed by
+            <Link> James Gunn</Link>n who wrote the screenplay with Nocle Perlman, it features an ensemble cast including <Link>Chriss Pratt</Link>,
+            <Link> Zoe Saldaña</Link>, <Link>Dave Bautista</Link>, <Link>Vin Diesel</Link>, and <Link>Bradley Cooper</Link> as the titular <Link>Guardians</Link>,
+            along with Lee Pace, <Link>Michael Rooker</Link>, <Link>Karen Gillian</Link>, Djimon Hounsou, <Link>John C. Reilly</Link>, <Link>Glenn Close</Link>,
+            and <Link>Benicio del Toro</Link>. In the film, <Link to="/mcu/peter_quill">Peter Quill</Link> (Pratt) and a group of extraterrestrial criminals go on
+            the run after stealing a <Link>powerful artfiact</Link>.
+          </p>
+          <p>
+            Perlman began working on the screenplay in 2009. Producer <Link>Kevin Feige</Link> first publicly mentioned Guardians of the Galaxy as a potential film
+            in 2010 and Marvel Studios announced it was in active development at the June 2012 San Diego Comic-Con. Gunn was hired to write and direct the film that
+            September. In February 2013, Pratt was hired to play Peter Quill / Star-Lord, and the supporting cast members were subsequently confirmed. Principal
+            photography began in July 2013 at Shepperton Stuidos in England, with filming continuing in London before wrapping in October 2013. In addition to an
+            original score by Tyler Bates, the soundtrack includes several popular songs from the 1960s and 1970s chosen by Gunn. Post-production was completed on
+            July 7, 2014.
+          </p>
+          <p>
+            Guardians of the Galaxy premiered at the Dolby Theatre in Hollywood, Los Angeles, on July 21, 2014, and was theatrically released in the United States
+            on August 1 as part of <Link>Phase Two</Link> of the MCU. It was a critical and commercial success, grossing $773.3 million worldwide and became the
+            third highest grossing film of 2014. It was praised for its screenplay, direction, acting, humor, soundtrack, visual effects, and action sequences. It
+            was nominated for two awards at the 87th Academy Awards, and received numerous other accolades. Two sequels have been released:
+            <Link to="/mcu/guardians_of_the_galaxy_vol_2">Guardians of the Galaxy Vol. 2</Link> (2017) and
+            <Link to="/mcu/guardians_of_the_galaxy_vol_3">Guardians of the Galaxy Vol. 3</Link> (2023).
+          </p>
         </div>
       </main>
     </>
