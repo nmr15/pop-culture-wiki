3d32f918e480d2a3e829dbb391368ed10b27ecc8
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-17 15:32:09 -0700

    new array

--- src/data/dataSimpsons.js
+++ src/data/dataSimpsons.js
@@ -1,34 +1,69 @@
+export const simpsonsMeta = [
+  {
+    "id": "simpsonsMeta1",
+    "title": "The Simpsons",
+    "file": "SimpsonsSeries"
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/The_Simpsons_yellow_logo.svg/500px-The_Simpsons_yellow_logo.svg.png",
+    "type": "series"
+    "route": "/simpsons/the_simpsons_series"
+  },
+  {
+    "id": "simpsonsMeta2",
+    "title": "The Simpsons Movie",
+    "file": "SimpsonsMovie"
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d5/The_Simpsons_Movie_%282007%29.png",
+    "type": "film"
+    "route": "/simpsons/the_simpsons_movie"
+  },
+  {
+    "id": "simpsonsMeta3",
+    "title": "The Simpsons Characters",
+    "file": "SimpsonsChars"
+    "img": "",
+    "type": "list"
+    "route": "/simpsons/the_simpsons_characters"
+  },
+  {
+    "id": "simpsonsMeta4",
+    "title": "The Simpsons Episodes",
+    "file": "SimpsonsEps"
+    "img": "",
+    "type": "list"
+    "route": "/simpsons/the_simpsons_episodes"
+  },
+]
+
 export const simpsonsChars = [
   {
-    "id": "simpsonschar1",
+    "id": "simpsonsChar1",
     "title": "Homer Simpson",
     "file": "HomerSimpson",
     "img": "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
     "route": "/simpsons/homer_simpson"
   },
   {
-    "id": "simpsonschar2",
+    "id": "simpsonsChar2",
     "title": "Marge Simpson",
     "file": "MargeSimpson",
     "img": "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
     "route": "/simpsons/marge_simpson"
   },
   {
-    "id": "simpsonschar3",
+    "id": "simpsonsChar3",
     "title": "Bart Simpson",
     "file": "BartSimpson",
     "img": "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
     "route": "/simpsons/bart_simpson"
   },
   {
-    "id": "simpsonschar4",
+    "id": "simpsonsChar4",
     "title": "Lisa Simpson",
     "file": "LisaSimpson",
     "img": "https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png",
     "route": "/simpsons/lisa_simpson"
   },
   {
-    "id": "simpsonschar5",
+    "id": "simpsonsChar5",
     "title": "Maggie Simpson",
     "file": "MaggieSimpson",
     "img": "https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png",
