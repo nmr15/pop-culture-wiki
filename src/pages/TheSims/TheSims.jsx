4542bc6a1c646e545ee954dcb88b78e84a58ee09
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-17 15:58:16 -0700

    update sims

--- src/pages/TheSims/TheSims.jsx
+++ src/pages/TheSims/TheSims.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
 
 const TheSims = () =>
 {
@@ -13,6 +15,16 @@ const TheSims = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Development</a></li>
+
+          <Collapse 
+            id="3"
+            item="Legacy"
+            sub={[
+              {id: "3.1", title: "Film"},
+              {id: "3.2", title: "Reality television show"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
@@ -38,13 +50,31 @@ const TheSims = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Creator</th>
+              <td className="infobox-data"><Link>Will Wright</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Developer</th>
+              <td className="infobox-data"><Link>Maxis</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Publisher</th>
+              <td className="infobox-data"><Link>Electronic Arts</Link></td>
             </tr>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>The Sims</b> is a series of life simulation video games developed by <Link>Maxis</Link> and published by <Link>Electronic Arts</Link> (EA). The
+            franchise has sold nearly 200 million copies worldwide, and is one of the best selling video game series of all time. It is also part of the larger
+            Sim series, started by SimCity in 1989.
+          </p>
+          <p>
+            The games in the Sims series are largely sandbox games, in that they lack any defined goals (except for some later expansion packs and console versions
+            which introduced this gameplay style). The player creates virtual people called "Sims", places them in houses, and helps direct their moods and satisfy
+            their desires. Players can either place their Sims in pre-constructed homes or build theme themselves. Each successive expansion pack and game in the
+            series augmented what the player could do with their Sims.
+          </p>
         </div>
       </main>
     </>
