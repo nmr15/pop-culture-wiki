192eab64bf274c61c24fcc0d7956581d1aaef3a2
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-17 14:10:45 -0700

    update kingo

--- src/pages/Mcu/Characters/KingoMCU.jsx
+++ src/pages/Mcu/Characters/KingoMCU.jsx
@@ -15,7 +15,32 @@ const KingoMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
-
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Creation"},
+              {id: "1.2", title: "Protecting humanity", sub2: [
+                {id: "1.2.1", title: "Arriving on Earth"},
+                {id: "1.2.2", title: "Battle of Babylon"},
+                {id: "1.2.3", title: "Defending Korea"},
+                {id: "1.2.4", title: "Ending the war"},
+                {id: "1.2.5", title: "Eternals disbandment"}
+              ]},
+              {id: "1.3", title: "Acting career", sub2: [
+                {id:"1.3.1", title: "First acting job"},
+                {id:"1.3.2", title: "Pranked"},
+                {id:"1.3.3", title: "Leaving Sprite"},
+                {id:"1.3.4", title: "Bollywood star"}
+              ]},
+              {id: "1.4", title: "The Emergence", sub2: [
+                {id: "1.4.1", title: "Reuniting with the Eternals"},
+                {id: "1.4.2", title: "Ambush at Druig's compound"},
+                {id: "1.4.3", title: "Ikaris' betrayal"},
+                {id: "1.4.4", title: "Judged by Arishem"}
+              ]}
+            ]}
+          />
           <Collapse 
             id="2"
             item="Powers and abilities"
