697f9df69725ac90944afce126f18c7856e3604e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-18 08:23:27 -0700

    update gotg2

--- src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
+++ src/pages/Mcu/Films/GuardiansOfTheGalaxyVol2.jsx
@@ -108,7 +108,32 @@ const GuardiansOfTheGalaxyVol2 = () =>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>Guardians of the Galaxy Vol. 2</b> is a 2017 American superhero film based on the <Link to="/marvelcomics">Marvel Comics</Link> superhero team
+            <Link> Guardians of the Galaxy</Link>, produced by <Link>Marvel Studios</Link> and distributed by Walt Disney Studios Motion Pictures. It is the sequel
+            to <Link to="/mcu/guardians_of_the_galaxy_film">Guardians of the Galaxy</Link> (2014) and the 15th film in the
+            <Link to="/mcu"> Marvel Cinematic Universe</Link> (MCU). Written and directed by <Link>James Gunn</Link>, the film stars an ensemble cast featuring
+            <Link> Chris Pratt</Link>, <Link>Zoe Saldaña</Link>, <Link>Dave Bautista</Link>, <Link>Vin Diesel</Link>, <Link>Bradley Cooper</Link>,
+            <Link> Michael Rooker</Link>, <Link>Karen Gillian</Link>, <Link>Pom Klementieff</Link>, Sylvester Stallone, and <Link>Kurt Russell</Link>. In the film,
+            The <Link>Guardians</Link> travel throughout the cosmos as they help <Link to="/mcu/peter_quill">Peter Quill</Link> (Pratt) learn more about his
+            mysterious parentage.
+          </p>
+          <p>
+            The film was officially announced at the 2014 San Diego Comic-Con before the theatrical release of the first film, along with Gunn's return as writer and
+            director from the first film, with the title of the sequel revealed a year later in June 2015. Principal photography began in February 2016 at Pinewood
+            Atlanta Studios in Fayette County, Georgia, with many crew changes from the first film due to other commitments. Filming concluded in June 2016. Gunn
+            chose to set the sequel shortly after the first film to explore the characters' new roles as the Guardians, and to follow the storyline of Quill's father
+            established throughout that previous film. Russell was confirmed as Quill's father in July 2016, portraying <Link>Ego</Link>, a departure from Quill's
+            comic father.
+          </p>
+          <p>
+            Guardians of the Galaxy Vol. 2 premiered in Tokyo on April 10, 2017, and was released in the United States on May 5, 2017, as part of <Link>Phase
+            Three</Link> of the MCU. It grossed more than $869 million worldwide, making it the eigth highest grossing film of 2017, while also outgrossing its
+            predecessor. The film received praise for its visuals, direction, soundtrack, action sequences, humor, and performances, though some critics deemed it
+            inferior to the original. It received a nomination for Best Visual Effects at the 90th Academy Awards. A sequel,
+            <Link to="/mcu/guardians_of_the_galaxy_vol_3">Guardians of the Galaxy Vol. 3</Link>, was released on May 5, 2023, preceded by the <Link>Disney+</Link>
+            special <Link to="/mcu/the_guardians_of_the_galaxy_holiday_special">The Guardians of the Galaxy Holiday Special</Link> in November 2023.
+          </p>
         </div>
       </main>
     </>
