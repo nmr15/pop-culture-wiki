7f3a1f2d231f8988111cc552dfe82505e69293d0
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-20 13:09:31 -0700

    update ros

--- src/pages/StarWars/Films/RiseOfSkywalker.jsx
+++ src/pages/StarWars/Films/RiseOfSkywalker.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableStarWars from '../../../components/TableStarWars'
 
 const RiseOfSkywalker = () =>
 {
@@ -13,6 +16,47 @@ const RiseOfSkywalker = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Opening crawl</a></li>
+
+          <li className="sidebar-list-item"><a href="#3">Cast</a></li>
+          <Collapse 
+            id="4"
+            item="Production"
+            sub={[
+              {id: "4.1", title: "Development"},
+              {id: "4.2", title: "Casting"},
+              {id: "4.3", title: "Filming"},
+              {id: "4.4", title: "Post-production"},
+              {id: "4.5", title: "Music"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Marketing"
+            sub={[
+              {id: "5.1", title: "Promotion"},
+              {id: "5.2", title: "Tie-in literature and merchandise"},
+              {id: "5.3", title: "Video games"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Release"
+            xub={[
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home meida"}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office"},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Audience response"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Future</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
