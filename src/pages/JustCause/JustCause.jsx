e9cb35567a9c2ffaa82c9a56fc3e66ba33fcca48
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-24 10:40:31 -0700

    update jc

--- src/pages/JustCause/JustCause.jsx
+++ src/pages/JustCause/JustCause.jsx
@@ -79,6 +79,29 @@ const JustCause = () =>
             The series directly draws its name from the real-life United States Invasion of Panama, codenamed "Operation Just Cause".
           </p>
         </div>
+        <div>
+          <Heading2 id={1} title={"Overview"} />
+          <div>
+            <p>
+              Each installment in the series takes place on a different fictional island nation, where the player plays the character
+              <Link to="/just_cause/rico_rodriguez">Rico Rodriguez</Link>, a secret agent who originally hails from the fictional nation of
+              <Link to="/just_cause/medici">Medici</Link> (featured in the third game). On foot, Rico can walk, swim, jump, and operate weapons. Players can also take control of
+              vehicles found in the world and perform stunts while driving them. From the second game onwards, players can also utilize Rico's
+              <Link to="/just_cause/parachute"> parachute</Link> and <Link to="/just_cause/grappling_hook">grappling hook</Link> to travel around the map, with the third game
+              intoducing a <Link to="/just_cause/wingsuit">wingsuit</Link> for further movement options.
+            </p>
+            <p>
+              During the game, the player is given a main storyline as well as several side missions. Side missions may include liberating a village or taking over a drug
+              cartel's villa. In Just Cause, these side missions are repetitive but necessary to gain points with certain factions. In Just Cause 2, the side missions become
+              unique and more complex.
+            </p>
+            <p>
+              When not playing story or side missions, the player can roam freely in the open world. However, committing certain aggressive acts will attract a "heat" level
+              (similar to <Link to="/gta">Grand Theft Auto's</Link> <Link>wanted level</Link>), which will then increase the strength and number of enemy NPC's spawned seeking to
+              kill the player.
+            </p>
+          </div>
+        </div>
       </main>
     </>
   )
