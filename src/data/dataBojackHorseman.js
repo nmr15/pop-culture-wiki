ca9a6b6531a59a2cfa3fe18fa144c20c5c0d9f87
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-24 13:49:54 -0700

    new array

---
+++ src/data/dataBojackHorseman.js
@@ -0,0 +1,37 @@
+export const bojackChars = [
+  {
+    "id": "bojackChar1",
+    "title": "BoJack Horseman",
+    "file": "BojackHorsemanChar",
+    "img": "https://static.wikia.nocookie.net/bojackhorseman/images/d/d2/BoJack_Horsemann.png",
+    "route": "/bojack_horseman/bojack_horseman_character"
+  },
+  {
+    "id": "bojackChar2",
+    "title": "Diane Nguyen",
+    "file": "DianeNguyen",
+    "img": "https://static.wikia.nocookie.net/bojackhorseman/images/8/86/Diane_Nguyen_S1-4_design.png",
+    "route": "/bojack_horseman/diane_nguyen"
+  },
+  {
+    "id": "bojackChar3",
+    "title": "Princess Carolyn",
+    "file": "PrincessCarolyn",
+    "img": "https://static.wikia.nocookie.net/bojackhorseman/images/d/de/Princess_Carolyn.png",
+    "route": "/bojack_horseman/princess_carolyn"
+  },
+  {
+    "id": "bojackChar4",
+    "title": "Todd Chavez",
+    "file": "ToddChavez",
+    "img": "https://static.wikia.nocookie.net/bojackhorseman/images/5/53/Todd_Chavez.png",
+    "route": "/bojack_horseman/todd_chavez"
+  },
+  {
+    "id": "bojackChar5",
+    "title": "Mr. Peanutbutter",
+    "file": "MrPeanutbutter",
+    "img": "https://static.wikia.nocookie.net/bojackhorseman/images/4/4b/Mr._Peanutbutter_casual.png",
+    "route": "/bojack_horseman/mr._peanutbutter"
+  }
+]
