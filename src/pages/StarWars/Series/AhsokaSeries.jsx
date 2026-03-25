1b5ec6f8b3088027cfcf41e138dd98d3d44a372c
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-25 11:31:07 -0700

    update ahsoka

--- src/pages/StarWars/Series/AhsokaSeries.jsx
+++ src/pages/StarWars/Series/AhsokaSeries.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableStarWars from '../../../components/TableStarWars'
 
 const AhsokaSeries = () =>
 {
@@ -13,6 +16,47 @@ const AhsokaSeries = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Premise</a></li>
+          <Collapse 
+            id="2"
+            item="Cast and characters"
+            sub={[
+              {id: "2.1", title: "Starring"},
+              {id: "2.2", title: "Recurring"},
+              {id: "2.3", title: "Guests"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Episodes"
+            sub={[
+              {id: "3.1", title: "Season 1 (2023)"},
+              {id: "3.2", title: "Season 2"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Production"
+            sub={[
+              {id: "4.1", title: "Development"},
+              {id: "4.2", title: "Writing"},
+              {id: "4.3", title: "Casting"},
+              {id: "4.4", title: "Design"},
+              {id: "4.5", title: "Filming"},
+              {id: "4.6", title: "Music"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#5">Release</a></li>
+          <Collapse 
+            id="5"
+            item="Reception"
+            sub={[
+              {id: "5.1", title: "Viewership"},
+              {id: "5.2", title: "Critical response"},
+              {id: "5.3", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#6">Tie-in media</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -38,8 +82,20 @@ const AhsokaSeries = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Created by</th>
+              <td className="infobox-data"><Link>Dave Filoni</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Episodes</th>
+              <td className="infobox-data"><a href="#3">List of Ahsoka episodes</a></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Films and series</th>
+              <td className="infobox-data"><Link>List of Star Wars films and series</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Characters</th>
+              <td className="infobox-data"><Link>List of Star Wars characters</Link></td>
             </tr>
           </tbody>
         </table>
