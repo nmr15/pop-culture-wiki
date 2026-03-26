4aefd43a9c00583deb478d6300e01d079659646e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-26 15:19:07 -0700

    update asaj

--- src/pages/StarWars/Characters/AsajjVentress.jsx
+++ src/pages/StarWars/Characters/AsajjVentress.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Collapse from '../../../components/Collapse'
+import Heading2 from '../../../components/Heading2'
+import TableStarWars from '../../../components/TableStarWars'
 
 const AsajjVentress = () =>
 {
@@ -13,6 +16,60 @@ const AsajjVentress = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+
+          <Collapse 
+            id="2"
+            item="Personality and traits"
+            sub={[
+              {id: "2.1", title: "Disciple of darkness"},
+              {id: "2.2", title: "Attachment, loss, and letting go"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Powers and abilities"
+            sub={[
+              {id: "3.1", title: "Lightsaber abilities"},
+              {id: "3.2", title: "Force powers"},
+              {id: "3.3", title: "Other skills"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Equipment</a></li>
+          <Collapse 
+            id="5"
+            item="Characterization"
+            sub={[
+              {id: "5.1", title: "Creation"},
+              {id: "5.2", title: "Portrayal"}
+            ]}
+          />
+          <Collapse 
+            id="6"
+            item="Appearances"
+            sub={[
+              {id: "6.1", title: "The Clone Wars"},
+              {id: "6.2", title: "Dark Disciple"},
+              {id: "6.3", title: "The Bad Batch"},
+              {id: "6.4", title: "Tales of the Underworld"},
+              {id: "6.5", title: "Other works"},
+              {id: "6.6", title: "Legends", sub2: [
+                {i: "6.6.1", title: "Clone Wars"},
+                {i: "6.6.2", title: "Novels"},
+                {i: "6.6.3", title: "Comic books"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="7"
+            item="Video game appearances"
+            sub={[
+              {id: "7.1", title: "The Lego Star Wars series"},
+              {id: "7.2", title: "Star Wars Battlefront: Renegade Squadron"},
+              {id: "7.3", title: "Star Wars: The Clone Wars video games"},
+              {id: "7.4", title: "Star Wars: The Force Unleashed duology"},
+              {id: "7.5", title: "Other Star Wars video games"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
@@ -39,8 +96,12 @@ const AsajjVentress = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Voiced by</th>
+              <td className="infobox-data">
+                <Link>Grey DeLisle</Link> <br />
+                <Link>Nika Futterman</Link> <br />
+                Trish Patendon
+              </td>
             </tr>
           </tbody>
         </table>
