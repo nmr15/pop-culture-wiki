27e3a57d703e02bcf2fc147a11c666e3cc943476
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-30 13:25:02 -0700

    update sawderp

--- src/pages/StarWars/Characters/SawGerrera.jsx
+++ src/pages/StarWars/Characters/SawGerrera.jsx
@@ -17,7 +17,55 @@ const SawGerrera = () =>
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
           <li className="sidebar-list-item"><a href="#1">Concept and creation</a></li>
-          
+          <Collapse 
+            id="2"
+            item="Biography"
+            sub={[
+              {id: "2.1", title: "Early life and career"},
+              {id: "2.2", title: "Clone Wars combatant", sub2: [
+                {id: "2.2.1", title: "Helping hand from the Jedi"},
+                {id: "2.2.2", title: "Taking the fight to the Confederacy"},
+                {id: "2.2.3", title: "The King's blessing"},
+                {id: "2.2.4", title: "The true cost of conflict"},
+                {id: "2.2.5", title: "Siege of Onderon"}
+              ]},
+              {id: "2.3", title: "Age of the Empire", sub2: [
+                {id: "2.3.1", title: "Early insurgency", sub3: [
+                  {id: "2.3.1.1", title: "Meeting with the Bad Bath"},
+                  {id: "2.3.1.2", title: "Recovering Soujen Vak-Nhalis"},
+                  {id: "2.3.1.3", title: "A death on Li'eta"},
+                  {id: "2.3.1.4", title: "The cache and the senator"},
+                  {id: "2.3.1.5", title: "Boxed in"},
+                  {id: "2.3.1.6", title: "An act of terror and the conferenc"},
+                  {id: "2.3.1.7", title: "Recruitment on Yiagla"},
+                  {id: "2.3.1.8", title: "Raven's Peak"},
+                  {id: "2.3.1.9", title: "Helping the Ersos"}
+                ]},
+                {id: "2.3.2", title: "Freeing Wookiees"},
+                {id: "2.3.3", title: "Mentor for Jyn"},
+                {id: "2.3.4", title: "Dealing with Axis", sub3: [
+                  {id: "2.3.4.1", title: "Meeting with Luthen Rael"},
+                  {id: "2.3.4.2", title: "Second meeting with Rael"}
+                ]},
+                {id: "2.3.5", title: "The perfect night", sub3: [
+                  {id: "2.3.5.1", title: "Rhydonium reverie"},
+                  {id: "2.3.5.2", title: "High on his own supply"}
+                ]},
+                {id: "2.3.6", title: "Assassination of Moff Panaka"}
+              ]},
+              {id: "2.4", title: "Galactic civil war", sub2: [
+                {id: "2.4.1", title: "Mission to Geonosis"},
+                {id: "2.4.2", title: "Jalindi and a personal quest"},
+                {id: "2.4.3", title: "Mission to Dooma 4"},
+                {id: "2.4.4", title: "Taking the fight to Jedha"},
+                {id: "2.4.5", title: "Reuniting with Jyn and death"}
+              ]},
+              {id: "2.5", title: "Legacy", sub2: [
+                {id: "2.5.1", title: "Partisan inspiration"},
+                {id: "2.5.2", title: "Under a New Republic"}
+              ]}
+            ]}
+          />
           <Collapse 
             id="3"
             item="Personality and traits"
