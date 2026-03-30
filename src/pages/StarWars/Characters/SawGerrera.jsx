8afa124cde19acc7f0c1de84dc6927a725aba4c2
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-30 13:57:13 -0700

    update sawderp

--- src/pages/StarWars/Characters/SawGerrera.jsx
+++ src/pages/StarWars/Characters/SawGerrera.jsx
@@ -162,7 +162,49 @@ const SawGerrera = () =>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>Saw Gerrera</b> was a human male Onderonian resistance fighter who, as a leading member of the Onderon rebels, fought against the
+            <Link> Confdederacy of Independent System</Link> on <Link>Onderon</Link> during the <Link>Clone Wars</Link>. He and his sister, Steela Gerrera, were
+            instrumental in the rebel liberation of their homeworld during the Orendon Civil War. Following the rise of the <Link>Galactic Empire</Link> at the end
+            of the war, Saw became a key member in the fith against the fascist government and the formation of the Alliance to Restore the Republic. His tactics
+            during the early rebellion agains the Empire led him to be seen as an extremist, one whose notoriety was recognized by both the Empire, the Alliance, and,
+            many years later, the New Republic.
+          </p>
+          <p>
+            In the early resistance against the Separatist Alliance on Onderon, Gererra and his fellow soldiers were outnumbered and outgunned by the Separatist
+            Droid Army. They requested help from the Jedi High Council, which led the Grand Army of the Republic in its fight against the Separatists, but the Jedi
+            Order chose not to become actively involved in the fighting on Onderon. Instead, they sent a team of advisors,
+            <Link to="/starwars/anakin_skywalker"> General Anakin Skywalker</Link> and <Link to="/starwars/obi_wan_kenobi">Obi-Wan Kenobi</Link>,
+            <Link to="/starwars/ahsoka_tano"> Commander Ahsoka Tano</Link>, and <Link to="/starwars/captain_rex">Clone Captain Rex</Link>, to train the Onderonian
+            fighters. The Jedi and Captain Rex taught them proper techniques and strategies for taking on the <Link>battle droid</Link> army, and, through their
+            efforts, the rebels began to turn the tide. Gerrera came to resent that the rebels chose Steela as their leader, and he was soon captured in an
+            ill-concocted solo effort to rescue the deposed King Ramsis Dendup from the Separatists. They were both rescued by the rebels, who eventually
+            mounted a final battle against the Separatists. Through their efforts, the rebels liberated their world and returned Dendup to the throne. The fight,
+            however, came at a personal cost: Steela was killed during the final battle, and Gerrera was left to mourn his sister, feeling responsible for her death.
+          </p>
+          <p>
+            At the end of the Clone Wars, the Republic was transformed into the Galactic Empire. Horrified, Gerrera and his fellow Onendorians resisted the Empire,
+            and the Empire, despite the fact that Saw and his team had been instrumental in the liberation of Onderon, branded them as insurgents. Gerrera managed
+            to evade Imperial capture, and he continued to fight against the Empire. During this time, Saw relocated Imperial scientist <Link>Galen Erso</Link>, his
+            wife Lyra, and their daughter, <Link to="/starwars/jyn_erso">Jyn</Link>, to the uninhabited planet of Lah'mu. When
+            <Link to="/starwars/orson_krennic"> Orson Krennic</Link>, an old friend of Galen's, discovered their location and took Galen away and killed Lyra, Saw
+            traveled to Lah'mu, adopted Jyn, and raised her, all the while claiming that she was his own daughter.
+          </p>
+          <p>
+            Gerrera fought against the Imperial Military and led a resistance group known as the Partisans, a military organization that some conssider an
+            extremist, using terrorist tactics and operating under the codename, "Stoneface." His resistance cell, a continuation of his Onderonian resistance, was
+            one of the first in the group of cells that formed the Rebel Alliance. He was later involved in the early campaigns of the Rebel Alliance, by which time
+            his more, extreme tactics had come to stand in stark contrast with those of Alliance leaders <Link>Bail Organa</Link> and
+            <Link to="/starwars/mon_mothma"> Mon Mothma</Link>. Gerrera eventually cut ties with Organa and Mothma, who viewed him as a militant and extremist.
+          </p>
+          <p>
+            Having learned that the Empire's massive <Link>kyber crystal</Link> had come from <Link>Jedha</Link>, Saw and the Partisans based their operation there.
+            After the capture of an Imperial ensign, <Link>Bodhi Rook</Link>, who claimed that the Empire was building a planet-killer, Saw was reunited with Jyn,
+            whom the Alliance had sent to meet with him. Since Rook had sent him a message from Jyn's father. Gerrera showed it to her, which revealed that the Empire
+            was constructing a planet-killing superweapon called the <Link>Death Star</Link>. As the message played, however, the Death Star struck Jedha City and
+            destroyed it. As the ripple of the explosion reached them, Gerrera told Jyn he was done running and stayed at the base, and was killed by the aftermath
+            of the blast.
+          </p>
         </div>
       </main>
     </>
