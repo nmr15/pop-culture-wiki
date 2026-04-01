af97e9425de5ecf0a4fb3e7446830246a95543e4
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-01 11:32:56 -0700

    updat rm

--- src/pages/RickandMorty/RickandMorty.jsx
+++ src/pages/RickandMorty/RickandMorty.jsx
@@ -111,6 +111,63 @@ const RickandMorty = () =>
         <div className="div-top">
           
         </div>
+        <div>
+          <Heading2 id={1} title={"Premise"} />
+          <div>
+
+          </div>
+        </div>
+        <div>
+          <Heading2 id={2} title={"Episodes"} />
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <Heading2 id={3} title={"Production"} />
+          <div>
+            <h3 className="article-heading-3" id="3.1">Development</h3>
+            <h3 className="article-heading-3" id="3.2">Writing</h3>
+            <h3 className="article-heading-3" id="3.3">Animation and voice recording</h3>
+            <h3 className="article-heading-3" id="3.4">Roiland's firing</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={4} title={"Themes and analysis"} />
+          <div>
+            <h3 className="article-heading-3" id="4.1">Comedic style</h3>
+            <h3 className="article-heading-3" id="4.2">Philosophy</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={5} title={"Release"} />
+          <div>
+            <h3 className="article-heading-3" id="5.1">Broadcast</h3>
+            <h3 className="article-heading-3" id="5.2">Streaming service and home media</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={6} title={"Reception"} />
+          <div>
+            <h3 className="article-heading-3" id="6.1">Critical response</h3>
+            <h3 className="article-heading-3" id="6.2">Ratings</h3>
+            <h3 className="article-heading-3" id="6.3">Industry impact</h3>
+            <h3 className="article-heading-3" id="6.4">Awards and nominations</h3>
+          </div>
+        </div>
+        <div>
+          <Heading2 id={7} title={"Other media and products"} />
+          <div>
+            <h3 className="article-heading-3" id="7.1">Possible feature film</h3>
+            <h3 className="article-heading-3" id="7.2">Spin-off series</h3>
+            <h3 className="article-heading-3" id="7.3">Cameos</h3>
+            <h4 className="article-heading-4" id="7.3.1">Film</h4>
+            <h4 className="article-heading-4" id="7.3.2">Television</h4>
+
+            <h3 className="article-heading-3" id="7.4">Comics</h3>
+            <h3 className="article-heading-3" id="7.5">Product tie-in</h3>
+          </div>
+        </div>
       </main>
     </>
   )
