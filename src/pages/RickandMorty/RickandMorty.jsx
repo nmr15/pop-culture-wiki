97bc5fa7c95b799e418b8d50eb42060a981b3c28
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-01 13:02:59 -0700

    update rm

--- src/pages/RickandMorty/RickandMorty.jsx
+++ src/pages/RickandMorty/RickandMorty.jsx
@@ -139,7 +139,25 @@ const RickandMorty = () =>
         <div>
           <Heading2 id={1} title={"Premise"} />
           <div>
-
+            <p>
+              The show revolves around the adventures of the members of the Smith household, which consists of parents Jerry and Beth, their children Summer and
+              Morty, and Beth's father, Rick Sanchez, who lives with them as a guest. The family lives in a suburb outside of Seattle, Washington, with the
+              characters traveling to other planets and dimensions through portals and Rick's flying saucer.
+            </p>
+            <p>
+              Rick is an eccentric and alcoholic mad scientist, who eschews many ordinary conventions such as school, marriage, love, and family. He frequently goes
+              on adeventures with his 14-year-old grandson, Morty, a kind-hearted but easily distressed boy, whose naive but grounded moral compass plays
+              counterpoint to Rick's Machiavellian ego. Morty's 17-year old sister, Summer, is a more conventional teenager who worries about improving her status
+              among her peers and sometimes follows Rick and Morty on their adventures. The kids' mother, Beth, is a generally level-headed person and assertive
+              force in the household, though self-conscious about her professional role as a horse surgeon. She is dissastisfied with her marriage to Jerry, a
+              simple-minded and insecure person, who disapproves of Rick's influence over his family.
+            </p>
+            <p>
+              Different versions of the characters inhabit other dimensions throughout the show's multiverse and their personal characteristics can vary from one
+              reality to another. The show's original Rick identifies himself as "Rick Sanchez of Earth Dimension C-137", in reference to his original universe,
+              but this does not apply to any other member of the Smith household. For instance, in the season one episode "<Link>Rick Potion #9</Link>", after
+              turning the entire world population into monsters, Rick and Morty move to a different dimension, leaving the original Summer, Beth, and Jerry behind.
+            </p>
           </div>
         </div>
         <div>
