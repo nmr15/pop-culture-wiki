12bfcefe03a481c58ef3690ffc856611f650917e
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-01 13:58:07 -0700

    update thelastderp

--- src/pages/StarWars/Films/LastJedi.jsx
+++ src/pages/StarWars/Films/LastJedi.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Collapse from '../../../components/Collapse'
 import Heading2 from '../../../components/Heading2'
+import TableStarWars from '../../../components/TableStarWars'
 
 const LastJedi = () =>
 {
@@ -15,44 +16,74 @@ const LastJedi = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
-          <li className="sidebar-list-item"><a href="#1">Plot</a></li>
-          <li className="sidebar-list-item"><a href="#2">Cast</a></li>
+          <li className="sidebar-list-item"><a href="#1">Opening crawl</a></li>
+          {/* <Collapse 
+            id="2"
+            item="Plot"
+            sub={[
+              {id: "2.1", title: "Evacutation of D'Qar"},
+              {id: "2.2", title: "Bomber run"},
+              {id: "2.3", title: ""},
+              {id: "2.4", title: ""},
+              {id: "2.5", title: ""},
+              {id: "2.6", title: ""},
+              {id: "2.7", title: ""},
+              {id: "2.8", title: ""},
+              {id: "2.9", title: ""},
+              {id: "2.10", title: ""},
+              {id: "2.11", title: ""},
+              {id: "2.12", title: ""},
+              {id: "2.13", title: ""},
+              {id: "2.14", title: ""},
+              {id: "2.15", title: ""},
+              {id: "2.16", title: ""},
+              {id: "2.17", title: ""},
+              {id: "2.18", title: ""},
+              {id: "2.19", title: ""},
+              {id: "2.20", title: ""},
+              {id: "2.21", title: ""},
+              {id: "2.22", title: ""},
+              {id: "2.23", title: ""},
+              {id: "2.24", title: ""}
+            ]}
+          /> */}
+          <li className="sidebar-list-item"><a href="#3">Cast</a></li>
           <Collapse 
-            id="3"
+            id="4"
             item="Production"
             sub={[
-              {id: "3.1", title: "Development"},
-              {id: "3.2", title: "Writing"},
-              {id: "3.3", title: "Casting"},
-              {id: "3.4", title: "Filming"},
-              {id: "3.5", title: "Music"}
+              {id: "4.1", title: "Development"},
+              {id: "4.2", title: "Writing"},
+              {id: "4.3", title: "Casting"},
+              {id: "4.4", title: "Filming"},
+              {id: "4.5", title: "Music"}
             ]}
           />
-          <li className="sidebar-list-item"><a href="#4">Marketing</a></li>
-          <Collapse 
-            id="5"
-            item="Release"
-            sub={[
-              {id: "5.1", title: "Theatrical"},
-              {id: "5.2", title: "Home media"}
-            ]}
-          />
+          <li className="sidebar-list-item"><a href="#5">Marketing</a></li>
           <Collapse 
             id="6"
-            item="Reception"
+            item="Release"
             sub={[
-              {id: "6.1", title: "Box office", sub2: [
-                {id: "6.1.1", title: "United States and Canada"},
-                {id: "6.1.2", title: "Other territories"}
-              ]},
-              {id: "6.2", title: "Critical response"},
-              {id: "6.3", title: "Audience reception", sub2: [
-                {id: "6.3.1", title: "Harassment"}
-              ]},
-              {id: "6.4", title: "Accolades"}
+              {id: "6.1", title: "Theatrical"},
+              {id: "6.2", title: "Home media"}
             ]}
           />
-          <li className="sidebar-list-item"><a href="#7">Sequel</a></li>
+          <Collapse 
+            id="7"
+            item="Reception"
+            sub={[
+              {id: "7.1", title: "Box office", sub2: [
+                {id: "7.1.1", title: "United States and Canada"},
+                {id: "7.1.2", title: "Other territories"}
+              ]},
+              {id: "7.2", title: "Critical response"},
+              {id: "7.3", title: "Audience reception", sub2: [
+                {id: "7.3.1", title: "Harassment"}
+              ]},
+              {id: "7.4", title: "Accolades"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Sequel</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
