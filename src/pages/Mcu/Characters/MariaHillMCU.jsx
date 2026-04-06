f43132c86fc70ec32177a25f22a4d9c7e65b2aed
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-06 13:48:01 -0700

    update mh

--- src/pages/Mcu/Characters/MariaHillMCU.jsx
+++ src/pages/Mcu/Characters/MariaHillMCU.jsx
@@ -15,6 +15,72 @@ const MariaHillMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "S.H.I.E.L.D. Agent", sub2: [
+                {id: "1.1.1", title: "Recruited by Director Fury"}
+              ]},
+              {id: "1.2", title: "Chitauri invasion", sub2: [
+                {id: "1.2.1", title: "Destruction of P.E.G.A.S.U.S."},
+                {id: "1.2.2", title: "Assembling the Avengers"},
+                {id: "1.2.3", title: "Attack on the Helicarrier"},
+                {id: "1.2.4", title: "Losing Phil Coulson"},
+                {id: "1.2.5", title: "Battle of New York"},
+                {id: "1.2.6", title: "Invasion aftermath"},
+                {id: "1.2.7", title: "Resurrection of Phil Coulson"}
+              ]},
+              {id: "1.3", title: "Fall of S.H.I.E.L.D.", sub2: [
+                {id: "1.3.1", title: "Attack on Nick Fury"},
+                {id: "1.3.2", title: "Saving Captain America"},
+                {id: "1.3.3", title: "Battle at the Triskelion"},
+                {id: "1.3.4", title: "Stark Industries"},
+              ]},
+              {id: "1.4", title: "Assisting Phil Coulson", sub2: [
+                {id: "1.4.1", title: "Confronted by Melinda May"},
+                {id: "1.4.2", title: "Attack on Providence"},
+                {id: "1.4.3", title: "Confronting Grant Ward"}
+              ]},
+              {id: "1.5", title: "Working with the Avengers", sub2: [
+                {id: "1.5.1", title: "Avengers Reassembled"},
+                {id: "1.5.2", title: "Finding the Scepter"},
+                {id: "1.5.3", title: "Debriefing Captain America"},
+                {id: "1.5.4", title: "Avengers' celebration"},
+                {id: "1.5.5", title: "Attack on Avengers Tower"},
+                {id: "1.5.6", title: "Avengers over Ultron"},
+                {id: "1.5.7", title: "Hiding the Avengers"},
+                {id: "1.5.8", title: "Battle of Sokovia"}
+              ]},
+              {id: "1.6", title: "Nick Fury's crew", sub: [
+                {id: "1.6.1", title: "Avengers Compound"},
+                {id: "1.6.2", title: "Thanos victory"},
+                {id: "1.6.3", title: "The Blip"}
+              ]},
+              {id: "1.7", title: "Secret Invasion", sub2: [
+                {id: "1.7.1", title: "Fighting back"},
+                {id: "1.7.2", title: "Nick Fury's return"},
+                {id: "1.7.3", title: "Pursuing G'iah"},
+                {id: "1.7.4", title: "Chat with Nick Fury"},
+                {id: "1.7.5", title: "Unity Day bombing"}
+              ]},
+              {id: "1.8", title: "Legacy", sub2: [
+                {id: "1.8.1", title: "Mother's grief"},
+                {id: "1.8.2", title: "Death footage leaked"}
+              ]}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#2">Abilities</a></li>
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Weapons"},
+              {id: "3.2", title: "Other equipment"},
+              {id: "3.3", title: "Vehicles"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -41,8 +107,20 @@ const MariaHillMCU = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data"><Link>Cobie Smulders</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Appearances</th>
+              <td className="infobox-data">
+                <Link to="mcu/the_avengers_film">The Avengers</Link> <br />
+                <Link to="/mcu/captain_america_the_winter_soldier">Captain America: The Winter Soldier</Link> <br />
+                <Link to="/mcu/avengers_age_of_ultron">Avengers: Age of Ultron</Link> <br />
+                <Link to="/mcu/avengers_infinity_war">Avengers: Infinity War</Link> (post-credits scene) <br />
+                <Link to="/mcu/avengers_endgame">Avengers: Endgame</Link> <br />
+                <Link to="/mcu/spiderman_far_from_home">Spider-Man: Far From Home</Link> <br />
+                <Link to="/mcu/secret_invasion">Secret Invasion</Link>
+              </td>
             </tr>
           </tbody>
         </table>
