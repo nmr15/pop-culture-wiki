ebfcef73890c13532d1b46a871af4093e0945738
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-06 16:21:26 -0700

    update table

--- src/components/TableSpiderManFilms.jsx
+++ src/components/TableSpiderManFilms.jsx
@@ -23,6 +23,36 @@ const TableSpiderManFilms = () =>
                 </div>
               </td>
             </tr>
+            <tr>
+              <th>Webb films</th>
+              <td>
+                <div className="table-list">
+                  {webbFilms.map((webb) => (
+                    <Link to={webb.route} className="table-list-link" key={webb.id}>{webb.title}</Link>
+                  ))}
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>Spider-Verse films</th>
+              <td>
+                <div className="table-list">
+                  {spiderverseFilms.map((verse) => (
+                    <Link to={verse.route} className="table-list-link" key={verse.id}>{verse.title}</Link>
+                  ))}
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>Sony Spider-Man Universe films</th>
+              <td>
+                <div className="table-list">
+                  {ssuFilms.map((ssu) => (
+                    <Link to={ssu.route} className="table-list-link" key={ssu.id}>{ssu.title}</Link>
+                  ))}
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
