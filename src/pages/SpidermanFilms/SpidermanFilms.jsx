406605011f94f6f8d6cbe9f72de784a8d4e296fe
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-06 14:53:07 -0700

    update spf

--- src/pages/SpidermanFilms/SpidermanFilms.jsx
+++ src/pages/SpidermanFilms/SpidermanFilms.jsx
@@ -114,7 +114,32 @@ const SpidermanFilms = () =>
           </tbody>
         </table>
         <div className="div-top">
-            
+          <p>
+            <b>Spider-Man</b>, a superhero character, created by <Link>Stan Lee</Link> and <Link>Steve Ditko</Link> for comic books published by
+            <Link to="/marvelcomics"> Marvel Comics</Link>, has appeared in film since CBS's 1977 television film pilot, for the program The Amazing Spider-Man.
+            Marvel Comics pursued plans for a feature film in the 1980s in a tumultuous development process that involved multiple directors, writers, and
+            financiers. After protected litigation over the Spider-Man copyrights, Sony Pictures and its subsidiariy Columbia obtained the film rights for $7 million
+            through a joint venture with Marvel in 1999.
+          </p>
+          <p>
+            Beginning in the 2000s, Columbia developed a film trilogy starring <Link>Tobey Maguire</Link> as <Link>Spider-Man</Link>, which includes
+            <Link to="/spiderman_films/spider_man"> Spider-Man</Link> (2002), <Link to="/spiderman_films/spider_man_2">Spider-Man 2</Link> (2004), and
+            <Link to="/spiderman_films/spider_man_3"> Spider-Man 3</Link> (2007). The original film, in particular, greatly influenced Hollywood's conception of
+            superheroes. A reboot series featuring <Link>Andrew Garfield</Link> as <Link>Spider-Man</Link>, consisting of
+            <Link to="/spiderman_films/the_amazing_spider_man"> The Amazing Spider-Man</Link> (2012), and
+            <Link to="/spiderman_films/the_amazing_spider_man_2"> The Amazing Spider-Man 2</Link> (2014). Sony then licensed the rights to <Link>Marvel Studios</Link>
+            in a renewed agreement, yielding a succession of films that incorporated the character into the <Link to="/mcu">Marvel Cinematic Universe</Link> (MCU).
+            The MCU features a version of <Link to="/mcu/peter_parker">Spider-Man</Link> played by <Link>Tom Holland</Link>, beginning with
+            <Link to="/mcu/captain_america_civil_war"> Captain America: Civil War</Link> (2016). The animated Spider-Verse centers on a cast of Spider-Men led by
+            <Link> Miles Morales</Link>, the main protagonist voiced by Shameik Moore. Sony's <Link>Spider-Man Universe</Link> (SSU) operates with a loosely
+            shared continuity to all existing Spider-Man film franchises but does not explicitly feature the character. Amateur films about Spider-Man explore a
+            range of interpretations from ordinary people.
+          </p>
+          <p>
+            The mainstream Spider-Man films have been highly successful and make up the second highest grossing film franchise of all time, collectively grossing
+            over $11 billion worldwide. Critical opinion of films skew positive, occasionally garnering Academy Awards recognition for achievement in special
+            effects and animation.
+          </p>
         </div>
       </main>
     </>
