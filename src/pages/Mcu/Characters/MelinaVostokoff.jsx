e0214dbaeeae58324ca57a65ba8d0b025f1ec382
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-07 13:40:16 -0700

    update mv

--- src/pages/Mcu/Characters/MelinaVostokoff.jsx
+++ src/pages/Mcu/Characters/MelinaVostokoff.jsx
@@ -15,6 +15,34 @@ const MelinaVostokoff = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Red Room assassin", sub2: [
+                {id: "1.1.1", title: "Black Widow training"},
+                {id: "1.1.2", title: "Undercover mission"},
+                {id: "1.1.3", title: "Dreykov's architect"}
+              ]},
+              {id: "1.2", title: "Betraying Dreykov", sub2: [
+                {id: "1.2.1", title: "Family reunion"},
+                {id: "1.2.2", title: "Making a plan"},
+                {id: "1.2.3", title: "Destruction of the Red Room"},
+                {id: "1.2.4", title: "Family split apart"}
+              ]}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#2">Abilities</a></li>
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Weapons"},
+              {id: "3.2", title: "Other equipment"},
+              {id: "3.3", title: "Vehicles"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -41,8 +69,12 @@ const MelinaVostokoff = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data"><Link>Rachel Weisz</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Appearances</th>
+              <td className="infobox-data"><Link to="/mcu/black_widow_film">Black Widow</Link></td>
             </tr>
           </tbody>
         </table>
