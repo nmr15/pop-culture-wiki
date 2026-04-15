dea094322bb2214f27a409d7ee7994fc5ba0f632
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-15 15:27:19 -0700

    update ahs

--- src/pages/StarWars/Characters/AhsokaTano.jsx
+++ src/pages/StarWars/Characters/AhsokaTano.jsx
@@ -110,10 +110,69 @@ const AhsokaTano = () =>
                 ]},
                 {id: "2.6.5", title: "Burying the dead"}
               ]},
-              {id: "2.7", title: "Age of the Empire"},
-              {id: "2.8", title: "Fighting for the rebellion"},
+              {id: "2.7", title: "Age of the Empire", sub2: [
+                {id: "2.7.1", title: "Life on Thabeska"},
+                {id: "2.7.2", title: "Raada", sub3: [
+                  {id: "2.7.2.1", title: "A pastoral refuge"},
+                  {id: "2.7.2.2", title: "Stockpiling supplies"},
+                  {id: "2.7.2.3", title: "Sowing seeds of rebellion"},
+                  {id: "2.7.2.4", title: "Building a resistance"},
+                  {id: "2.7.2.5", title: "Sabotage mission"},
+                  {id: "2.7.2.6", title: "An abortive uprising"},
+                  {id: "2.7.2.7", title: "A resistance in hiding"}
+                ]},
+                {id: "2.7.3", title: "Return to the Fardis", sub3: [
+                  {id: "2.7.3.1", title: "Reestablishing work"},
+                  {id: "2.7.3.2", title: "Duties"},
+                  {id: "2.7.3.3", title: "Final departure"}
+                ]},
+                {id: "2.7.4", title: "Entanglements"},
+                {id: "2.7.5", title: "A new ally"},
+                {id: "2.7.6", title: "Planning her return to Raada"},
+                {id: "2.7.7", title: "Return to Raada", sub3: [
+                  {id: "2.7.7.1", title: "Voyage"},
+                  {id: "2.7.7.2", title: "Reunion with Miara"},
+                  {id: "2.7.7.3", title: "Duel with the Sixth Brother"},
+                  {id: "2.7.7.4", title: "The evacuation"}
+                ]},
+                {id: "2.7.8", title: "Joining Organa's rebellion"},
+                {id: "2.7.9", title: "Deciding to take an active role"},
+                {id: "2.7.10", title: "Vader's vision"}
+              ]},
+              {id: "2.8", title: "Fighting for the rebellion", sub2: [
+                {id: "2.8.1", title: "Capturing Admiral Gann"},
+                {id: "2.8.2", title: "The Bardottan Sphere"},
+                {id: "2.8.3", title: "Undercover messenger"},
+                {id: "2.8.4", title: "A lesson for Bridger"},
+                {id: "2.8.5", title: "Attempting to rescue Janard"},
+                {id: "2.8.6", title: "The Siege of Lothal"},
+                {id: "2.8.7", title: "Reuniting with Rex"},
+                {id: "2.8.8", title: "Discovering more Inquisitors"},
+                {id: "2.8.9", title: "Rescuing infants"},
+                {id: "2.8.10", title: "Anguish on Lothal"},
+                {id: "2.8.11", title: "Twilight of the apprentice", sub3: [
+                  {id: "2.8.11.1", title: "Traveling to Malachor"},
+                  {id: "2.8.11.2", title: "The fateful duel"}
+                ]},
+                {id: "2.8.12", title: "The World Between Worlds"}
+              ]},
               {id: "2.9", title: "Lost and found"},
-              {id: "2.10", title: "Thrawn campaign"},
+              {id: "2.10", title: "Thrawn campaign", sub2: [
+                {id: "2.10.1", title: "Dealing with Morgan Elsbeth", sub3: [
+                  {id: "2.10.1.1", title: "Fight on Corvus"},
+                  {id: "2.10.1.2", title: "The Mandalorian and the foundling"},
+                  {id: "2.10.1.3", title: "Liberating the city"}
+                ]},
+                {id: "2.10.2", title: "Visiting Ossus"},
+                {id: "2.10.3", title: "Searching for Grand Admiral Thrawn", sub3: [
+                  {id: "2.10.3.1", title: "The ancient temple"},
+                  {id: "2.10.3.2", title: "Speaking with Syndulla"},
+                  {id: "2.10.3.3", title: "A difficult reunion"},
+                  {id: "2.10.3.4", title: "Pursuit of the Empire"},
+                  {id: "2.10.3.5", title: "Corellia Invesigation"},
+                  {id: "2.10.3.6", title: "Skirmish over Seatos"}
+                ]}
+              ]},
               {id: "2.11", title: "Legacy"}
             ]}
           />
