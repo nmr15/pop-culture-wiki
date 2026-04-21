3439423a4c6e2b582c519c9616ea94e44aa70dd8
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-04-21 08:21:32 -0700

    update halo

--- src/data/dataHalo.js
+++ src/data/dataHalo.js
@@ -1,1 +1,58 @@
-export const halo = 
+export const halo = [
+  {
+    "id": "halogames1",
+    "title": "Halo: Combat Evolved",
+    "file": "Halo1",
+    "img": "https://static.wikia.nocookie.net/halo/images/8/81/Halo_Combat_Evolved_-_Xbox_Cover.png",
+    "route": "/halo/halo_combat evolved"
+  },
+  {
+    "id": "halogames2",
+    "title": "Halo 2",
+    "file": "Halo2",
+    "img": "https://static.wikia.nocookie.net/halo/images/7/7c/Halo_2_box_art.jpg",
+    "route": "/halo/halo_2"
+  },
+  {
+    "id": "halogames3",
+    "title": "Halo 3",
+    "file": "Halo3",
+    "img": "https://static.wikia.nocookie.net/halo/images/e/e5/Halo3coverart.JPG",
+    "route": "/halo/halo_3"
+  },
+  {
+    "id": "halogames4",
+    "title": "Halo 3: ODST",
+    "file": "Halo3ODST",
+    "img": "https://static.wikia.nocookie.net/halo/images/a/a0/Halo_3_ODST_Cover.png",
+    "route": "/halo/halo_3_odst"
+  },
+  {
+    "id": "halogames5",
+    "title": "Halo: Reach",
+    "file": "HaloReach",
+    "img": "https://static.wikia.nocookie.net/halo/images/6/65/Halo_Reach_%28Standard_with_ESRB%29.png",
+    "route": "/halo/halo_reach"
+  },
+  {
+    "id": "halogames6",
+    "title": "Halo 4",
+    "file": "Halo4",
+    "img": "https://static.wikia.nocookie.net/halo/images/a/a1/Halo-4-Box-Art.jpg",
+    "route": "/halo/halo_4"
+  },
+  {
+    "id": "halogames7",
+    "title": "Halo 5: Guardians",
+    "file": "Halo5",
+    "img": "https://static.wikia.nocookie.net/halo/images/0/0a/H5G_BoxArtFinal-Front.png",
+    "route": "/halo/halo_5_guardians"
+  },
+  {
+    "id": "halogames8",
+    "title": "Halo Infinite",
+    "file": "HaloInfinite",
+    "img": "https://static.wikia.nocookie.net/halo/images/e/e3/Halo_Infinite_Official_Release_Poster.png",
+    "route": "/halo/halo_infinite"
+  }
+]
