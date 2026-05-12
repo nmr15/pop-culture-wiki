87079af791afffb8c7218d0c3ae91cdf6a6888cd
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-05-12 13:26:30 -0700

    new array

---
+++ src/data/dataCallofDuty.js
@@ -0,0 +1,149 @@
+export const codGames = [
+  {
+    "id": "codgames1",
+    "title": "Call of Duty",
+    "file": "CallofDuty1",
+    "img": "https://upload.wikimedia.org/wikipedia/en/7/7c/Call_Of_Duty_%282003%29%2CCover%2CUpdated.jpg",
+    "route": "/call_of_duty/call_of_duty_game"
+  },
+  {
+    "id": "codgames2",
+    "title": "Call of Duty 2",
+    "file": "CallofDuty2",
+    "img": "https://upload.wikimedia.org/wikipedia/en/d/d1/Call_of_Duty_2_Box.jpg",
+    "route": "/call_of_duty/call_of_duty_2"
+  },
+  {
+    "id": "codgames3",
+    "title": "Call of Duty 3",
+    "file": "CallofDuty3",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/51/Call_of_Duty_3_Game_Cover.jpg",
+    "route": "/call_of_duty/call_of_duty_3"
+  },
+  {
+    "id": "codgames4",
+    "title": "Call of Duty 4: Modern Warfare",
+    "file": "CallofDuty4",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/5f/Call_of_Duty_4_Modern_Warfare.jpg",
+    "route": "/call_of_duty/call_of_duty_4:_modern_warfare"
+  },
+  {
+    "id": "codgames5",
+    "title": "Call of Duty: World at War",
+    "file": "CallofDutyWaW",
+    "img": "https://upload.wikimedia.org/wikipedia/en/6/69/WAW_Cover_Art.jpg",
+    "route": "/call_of_duty/call_of_duty:_world_at_war"
+  },
+  {
+    "id": "codgames6",
+    "title": "Call of Duty: Modern Warfare 2",
+    "file": "CallofDutyMW2",
+    "img": "https://upload.wikimedia.org/wikipedia/en/5/52/Call_of_Duty_Modern_Warfare_2_%282009%29_cover.png",
+    "route": "/call_of_duty/call_of_duty:_modern_warfare_2"
+  },
+  {
+    "id": "codgames7",
+    "title": "Call of Duty: Black Ops",
+    "file": "CallofDutyBO",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/02/CoD_Black_Ops_cover.png",
+    "route": "/call_of_duty/call_of_duty:_black_ops"
+  },
+  {
+    "id": "codgames8",
+    "title": "Call of Duty: Modern Warfare 3",
+    "file": "CallofDutyMW3",
+    "img": "https://upload.wikimedia.org/wikipedia/en/b/bf/Call_of_Duty_Modern_Warfare_3_box_art.png",
+    "route": "/call_of_duty/call_of_duty:_modern_warfare_3"
+  },
+  {
+    "id": "codgames9",
+    "title": "Call of Duty: Black Ops II",
+    "file": "CallofDutyBO2",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/05/Call_of_Duty_Black_Ops_II_box_artwork.png",
+    "route": "/call_of_duty/call_of_duty:_black_ops_ii"
+  },
+  {
+    "id": "codgames10",
+    "title": "Call of Duty: Ghosts",
+    "file": "CallofDutyGhosts",
+    "img": "https://upload.wikimedia.org/wikipedia/en/9/9c/Call_of_duty_ghosts_box_art.jpg",
+    "route": "/call_of_duty/call_of_duty:_ghosts"
+  },
+  {
+    "id": "codgames11",
+    "title": "Call of Duty: Advanced Warfare",
+    "file": "CallofDutyAW",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/3b/Advanced_Warfare.jpg",
+    "route": "/call_of_duty/call_of_duty:_advanced_warfare"
+  },
+  {
+    "id": "codgames12",
+    "title": "Call of Duty: Black Ops III",
+    "file": "CallofDutyBOIII",
+    "img": "https://upload.wikimedia.org/wikipedia/en/b/b1/Black_Ops_3.jpg",
+    "route": "/call_of_duty/call_of_duty:_black_ops_iii"
+  },
+  {
+    "id": "codgames13",
+    "title": "Call of Duty: Infinite Warfare",
+    "file": "CallofDutyIW",
+    "img": "https://upload.wikimedia.org/wikipedia/en/8/87/Call_of_Duty_Infinite_Warfare_cover.jpg",
+    "route": "/call_of_duty/call_of_duty:_infinite_warfare"
+  },
+  {
+    "id": "codgames14",
+    "title": "Call of Duty: Black Ops 4",
+    "file": "CallofDutyBO4",
+    "img": "https://upload.wikimedia.org/wikipedia/en/1/1c/Call_of_Duty_Black_Ops_4_official_box_art.jpg",
+    "route": "/call_of_duty/call_of_duty:_black_ops_4"
+  },
+  {
+    "id": "codgames15",
+    "title": "Call of Duty: Modern Warfre",
+    "file": "CallofDutyMW",
+    "img": "https://upload.wikimedia.org/wikipedia/en/1/1f/Call_of_Duty_Modern_Warfare_%282019%29_cover.jpg",
+    "route": "/call_of_duty/call_of_duty:_modern_warfare"
+  },
+  {
+    "id": "codgames16",
+    "title": "Call of Duty: Black Ops Cold War",
+    "file": "CallofDutyBOCW",
+    "img": "https://upload.wikimedia.org/wikipedia/en/3/31/BOCW_Cover_Art.jpg",
+    "route": "/call_of_duty/call_of_duty:_black_ops_cold_war"
+  },
+  {
+    "id": "codgames17",
+    "title": "Call of Duty: Vanguard",
+    "file": "CallofDutyVanguard",
+    "img": "https://upload.wikimedia.org/wikipedia/en/0/06/Call_of_Duty_Vanguard_cover_art.jpg",
+    "route": "/call_of_duty/call_of_duty:_vanguard"
+  },
+  {
+    "id": "codgames18",
+    "title": "Call of Duty: Modern Warfare II",
+    "file": "CallofDutyMWII",
+    "img": "https://upload.wikimedia.org/wikipedia/en/4/4a/Call_of_Duty_Modern_Warfare_II_Key_Art.jpg",
+    "route": "/call_of_duty/call_of_duty:_modern_warfare_ii"
+  },
+  {
+    "id": "codgames19",
+    "title": "Call of Duty: Modern Warfare III",
+    "file": "CallofDutyMWIII",
+    "img": "https://upload.wikimedia.org/wikipedia/en/f/f6/MWIII_Cover_Art.png",
+    "route": "/call_of_duty/call_of_duty:_modern_warfare_iii"
+  },
+  {
+    "id": "codgames20",
+    "title": "Call of Duty: Black Ops 6",
+    "file": "CallofDutyBO6",
+    "img": "https://upload.wikimedia.org/wikipedia/en/c/c9/Call_of_Duty_Black_Ops_6_Key_Art.png",
+    "route": "/call_of_duty/call_of_duty:_black_ops_6"
+  },
+  {
+    "id": "codgames20",
+    "title": "Call of Duty: Black Ops 7",
+    "file": "CallofDutyBO7",
+    "img": "https://upload.wikimedia.org/wikipedia/en/a/a3/Call_of_Duty_Black_Ops_7_Key_Art.png",
+    "route": "/call_of_duty/call_of_duty:_black_ops_7"
+  }
+]
