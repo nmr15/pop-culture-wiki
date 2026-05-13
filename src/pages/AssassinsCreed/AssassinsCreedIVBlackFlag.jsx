50175c47027f25dd745c66225aee825a436f3f47
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-05-13 16:02:21 -0700

    update ac4

--- src/pages/AssassinsCreed/AssassinsCreedIVBlackFlag.jsx
+++ src/pages/AssassinsCreed/AssassinsCreedIVBlackFlag.jsx
@@ -1,13 +1,32 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Collapse from '../../components/Collapse'
+import Heading2 from '../../components/Heading2'
 import TableAssassinsCreed from '../../components/TableAssassinsCreed'
 
 const AssassinsCreedIVBlackFlag = () =>
 {
+  useEffect(() =>
+  {
+    document.title = "Assassin's Creed IV: Black Flag - Pop Culture Wiki";
+  }, []);
+
   return(
     <>
       <aside className="sidebar">
-        <ul className="sidebar-list"></ul>
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Gameplay</a></li>
+          <Collapse 
+            id="2"
+            item="Synopsis"
+            sub={[
+              {id: "2.1", title: "Characters"},
+              {id: "2.2", title: "Setting"},
+              {id: "2.3", title: "Plot"}
+            ]}
+          />
+        </ul>
       </aside>
       <main className="main" id="top">
         <ul className="breadcrumb">
@@ -18,13 +37,38 @@ const AssassinsCreedIVBlackFlag = () =>
         <div>
           <h1 className="article-heading">Assassin's Creed IV: Black Flag</h1>
         </div>
-        <div className="img-right img-top">
-          <img 
-            src="https://upload.wikimedia.org/wikipedia/en/2/28/Assassin%27s_Creed_IV_-_Black_Flag_cover.jpg" 
-            alt="Assassin's Creed IV: Black Flag logo" 
-            className="article-heading-img"
-          />
-        </div>
+        <table className="infobox">
+          <tbody>
+            <tr>
+              <th colspan="2">
+                <span>Assassin's Creed IV: Black Flag</span>
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/2/28/Assassin%27s_Creed_IV_-_Black_Flag_cover.jpg" alt="Assassin's Creed IV: Black Flag cover" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Developers</th>
+              <td className="infobox-data"><Link>Ubisoft Montreal</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Publisher</th>
+              <td className="infobox-data">Ubisoft</td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Previous Assassin's Creed game</th>
+              <td className="infobox-data"><Link to="/assassinscreed/assassins_creed_iii">Assassin's Creed III</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Next Assassin's Creed game</th>
+              <td className="infobox-data"><Link to="/assassinscreed/assassins_creed_rogue">Assassin's Creed Rogue</Link></td>
+            </tr>
+          </tbody>
+        </table>
       </main>
     </>
   )
