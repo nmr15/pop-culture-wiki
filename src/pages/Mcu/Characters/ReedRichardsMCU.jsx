48c9af8e898ef6db270bed6d8a3f40b8821c0c89
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-05-27 16:05:34 -0700

    update rdrchrd

--- src/pages/Mcu/Characters/ReedRichardsMCU.jsx
+++ src/pages/Mcu/Characters/ReedRichardsMCU.jsx
@@ -15,6 +15,56 @@ const ReedRichardsMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life", sub2: [
+                {id: "1.1.1", title: "Teaching science"},
+                {id: "1.1.2", title: "Construction of the Baxter Building"}
+              ]},
+              {id: "1.2", title: "Founding the Fantastic Four", sub2: [
+                {id: "1.2.1", title: "Gaining abilities"},
+                {id: "1.2.2", title: "Isolation"},
+                {id: "1.2.3", title: "Revolt of Subterranea"},
+                {id: "1.3.4", title: "Establishing peace"}
+              ]},
+              {id: "1.3", title: "Fame", sub2: [
+                {id: "1.3.1", title: "Interviews"},
+                {id: "1.3.2", title: "Fighting Mad Thinker"},
+                {id: "1.3.3", title: "Advancing scientific study"},
+                {id: "1.3.4", title: "Expecting a new member"}
+              ]},
+              {id: "1.4", title: "Advent of Galactus", sub2: [
+                {id: "1.4.1", title: "Encountering the Silver Surfer"},
+                {id: "1.4.2", title: "Preparing for launch"},
+                {id: "1.4.3", title: "Visitng LHS-275"},
+                {id: "1.4.4", title: "Escape from the Silver Surfer"},
+                {id: "1.4.5", title: "Breakers of bad news"},
+                {id: "1.4.6", title: "Encouraging the world"},
+                {id: "1.4.7", title: "Battle of New York"}
+              ]},
+              {id: "1.5", title: "Continuing to change", sub2: [
+                {id: "1.5.1", title: "New adventure"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Poers and abilities"
+            sub={[
+              {id: "2.1", title: "Powers"},
+              {id: "2.2", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Vehicles"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
