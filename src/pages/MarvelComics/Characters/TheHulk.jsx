3d4458db242f84d714f998d8763ff8fe5bd80ede
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-06-11 15:31:34 -0700

    update hlk

--- src/pages/MarvelComics/Characters/TheHulk.jsx
+++ src/pages/MarvelComics/Characters/TheHulk.jsx
@@ -41,13 +41,38 @@ const TheHulk = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th colspan="2">Publication information</th>
+            </tr>
+            <tr>
+              <th className="infobox-data">Publisher</th>
+              <td className="infobox-data"><Link to="/marvelcomics">Marvel Comics</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Created by</th>
+              <td className="infobox-data">
+                <Link>Stan Lee</Link> <br />
+                <Link>Jack Kirby</Link>
+              </td>
+            </tr>
+            <tr>
+              <th colspan="2">In-story information</th>
+            </tr>
+            <tr>
+              <th className="infobox-data">Full name</th>
+              <td className="infobox-data">Robert Bruce Banner</td>
             </tr>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            The <b>Hulk</b> is a superhero appearing in American comic books published by <Link to="/marvelcomics">Marvel Comics</Link>. Created by writer
+            <Link> Stan Lee</Link> and artist <Link>Jack Kirby</Link>, the character first appeared in the debut issue of
+            <Link to="/marvelcomics/the_incredible_hulk_(comic_book)">The Incredible Hulk</Link> (May 1962). In his comic book appearances, the
+            character, who has dissociative identity disorder (DID), is primarily represnted by the alter ego Hulk, an immense, green-skinned, hulking brute,
+            possessing a limitless degree of physical strength, and the alter ego Dr. <b>Robert Bruce Banner</b>, a physically weak, socially withdrawn, and
+            emotionally reserved physicist, both of whom typically resent each other. Lee stated that the Hulk's creation was inspired by a combination of
+            Frankenstein and Dr. Jekyll and Mr. Hyde.
+          </p>
         </div>
       </main>
     </>
