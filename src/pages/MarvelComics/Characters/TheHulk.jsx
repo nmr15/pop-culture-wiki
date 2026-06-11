3690119f5701ecd41c02d2a18857789fd9ccc5c9
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-06-11 15:51:27 -0700

    update hlk

--- src/pages/MarvelComics/Characters/TheHulk.jsx
+++ src/pages/MarvelComics/Characters/TheHulk.jsx
@@ -36,7 +36,7 @@ const TheHulk = () =>
             <tr>
               <td colspan="2">
                 <span>
-                  <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Hulk_%28circa_2019%29.png" alt="The Hulk" />
+                  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Hulk_%28circa_2019%29.png/220px-Hulk_%28circa_2019%29.png" alt="The Hulk" />
                 </span>
               </td>
             </tr>
@@ -72,6 +72,24 @@ const TheHulk = () =>
             possessing a limitless degree of physical strength, and the alter ego Dr. <b>Robert Bruce Banner</b>, a physically weak, socially withdrawn, and
             emotionally reserved physicist, both of whom typically resent each other. Lee stated that the Hulk's creation was inspired by a combination of
             Frankenstein and Dr. Jekyll and Mr. Hyde.
+          </p>
+          <p>
+            Following his accidental exposure to gamma rays while saving the life of Rick Jones during the detonation of an experimental bomb, Bruce is physically
+            transformed into the Hulk when subjected to emotional stress, at or against his will. This transformation often leads to destructive rampages and
+            conflicts that complicate Bruce's civilian life. The Hulk's level of strength is usually conveyed proportionate to his anger level. Commonly portrayed as
+            a raging savage, the Hulk has been represented with other alter egos like a gangster (<b>Joe Fixit</b>), a merged personality (<b>Merged Hulk</b>), a
+            mindless, destructive force (<b>War</b>), a brilliant warrior (<b>World-Breaker</b>), a self-hating protector (<b>Devil Hulk</b>), a genius scientist
+            in his own right (<b>Doc Green</b>), and several minor alter egos.
+          </p>
+          <p>
+            Despite Hulk and Bruce's desire for solitude, the character has a large supporting cast. This includes Bruce's love interest
+            <Link to="/marvelcomics/betty_ross"> Betty Ross</Link>, his best friend, Rick Jones, his cousin <Link to="/marvelcomics/she_hulk">She-Hulk</Link>, and
+            therapist and ally <Link to="/marvelcomics/doc_samson">Doc Samson</Link>. In addition, the Hulk alter ego has many key supporting characters, like his
+            co-founders of the superhero team the <Link to="/marvelcomics/avengers">Avengers</Link>, his queen Caiera, fellow warriors Korg and Miek, and sons
+            Skaar and Hiro-Kala. However, his uncontrollable power has brought him into conflict with his fellow heroes and others such as
+            <Link to="/marvelcomics/thunderbolt_ross"> General Thunderbolt Ross</Link>, Betty's father. Despite this, he tries his best to do what's right while
+            battling villains such as the <Link to="/marvelcomics/leader">Leader</Link>, the <Link to="/marvelcomics/abomination">Abomination</Link>, the
+            <Link to="/marvelcomics/absorbing_man"> Absorbing Man</Link>, and many more.
           </p>
         </div>
       </main>
