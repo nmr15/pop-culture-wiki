5ab51b62d9f246ae077486b70817d6de19e8c036
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-07-09 10:49:32 -0700

    update sstrm

--- src/pages/Mcu/Characters/SueStormMCU.jsx
+++ src/pages/Mcu/Characters/SueStormMCU.jsx
@@ -15,6 +15,45 @@ const SueStormMCU = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Biography"
+            sub={[
+              {id: "1.1", title: "Early life"},
+              {id: "1.2", title: "Founding the Fantastic Four", sub2: [
+                {id: "1.2.1", title: "Gaining abilities"},
+                {id: "1.2.2", title: "Isolation"},
+                {id: "1.2.3", title: "Revolt of Subterranea"},
+                {id: "1.2.4", title: "Establishing peace"}
+              ]},
+              {id: "1.3", title: "Fame", sub2: [
+                {id: "1.3.1", title: "Interviewed"},
+                {id: "1.3.2", title: "Teaching science"},
+                {id: "1.3.3", title: "Fighting the Mad Thinker"},
+                {id: "1.3.4", title: "Diplomatic career"},
+                {id: "1.3.5", title: "Expecting a new member"}
+              ]},
+              {id: "1.4", title: "Advent of Galactus", sub2: [
+                {id: "1.4.1", title: "Ecountering the Silver Surfer"}
+              ]}
+            ]}
+          />
+          <Collapse 
+            id="2"
+            item="Powers and abilities"
+            sub={[
+              {id: "2.1", title: "Powers"},
+              {id: "2.2", title: "Abilities"}
+            ]}
+          />
+          <Collapse 
+            id="3"
+            item="Equipment"
+            sub={[
+              {id: "3.1", title: "Vehicles"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Facilities</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -40,8 +79,12 @@ const SueStormMCU = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Portrayed by</th>
+              <td className="infobox-data">Vanessa Kirby</td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Appearances</th>
+              <td className="infobox-data"><Link to="/mcu/the_fantastic_four_first_steps">The Fantastic Four: First Steps</Link></td>
             </tr>
           </tbody>
         </table>
