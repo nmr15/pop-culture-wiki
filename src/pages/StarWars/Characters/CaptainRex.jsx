9fe72e75efe9cde6cadca0968cc06be88c41e7c9
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-07-16 13:31:39 -0700

    update cptnrx

--- src/pages/StarWars/Characters/CaptainRex.jsx
+++ src/pages/StarWars/Characters/CaptainRex.jsx
@@ -16,6 +16,35 @@ const CaptainRex = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <li className="sidebar-list-item"><a href="#1">Creation and development</a></li>
+
+          <Collapse 
+            id="3"
+            item="Personality and traits"
+            sub={[
+              {id: "3.1", title: "Captain in the clone army"},
+              {id: "3.2", title: "Friend to the Jedi"},
+              {id: "3.3", title: "Soldier without a Republic"},
+              {id: "3.4", title: "Relationship with his fellow veterans"},
+              {id: "3.5", title: "Member of the Rebellion"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#4">Skills and abilities</a></li>
+          <li className="sidebar-list-item"><a href="#5">Equipment</a></li>
+          <Collapse 
+            id="6"
+            item="Appearances"
+            sub={[
+              {id: "6.1", title: "Star Wars: The Clone Wars"},
+              {id: "6.2", title: "Star Wars: Rebels"},
+              {id: "6.3", title: "Star Wars: The Bad Batch"},
+              {id: "6.4", title: "Tales of the Jedi"},
+              {id: "6.5", title: "Ahsoka"},
+              {id: "6.6", title: "In other media"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#7">Merchandise</a></li>
+          <li className="sidebar-list-item"><a href="#8">Reception</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
