107b8d43ee7890c1645b387b42b4ae4c2ba61d94
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-08-17 16:39:17 -0700

    update cclps

--- src/pages/MarvelComics/Characters/Cyclops.jsx
+++ src/pages/MarvelComics/Characters/Cyclops.jsx
@@ -15,6 +15,18 @@ const Cyclops = () =>
       <aside className="sidebar">
         <ul className="sidebar-list">
           <li className="sidebar-list-item"><a href="#top">Top</a></li>
+          <Collapse 
+            id="1"
+            item="Publication history"
+            sub={[
+              {id: "1.1", title: "1960s"},
+              {id: "1.2", title: "1970s"},
+              {id: "1.3", title: "1980s"},
+              {id: "1.4", title: "1990s"},
+              {id: "1.5", title: "2000s"},
+              {id: "1.6", title: "2010s and 2020s"}
+            ]}
+          />
         </ul>
       </aside>
       <main className="main" id="top">
@@ -41,8 +53,22 @@ const Cyclops = () =>
               </td>
             </tr>
             <tr>
-              <th className="infobox-data">Template</th>
-              <td className="infobox-data">Template</td>
+              <th className="infobox-data">Publisher</th>
+              <td className="infobox-data"><Link to="/marvelcomics">Marvel Comics</Link></td>
+            </tr>
+            <tr>
+              <th className="infobox-data">Created by</th>
+              <td className="infobox-data">
+                <Link to="/people/stan_lee">Stan Lee</Link> (writer) <br />
+                <Link to="/people/jack_kirby">Jack Kirby</Link> (artist/co-plotter)
+              </td>
+            </tr>
+             <tr>
+              <th colspan="2">In-story information</th>
+            </tr>
+            <tr>
+              <th className="infobox-data">Full name</th>
+              <td className="infobox-data">Scott Summers</td>
             </tr>
           </tbody>
         </table>
