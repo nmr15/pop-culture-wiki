1013ccf2961239805ac6b27d33471dd127796f30
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-08-21 15:59:35 -0700

    new array

--- src/data/dataPixar.js
+++ src/data/dataPixar.js
@@ -203,3 +203,307 @@ export const pixarFilms = [
     "route": "/pixar/ello"
   }
 ]
+
+export const pixarChars = [
+  {
+    "id": "pixarChar1",
+    "title": "Woody",
+    "file": "Woody",
+    "img": "https://static.wikia.nocookie.net/pixar/images/e/ef/Woody_profile.jpg",
+    "route": "/toy_story/woody"
+  },
+  {
+    "id": "pixarChar2",
+    "title": "Buzz Lightyear",
+    "file": "BuzzLightyear",
+    "img": "https://static.wikia.nocookie.net/pixar/images/5/56/Buzz_Profile.jpg",
+    "route": "/toy_story/buzz_lightyear"
+  },
+  {
+    "id": "pixarChar3",
+    "title": "Jessie",
+    "file": "JessieTS",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d6/Jessie_Profile.PNG",
+    "route": "/toy_story/jessie"
+  },
+  {
+    "id": "pixarChar4",
+    "title": "Mr. Potato Head",
+    "file": "MrPotatoHead",
+    "img": "https://static.wikia.nocookie.net/pixar/images/f/f0/SurprisedMrP_tato.jpg",
+    "route": "/toy_story/mr._potato_head"
+  },
+  {
+    "id": "pixarChar5",
+    "title": "Slinky Dog",
+    "file": "SlinkyDog",
+    "img": "https://static.wikia.nocookie.net/pixar/images/c/ce/Slinky0002.png",
+    "route": "/toy_story/slinky_dog"
+  },
+  {
+    "id": "pixarChar6",
+    "title": "Rex",
+    "file": "RexTS",
+    "img": "https://static.wikia.nocookie.net/pixar/images/1/14/Rex_%28Transparent%29.png",
+    "route": "/toy_story/rex"
+  },
+  {
+    "id": "pixarChar7",
+    "title": "Hamm",
+    "file": "Hamm",
+    "img": "https://static.wikia.nocookie.net/pixar/images/8/8d/Hamm_Profile.PNG",
+    "route": "/toy_story/hamm"
+  },
+  {
+    "id": "pixarChar8",
+    "title": "Bo Peep",
+    "file": "BoPeep",
+    "img": "https://static.wikia.nocookie.net/pixar/images/f/f0/Bo_Peep.png",
+    "route": "/toy_story/bo_peep"
+  },
+  {
+    "id": "pixarChar9",
+    "title": "Andy Davis",
+    "file": "AndyDavis",
+    "img": "https://static.wikia.nocookie.net/pixar/images/e/ed/Andy.webp",
+    "route": "/toy_story/andy_davis"
+  },
+  {
+    "id": "pixarChar10",
+    "title": "Flik",
+    "file": "Flik",
+    "img": "https://static.wikia.nocookie.net/pixar/images/e/e5/Profile_-_Flik.webp",
+    "route": "/pixar/flik"
+  },
+  {
+    "id": "pixarChar11",
+    "title": "Princess Atta",
+    "file": "Atta",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/78/Profile_-_Atta.webp",
+    "route": "/pixar/princess_atta"
+  },
+  {
+    "id": "pixarChar12",
+    "title": "Dot",
+    "file": "DotABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/6/62/Profile-_Dot.webp",
+    "route": "/pixar/dot"
+  },
+  {
+    "id": "pixarChar13",
+    "title": "Hopper",
+    "file": "HopperABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/8/81/Hopper.png",
+    "route": "/pixar/hopper"
+  },
+  {
+    "id": "pixarChar14",
+    "title": "The Queen",
+    "file": "TheQueenABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/6/64/Queen.jpg",
+    "route": "/pixar/the_queen"
+  },
+  {
+    "id": "pixarChar15",
+    "title": "Slim",
+    "file": "SlimABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/4/47/Slimbugslife.png",
+    "route": "/pixar/slim"
+  },
+  {
+    "id": "pixarChar16",
+    "title": "Francis",
+    "file": "FrancisABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/2/22/Bugs-life-disneyscreencaps.com-3409.jpg",
+    "route": "/pixar/francis"
+  },
+  {
+    "id": "pixarChar17",
+    "title": "Heimlich",
+    "file": "Heimlich",
+    "img": "https://static.wikia.nocookie.net/pixar/images/5/59/Heimlich_screaming.jpg",
+    "route": "/pixar/heimlich"
+  },
+  {
+    "id": "pixarChar18",
+    "title": "Manny",
+    "file": "MannyABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/0/0c/Manny.jpg",
+    "route": "/pixar/manny"
+  },
+  {
+    "id": "pixarChar19",
+    "title": "Gypsy",
+    "file": "GypsyABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/73/Gypsy_the_Moth_%28A_Bug%27s_Life%29.png",
+    "route": "/pixar/gypsy"
+  },
+  {
+    "id": "pixarChar20",
+    "title": "Rosie",
+    "file": "RosieABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d2/Bugs-life-disneyscreencaps.com-3353.jpg",
+    "route": "/pixar/rosie"
+  },
+  {
+    "id": "pixarChar21",
+    "title": "Tuck and Roll",
+    "file": "TuckandRoll",
+    "img": "https://static.wikia.nocookie.net/pixar/images/b/b6/Bugs-life-disneyscreencaps.com-4505.jpg",
+    "route": "/pixar/tuck_and_roll"
+  },
+  {
+    "id": "pixarChar22",
+    "title": "Dim",
+    "file": "DimABL",
+    "img": "https://static.wikia.nocookie.net/pixar/images/3/3c/Dimbugslife.jpg",
+    "route": "/pixar/dim"
+  },
+  {
+    "id": "pixarChar23",
+    "title": "Molt",
+    "file": "Molt",
+    "img": "https://static.wikia.nocookie.net/pixar/images/4/4e/Molt.png",
+    "route": "/pixar/molt"
+  },
+  {
+    "id": "pixarChar24",
+    "title": "James P. Sullivan",
+    "file": "SullyMI",
+    "img": "https://static.wikia.nocookie.net/pixar/images/e/e7/SulleyMAW.png",
+    "route": "/monsters_inc/james_p._sullivan"
+  },
+  {
+    "id": "pixarChar25",
+    "title": "Mike Wazowski",
+    "file": "MikeMI",
+    "img": "https://static.wikia.nocookie.net/pixar/images/9/96/MikeMAW.png",
+    "route": "/monsters_inc/mike_wazowski"
+  },
+  {
+    "id": "pixarChar26",
+    "title": "Randall Boggs",
+    "file": "RandallMI",
+    "img": "https://static.wikia.nocookie.net/pixar/images/0/09/Randall_Boggs.png",
+    "route": "/monsters_inc/randall_boggs"
+  },
+  {
+    "id": "pixarChar27",
+    "title": "Boo",
+    "file": "BooMI",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/7c/Boo.png",
+    "route": "/monsters_inc/boo"
+  },
+  {
+    "id": "pixarChar28",
+    "title": "Henry J. Waternoose",
+    "file": "Waternoose",
+    "img": "https://static.wikia.nocookie.net/pixar/images/5/51/Henry_J._Waternoose_III.jpg",
+    "route": "/monsters_inc/henry_j._waternoose"
+  },
+  {
+    "id": "pixarChar29",
+    "title": "Celia Mae",
+    "file": "CeliaMI",
+    "img": "https://static.wikia.nocookie.net/pixar/images/5/55/Celia_Mae.png",
+    "route": "/monsters_inc/celia_mae"
+  },
+  {
+    "id": "pixarChar30",
+    "title": "Roz",
+    "file": "Roz",
+    "img": "https://static.wikia.nocookie.net/pixar/images/f/f3/Roz1.jpg",
+    "route": "/monsters_inc/roz"
+  },
+  {
+    "id": "pixarChar31",
+    "title": "Nemo",
+    "file": "Nemo",
+    "img": "https://static.wikia.nocookie.net/pixar/images/a/aa/Nemo-FN.png",
+    "route": "/finding_nemo/nemo"
+  },
+  {
+    "id": "pixarChar32",
+    "title": "Marlin",
+    "file": "Marlin",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d9/Marlin.png",
+    "route": "/finding_nemo/marlin"
+  },
+  {
+    "id": "pixarChar33",
+    "title": "Dory",
+    "file": "Dory",
+    "img": "https://static.wikia.nocookie.net/pixar/images/1/1f/Dory-white.jpg",
+    "route": "/finding_nemo/dory"
+  },
+  {
+    "id": "pixarChar34",
+    "title": "Gill",
+    "file": "GillFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/72/Gill.png",
+    "route": "/finding_nemo/gill"
+  },
+  {
+    "id": "pixarChar35",
+    "title": "Bloat",
+    "file": "BloatFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/5/5b/Bloat_finding_nemo.png",
+    "route": "/finding_nemo/bloat"
+  },
+  {
+    "id": "pixarChar36",
+    "title": "Gurgle",
+    "file": "Gurgle",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d2/Gurgle.png",
+    "route": "/finding_nemo/gurgle"
+  },
+  {
+    "id": "pixarChar37",
+    "title": "Peach",
+    "file": "PeachFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/7e/Peach-0.png",
+    "route": "/finding_nemo/peach"
+  },
+  {
+    "id": "pixarChar38",
+    "title": "Deb",
+    "file": "DebFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/7/79/Deb1.png",
+    "route": "/finding_nemo/deb"
+  },
+  {
+    "id": "pixarChar39",
+    "title": "Jacques",
+    "file": "JacquesFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d5/Jacques.jpg",
+    "route": "/finding_nemo/jacques"
+  },
+  {
+    "id": "pixarChar40",
+    "title": "Bubbles",
+    "file": "BubblesFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/0/0c/Bubbles.png",
+    "route": "/finding_nemo/bubbles"
+  },
+  {
+    "id": "pixarChar41",
+    "title": "Bruce",
+    "file": "BruceFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/8/8f/Bruce-FindingNemo3D.jpg",
+    "route": "/finding_nemo/bruce"
+  },
+  {
+    "id": "pixarChar42",
+    "title": "Crush",
+    "file": "CrushFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/c/cb/Crush-FindingNemo3D.jpg",
+    "route": "/finding_nemo/crush"
+  },
+  {
+    "id": "pixarChar43",
+    "title": "Nigel",
+    "file": "NigelFN",
+    "img": "https://static.wikia.nocookie.net/pixar/images/d/d9/Finding_Nemo_Nigel.png",
+    "route": "/finding_nemo/nigel"
+  }
+]
