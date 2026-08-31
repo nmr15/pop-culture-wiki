0caa7b32b9e1a9a1c232d38417d90628ffa6ce07
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-08-31 16:22:36 -0700

    update cycps

--- src/pages/MarvelComics/Characters/Cyclops.jsx
+++ src/pages/MarvelComics/Characters/Cyclops.jsx
@@ -27,6 +27,45 @@ const Cyclops = () =>
               {id: "1.6", title: "2010s and 2020s"}
             ]}
           />
+
+          <Collapse 
+            id="3"
+            item="Personality"
+            sub={[
+              {id: "3.1", title: "Reomantic relationships"}
+            ]}
+          />
+          <Collapse 
+            id="4"
+            item="Attributes"
+            sub={[
+              {id: "4.1", title: "Powers"},
+              {id: "4.2", title: "Abilities"},
+              {id: "4.3", title: "Weaknesses"}
+            ]}
+          />
+          <Collapse 
+            id="5"
+            item="Paraphernalia"
+            sub={[
+              {id: "5.1", title: "Equipment"},
+              {id: "5.2", title: "Weapons"},
+              {id: "5.3", title: "Transportation"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#6">Themes and motifs</a></li>
+          <Collapse 
+            id="7"
+            item="Supporting characters"
+            sub={[
+              {id: "7.1", title: "Allies"},
+              {id: "7.2", title: "Romantic interests"},
+              {id: "7.3", title: "Family"},
+              {id: "7.4", title: "Alternate versions"}
+            ]}
+          />
+          <li className="sidebar-list-item"><a href="#8">Reception</a></li>
+          <li className="sidebar-list-item"><a href="#9">In other media</a></li>
         </ul>
       </aside>
       <main className="main" id="top">
@@ -73,7 +112,15 @@ const Cyclops = () =>
           </tbody>
         </table>
         <div className="div-top">
-          
+          <p>
+            <b>Cyclops</b> is a superhero appearing in American comic books published by <Link to="/marvelcomics">Marvel Comics</Link>. Created by
+            writer <Link to="/people/stan_lee">Stan Lee</Link> and artist/co-plotter <Link to="/people/jack_kirby">Jack Kirby</Link>, the character
+            first appeared in <Link to="/marvelcomics/uncanny_x-men_(comic_book_series)">The X-Men</Link> #1 as a founding member of the
+            <Link to="/marvelcomics/x-men_(team)"> X-Men</Link>. Cyclops is a member of a subspecies of humans known as
+            <Link to="/marvelcomics/mutants"> mutants</Link>, born with superhuman abilities. Cyclops emits powerful beams of energy from his eyes
+            and can control the beams with the aid of special eyewear, which he must always wear. He was the first member of the X-Men, a team of
+            mutant heroes who fight for peace and equality between mutants and humans, and one of the team's primary leaders.
+          </p>
         </div>
       </main>
     </>
